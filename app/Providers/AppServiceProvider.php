5353457225b931a06552ec8863e98516362a729f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-03 23:26:59 +0300

    Switch to using CarbonImmutable across the whole app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,20 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Date;
+use Carbon\CarbonImmutable;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        //
+        // Switch to using the immutable versions of Carbon objects across the whole application.
+        Date::use(CarbonImmutable::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         //
     }
