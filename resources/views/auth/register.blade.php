c0545f10c5acee7b65172714e410a415bbcc0f0f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-04 13:59:53 +0300

    Views code style fixes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,17 +16,40 @@
 
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input
+                    id="email"
+                    class="block mt-1 w-full"
+                    type="email"
+                    name="email"
+                    :value="old('email')"
+                    required
+                    autofocus
+                    autocomplete="email"
+                />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-input
+                    id="password"
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-input
+                    id="password_confirmation"
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -55,6 +78,8 @@
                     {{ __('Register') }}
                 </x-jet-button>
             </div>
+
         </form>
+
     </x-jet-authentication-card>
 </x-guest-layout>
--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,8 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
+<input
+    {!! $attributes->merge([
+        'class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm',
+    ]) !!}
+    {{ $disabled ? 'disabled' : '' }}
+>
