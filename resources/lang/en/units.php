7397c21d097b5468ea769159d649acb5a9cd5594
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-04 13:27:58 +0300

    Units naming lang strings

---
+++ resources/lang/en/units.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'B' => 'B',
+    'KB' => 'KB',
+    'MB' => 'MB',
+    'GB' => 'GB',
+    'TB' => 'TB',
+    'PB' => 'PB',
+    'EB' => 'EB',
+    'ZB' => 'ZB',
+    'YB' => 'YB',
+
+];
