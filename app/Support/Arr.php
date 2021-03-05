fc62894ec5215526bf551c5507259d0ab4de850c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-05 13:50:03 +0300

    Add support classes scaffolding

---
+++ app/Support/Arr.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Support;
+
+use Illuminate\Support\Arr as IlluminateArr;
+
+/**
+ * Custom support class for manipulating arrays
+ */
+class Arr extends IlluminateArr
+{
+    //
+}
---
+++ app/Support/Str.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Support;
+
+use Illuminate\Support\Str as IlluminateStr;
+
+/**
+ * Custom support class for manipulating strings
+ */
+class Str extends IlluminateStr
+{
+    //
+}
