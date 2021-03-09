159a0bfe31e0ba67acebdc2464b1d7a39f65ffdd
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-09 22:43:19 +0300

    Input styling update

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,8 +1,10 @@
+{{--TODO: IMPORTANT! Unfinished! Active state, hover, check colors. Optimize transition. Maybe make faster. --}}
+
 @props(['name', 'id'])
 
 <input
     {{ $attributes->merge([
-        'class' => 'block w-full bg-navy-300 border-gray-400 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm',
+        'class' => 'block w-full bg-navy-300 border-gray-400 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm transition ease-in-out duration-150',
         'value' => old($name),
         'type' => 'text',
     ]) }}
