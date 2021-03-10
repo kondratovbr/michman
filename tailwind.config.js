c6a02fef2c35e5388dfd2e54d54afc805ab02c43
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-10 17:10:20 +0300

    Extend min-w helper classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,45 @@ const colors = require('tailwindcss/colors')
 const plugin = require('tailwindcss/plugin')
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/*
+ Custom smaller sizing scale for extending various utilities, like minWidth
+ */
+smallSizingScale = {
+    '0.5': '0.125rem',
+    '1': '0.25rem',
+    '1.5': '0.375rem',
+    '2': '0.5rem',
+    '2.5': '0.625rem',
+    '3': '0.75rem',
+    '3.5': '0.875rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '7': '1.75rem',
+    '8': '2rem',
+    '9': '2.25rem',
+    '10': '2.5rem',
+    '11': '2.75rem',
+    '12': '3rem',
+    '14': '3.5rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '28': '7rem',
+    '32': '8rem',
+    '36': '9rem',
+    '40': '10rem',
+    '44': '11rem',
+    '48': '12rem',
+    '52': '13rem',
+    '56': '14rem',
+    '60': '15rem',
+    '64': '16rem',
+    '72': '18rem',
+    '80': '20rem',
+    '96': '24rem',
+};
+
 module.exports = {
 
     purge: [
@@ -80,6 +119,8 @@ module.exports = {
                 'md-black': '0 4px 6px -1px rgba(1, 1, 1, 0.1), 0 2px 4px -1px rgba(1, 1, 1, 0.06)',
                 'md-gold-900': '0 4px 6px -1px rgba(255, 215, 0, 0.1), 0 2px 4px -1px rgba(255, 215, 0, 0.06)',
             },
+            // Extending min-width utilities using a custom sizing scale (see above).
+            minWidth: smallSizingScale,
         },
     },
 
