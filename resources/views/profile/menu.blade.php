9e161a14c33f48eee0c998e55f24d0fff668ca63
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-10 14:54:50 +0300

    Update side menu

--- resources/views/profile/menu.blade.php
+++ resources/views/profile/menu.blade.php
@@ -1,13 +1,15 @@
-<aside>
-    <ul class="space-y-2">
+{{--TODO: Unfinished. Need to change from hover to group-hover, and expand the interactive area so there's no gaps. Gaps are annoying. --}}
 
-        <x-menu.item>Item 1</x-menu.item>
+<aside>
+    <ul class="space-y-2 text-gray-200">
+
+        <x-menu.item>Profile</x-menu.item>
 
         <x-menu.item
-            class="bg-navy-300"
-        >Item 2</x-menu.item>
-        <x-menu.item>Item 3</x-menu.item>
-        <x-menu.item>Item 4</x-menu.item>
-        <x-menu.item>Item 5</x-menu.item>
+            class="bg-navy-300 text-gray-100"
+        >SSH Keys</x-menu.item>
+        <x-menu.item>Server Provider</x-menu.item>
+        <x-menu.item>Source Control</x-menu.item>
+        <x-menu.item>Michman API</x-menu.item>
     </ul>
 </aside>
