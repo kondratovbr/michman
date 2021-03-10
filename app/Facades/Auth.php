3fe3c112db375303aba63c1f01f9427244c5f89e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-10 14:53:18 +0300

    Add IDE-helper Auth Facade

---
+++ app/Facades/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Facades;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth as AuthFacade;
+
+/**
+ * Auth facade
+ *
+ * Exists only to provide some better info for an IDE.
+ *
+ * @method static User|null user()
+ */
+class Auth extends AuthFacade
+{
+    //
+}
