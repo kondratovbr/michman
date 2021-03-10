c11ce842d1ad5672e8ce7a1178b3e853587f1224
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-10 14:55:08 +0300

    Update page header

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+        <h2 class="font-semibold text-xl leading-tight">
+            {{ __('nav.account') }}
         </h2>
     </x-slot>
 
