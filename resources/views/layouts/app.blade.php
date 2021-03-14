369f9dfecb4f26202165887414e07d27402d7280
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-14 17:30:19 +0300

    Main layout update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,26 @@
     <div class="min-h-screen">
         <livewire:navbar/>
 
-        {{-- Page Heading --}}
-        @isset($header)
-            <header>
-                <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
+        {{-- Main Content Container --}}
+        <div class="container mx-auto pb-20 md:grid md:grid-cols-12">
 
-        {{-- Page Content --}}
-        <main>
-            {{ $slot }}
-        </main>
+            {{-- Page Heading --}}
+            @isset($header)
+                <header class="md:col-start-2 md:col-end-12">
+                    <div class="py-8 px-4 sm:px-6 lg:px-8">
+                        <div class="ml-1">
+                            {{ $header }}
+                        </div>
+                    </div>
+                </header>
+            @endisset
+
+            {{-- Page Content --}}
+            <main class="md:col-start-2 md:col-end-12">
+                {{ $slot }}
+            </main>
+
+        </div>
 
     </div>
 
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,19 +1,18 @@
 <x-layouts.app>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl leading-tight">
+        <h1 class="font-semibold text-xl leading-tight">
             {{ __('nav.account') }}
-        </h2>
+        </h1>
     </x-slot>
 
-    <div class="container mx-auto md:grid md:grid-cols-4 md:gap-6">
-        <div class="md:col-span-1 py-10 px-5">
+    <div class="md:grid md:grid-cols-12">
+        <div class="md:col-span-3 px-5">
             @include('profile.menu')
         </div>
-        <div class="md:col-span-3 max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="md:col-span-9 max-w-6xl mx-auto sm:px-6 lg:px-8">
 
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
-
                 <x-section-border/>
             @endif
 
@@ -21,7 +20,6 @@
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
                 </div>
-
                 <x-section-border/>
             @endif
 
@@ -29,7 +27,6 @@
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.two-factor-authentication-form')
                 </div>
-
                 <x-section-border/>
             @endif
 
@@ -39,11 +36,11 @@
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
                 <x-section-border/>
-
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.delete-user-form')
                 </div>
             @endif
         </div>
     </div>
+
 </x-layouts.app>
