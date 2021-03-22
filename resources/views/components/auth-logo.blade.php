0ad8d72058c1c5d811c69c0480ea2049b3bab2d3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-22 10:58:14 +0300

    Auth logo updated

--- resources/views/components/auth-logo.blade.php
+++ resources/views/components/auth-logo.blade.php
@@ -1,3 +1,8 @@
-<a href="{{ route('home') }}">
-    <x-logo class="w-16 h-16" />
+<a
+    class="group flex items-center p-3 rounded-md border border-gray-100 border-opacity-0 bg-navy-200 bg-opacity-0 hover:border-opacity-100 active:bg-opacity-100 focus:border-opacity-100 focus:outline-none transition-border-background ease-in-out duration-quick"
+    href="{{ route('home') }}"
+>
+    <div class="flex items-center transform group-hover:scale-110 group-focus:scale-110 transition-transform ease-in-out duration-quick">
+        <x-logo class="w-16 h-16" />
+    </div>
 </a>
