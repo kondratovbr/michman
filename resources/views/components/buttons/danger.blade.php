b5815670c787b00f5290940cedbe2980d1713e56
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-24 12:08:16 +0300

    Danger button bugfix

--- resources/views/components/buttons/danger.blade.php
+++ resources/views/components/buttons/danger.blade.php
@@ -4,7 +4,7 @@
         'hover:bg-red-700',
         'active:bg-red-800',
         'focus:bg-red-700 focus:ring-red-500',
-        'disabled:bg-gold-600',
+        'disabled:bg-red-600',
         'transition-ring-background',
     ]),
     'type' => 'button',
