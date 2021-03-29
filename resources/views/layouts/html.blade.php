fcc5adcc574110942f8a2e87424553f9bea2cd40
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-29 09:25:19 +0300

    Layout update

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -32,7 +32,7 @@
         <meta name="description" content="">
 {{--        TODO: IMPORTANT! Add SEO and misc metas. Don't forget to fill out these "canonical" and "description"! Also, favicons! --}}
     </head>
-    <body class="font-sans antialiased text-gray-100 bg-navy-100 {{ isDebug() ? 'debug-screens' : null }} w-screen">
+    <body class="font-sans antialiased w-screen overflow-x-hidden text-gray-100 bg-navy-100 {{ isDebug() ? 'debug-screens' : null }}">
 
         {{ $slot }}
 
