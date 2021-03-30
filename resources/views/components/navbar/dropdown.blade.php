6f63f02afb9c78ca9ec3f8e0f0c3497658fffea7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-30 09:50:59 +0300

    Comments and minor fixes

--- resources/views/components/navbar/dropdown.blade.php
+++ resources/views/components/navbar/dropdown.blade.php
@@ -5,13 +5,17 @@
 <div
     class="relative"
     x-data="{ open: false }"
+    {{-- TODO: IMPORTANT! Does this work on touch? --}}
     x-on:click.away="open = false"
     x-on:close.stop="open = false"
 >
-{{--If a button is used here - in Safari the child div (inside the button) doesn't stretch vertically by any means. A bug in safari.--}}
+
+    {{-- Dropdown button --}}
+{{--If a <button> is used here - in Safari the child div (inside the button) doesn't stretch vertically by any means. A bug in safari.--}}
     <a
         class="group py-2 h-full w-full flex items-stretch cursor-pointer focus:outline-none"
-        x-on:click="open = !open"
+        {{-- TODO: IMPORTANT! Does this work on touch? --}}
+        x-on:click.prevent="open = !open"
         role="button"
     >
         <div
@@ -25,19 +29,21 @@
         </div>
     </a>
 
+    {{-- Container for the menu --}}
     <div
+        class="absolute z-50 -mt-1 {{ $widthClass }} rounded-md border border-gray-600 shadow-lg {{ $alignmentClasses }}"
         x-show="open"
-        style="display: none;"
+        x-cloak
         x-transition:enter="transition ease-out duration-200"
         x-transition:enter-start="transform opacity-0 scale-95"
         x-transition:enter-end="transform opacity-100 scale-100"
         x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="absolute z-50 -mt-1 {{ $widthClass }} rounded-md border border-gray-600 shadow-lg {{ $alignmentClasses }}"
     >
         <div class="rounded-md py-2 bg-navy-300">
             {{ $content }}
         </div>
     </div>
+
 </div>
