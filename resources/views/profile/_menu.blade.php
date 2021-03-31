99e9b961ee37052988bc9534b1f68abcc322f8c8
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-31 17:10:35 +0300

    Comments

--- resources/views/profile/_menu.blade.php
+++ resources/views/profile/_menu.blade.php
@@ -1,7 +1,4 @@
-{{--TODO: IMPORTANT! Unfinished. Need to change from hover to group-hover, and expand the interactive area so there's no gaps. Gaps are annoying.
-        Also, probably make it narrower and put closer to the center.
-        Also, figure out smaller screens layout.
-        Also, add icons. --}}
+{{--TODO: Maybe make it narrower on bigger screens (xl and 2xl). Looks too separated from the main content.--}}
 {{--TODO: Figure out automatic active section highlight.--}}
 {{--TODO: IMPORTANT! This menu is supposed to be a part of a big Livewire component (Maybe only Alpine?), so the page doesn't reload when these sections are changed.--}}
 {{--TODO: Active link shouldn't hover?--}}
