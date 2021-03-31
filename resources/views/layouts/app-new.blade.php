ec8f42a9ad10a3cc6b21c38ad10ab154861c7014
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-03-31 17:06:06 +0300

    Comments

--- resources/views/layouts/app-new.blade.php
+++ resources/views/layouts/app-new.blade.php
@@ -13,10 +13,9 @@
         It is placed here so it loads before the rest of the page. --}}
         <livewire:bottombar/>
 
-        {{-- Main Layout Container --}}
+        {{-- Main layout container --}}
         <div class="container mx-auto pb-20 md:grid md:grid-cols-12">
-            {{-- Responsiveness Containment --}}
-{{--            TODO: IMPORTANT! Unfinished. Figure out responsiveness and mobile.--}}
+            {{-- Responsiveness containment --}}
             <div class="md:col-span-12 xl:col-start-2 xl:col-end-12">
 
                 {{ $slot }}
