e560adc11f016e90d8ac1ed3e23ecce2a202d042
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-01 10:23:46 +0300

    Main sections responsiveness

--- resources/views/components/action-section.blade.php
+++ resources/views/components/action-section.blade.php
@@ -1,14 +1,16 @@
-<div class="md:grid md:grid-cols-3 md:gap-6" {{ $attributes }}>
+<div {{ $attributes->merge([
+    'class' => 'lg:grid lg:grid-cols-3 lg:gap-6',
+]) }}>
 
-    <x-box class="mt-5 md:mt-0 md:col-span-2">
+    <x-section-title class="lg:order-2 lg:col-span-1">
+        <x-slot name="title">{{ $title }}</x-slot>
+        <x-slot name="description">{{ $description }}</x-slot>
+    </x-section-title>
+
+    <x-box class="lg:order-1 lg:col-span-2 mt-5 lg:mt-0">
         <x-slot name="content">
             {{ $content }}
         </x-slot>
     </x-box>
-
-    <x-section-title>
-        <x-slot name="title">{{ $title }}</x-slot>
-        <x-slot name="description">{{ $description }}</x-slot>
-    </x-section-title>
 
 </div>
--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,8 +1,15 @@
 @props(['submit'])
 
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+<div {{ $attributes->merge([
+    'class' => 'lg:grid lg:grid-cols-3 lg:gap-6',
+]) }}>
 
-    <x-box class="mt-5 md:mt-0 md:col-span-2">
+    <x-section-title class="lg:order-2 lg:col-span-1">
+        <x-slot name="title">{{ $title }}</x-slot>
+        <x-slot name="description">{{ $description }}</x-slot>
+    </x-section-title>
+
+    <x-box class="lg:order-1 lg:col-span-2 mt-5 lg:mt-0">
         <x-box.form wire:submit.prevent="{{ $submit }}">
 
             @isset($header)
@@ -19,10 +26,5 @@
 
         </x-box.form>
     </x-box>
-
-    <x-section-title>
-        <x-slot name="title">{{ $title }}</x-slot>
-        <x-slot name="description">{{ $description }}</x-slot>
-    </x-section-title>
 
 </div>
