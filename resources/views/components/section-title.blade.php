3aacf3287f7c6368a7588d5909e6fb8ebde6d352
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-01 11:32:52 +0300

    Section layout bugfix

--- resources/views/components/section-title.blade.php
+++ resources/views/components/section-title.blade.php
@@ -1,6 +1,6 @@
 {{--TODO: IMPORTANT! How I use sections right now this part ends up at the bottom of the section on mobile. Should fix somehow. --}}
 
-<div>
+<div {{ $attributes }}>
     <div class="px-4 sm:px-0">
         <h3 class="text-lg font-medium text-gray-100">{{ $title }}</h3>
 
