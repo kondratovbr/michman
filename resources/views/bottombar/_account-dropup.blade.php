e8df5fb9c6af5149c7ab978836ccaff1a51702e0
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-02 15:37:15 +0300

    Update _account-dropup.blade.php

--- resources/views/bottombar/_account-dropup.blade.php
+++ resources/views/bottombar/_account-dropup.blade.php
@@ -4,30 +4,36 @@
 
     <x-dropdown.back-button/>
 
-    <x-dropdown.link href="{{ route('account.show') }}">
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'profile']) }}">
         <x-slot name="icon"><i class="far fa-user"></i></x-slot>
         {{ __('account.profile.button') }}
     </x-dropdown.link>
 
-    <x-dropdown.link href="">
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'ssh-keys']) }}">
         <x-slot name="icon"><i class="fa fa-terminal"></i></x-slot>
         {{ __('account.ssh.button') }}
     </x-dropdown.link>
 
-    <x-dropdown.link href="">
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'providers']) }}">
         <x-slot name="icon"><i class="fa fa-server"></i></x-slot>
         {{ __('account.providers.button') }}
     </x-dropdown.link>
 
-    <x-dropdown.link href="">
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'vcs']) }}">
         <x-slot name="icon"><i class="fa fa-code-branch"></i></x-slot>
         {{ __('account.vcs.button') }}
     </x-dropdown.link>
 
-    <x-dropdown.link href="">
-    {{--    TODO: Maybe put a Michman logo or some other icon here.--}}
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'api']) }}">
+{{--        TODO: Maybe put a Michman logo or some other icon here.--}}
         <x-slot name="icon"><i class="fa fa-ship"></i></x-slot>
         {{ __('account.api.button') }}
+    </x-dropdown.link>
+
+{{--    TODO: CRITICAL! Don't forget to remove this!--}}
+    <x-dropdown.link href="{{ route('account.show', ['show' => 'foobar']) }}">
+        <x-slot name="icon"><i class="fa fa-poo"></i></x-slot>
+        Foobar
     </x-dropdown.link>
 
 </x-dropdown.menu>
