7ab4ed045689a5517b060204c20ddc755bdef051
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-06 14:07:11 +0300

    Update radio-card.blade.php

--- resources/views/components/radio-card.blade.php
+++ resources/views/components/radio-card.blade.php
@@ -14,12 +14,20 @@
     >
 
     <label
-        class="absolute inset-0 border rounded-lg px-6 py-4 bg-opacity-0 hover:bg-navy-400 hover:bg-opacity-100 sibling-hover:bg-navy-400 sibling-hover:bg-opacity-100 active:bg-navy-500 active:bg-opacity-100 input-checked:border-gold-800 input-checked:ring-2 input-checked:ring-inset input-checked:ring-gold-800 input-checked:shadow-lg cursor-pointer select-none transition-all ease-in-out duration-quick"
+        class="{{ classes(
+            'absolute inset-0 cursor-pointer select-none',
+            'px-6 py-4',
+            'border rounded-lg bg-opacity-0 ring-2 ring-inset ring-gold-800 ring-opacity-0',
+            'hover:bg-navy-400 hover:bg-opacity-100',
+            'active:bg-navy-500 active:bg-opacity-100',
+            'input-checked:border-gold-800 input-checked:ring-opacity-100 input-checked:shadow-lg',
+            'transition-ring-background ease-in-out duration-quick',
+        ) }}"
         for="{{ $name . '-' . $value }}"
     >
 
         @isset($content)
-            <div class="w-full h-full flex flex-col items-center justify-center space-y-2">
+            <div class="w-full h-full flex flex-col items-center justify-center">
                 {{ $content }}
             </div>
         @endisset
