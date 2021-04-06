8ab6e974ebf4ca3e006231c335ddf53224de763c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-06 14:07:23 +0300

    Update server provider create form

--- resources/views/providers/create-form.blade.php
+++ resources/views/providers/create-form.blade.php
@@ -18,40 +18,24 @@
 
                 <x-radio-card name="provider" value="aws" checked>
                     <x-slot name="content">
-                        <x-icon size="12"><i class="fab fa-aws fa-3x"></i></x-icon>
-                        <span>AWS</span>
+                        <x-icon size="16"><i class="fab fa-aws fa-3x"></i></x-icon>
+                        <span class="mt-1">AWS</span>
                     </x-slot>
                 </x-radio-card>
 
                 <x-radio-card name="provider" value="digital-ocean" checked>
                     <x-slot name="content">
-                        <x-icon size="16"><i class="fab fa-digital-ocean fa-4x"></i></x-icon>
-                        <span>DigitalOcean</span>
+                        <x-icon size="16"><i class="fab fa-digital-ocean fa-3x"></i></x-icon>
+                        <span class="mt-1">DigitalOcean</span>
                     </x-slot>
                 </x-radio-card>
 
                 <x-radio-card name="provider" value="linode">
                     <x-slot name="content">
-                        <x-icon size="16"><i class="fab fa-linode fa-4x"></i></x-icon>
-                        <span>Linode</span>
+                        <x-icon size="16"><i class="fab fa-linode fa-3x"></i></x-icon>
+                        <span class="mt-1">Linode</span>
                     </x-slot>
                 </x-radio-card>
-
-                {{--
-                <div class="w-32 border rounded-lg shadow-md px-6 py-4 cursor-pointer">
-                    <div class="flex flex-col items-center space-y-2">
-                        <x-icon size="16"><i class="fab fa-linode fa-4x"></i></x-icon>
-                        <span>Linode</span>
-                    </div>
-                </div>
-
-                <div class="w-32 border rounded-lg shadow-md px-6 py-4 cursor-pointer">
-                    <div class="flex flex-col items-center space-y-2">
-                        <x-icon size="16"><i class="fab fa-aws fa-4x"></i></x-icon>
-                        <span>AWS</span>
-                    </div>
-                </div>
-                --}}
 
             </div>
 
