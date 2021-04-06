371df7f96242538e6dc5111ceb9df2851dbd7bbf
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-06 13:47:40 +0300

    Update checkbox styling

--- resources/views/components/checkbox-new.blade.php
+++ resources/views/components/checkbox-new.blade.php
@@ -1,12 +1,12 @@
 @props(['id', 'name', 'defaultState', 'labelClass' => ''])
 
 <label
-    class="group -ml-2 py-1 px-2 inline-flex items-center cursor-pointer select-none rounded border border-gray-300 border-opacity-0 bg-navy-400 bg-opacity-0 hover:border-opacity-100 hover:text-gray-100 active:bg-opacity-100 transition-border-background ease-in-out duration-quick"
+    class="group -ml-2 py-1 px-2 inline-flex items-center cursor-pointer select-none rounded border border-gray-300 border-opacity-0 bg-navy-400 bg-opacity-0 hover:border-opacity-100 hover:text-gray-100 active:bg-opacity-100 focus:outline-none transition-border-background ease-in-out duration-quick"
     for="{{ $id ?? $name }}"
 >
     <input
         type="checkbox"
-        class="checkbox cursor-pointer rounded border-gray-300 border-opacity-100 bg-transparent tick-gray-900 checked-bg-gold-800 checked-border-opacity-0"
+        class="checkbox cursor-pointer rounded border-gray-300 border-opacity-100 bg-transparent checked-bg-gold-800 tick-gray-900 checked-border-opacity-0 focus:outline-none"
         id="{{ $id ?? $name }}"
         name="{{ $name }}"
         @if(old($name, $defaultState) === 'on')
