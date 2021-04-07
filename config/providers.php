33a38bb3be9139fec20b4590ea81b7339f460381
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-07 14:09:41 +0300

    Update providers config

--- config/providers.php
+++ config/providers.php
@@ -6,13 +6,31 @@
 
 return [
 
-    'digital_ocean_v2' => [
-        'base_path' => 'https://api.digitalocean.com/v2',
-        'form_type' => 'token',
-    ],
+    'default' => 'digital_ocean_v2',
 
-    'linode' => [
+    'list' => [
 
-    ],
+        'digital_ocean_v2' => [
+            'base_path' => 'https://api.digitalocean.com/v2',
+            'auth_type' => 'token',
+            'icon' => 'fab fa-digital-ocean',
+            'disabled' => false,
+        ],
+
+        'aws' => [
+            'base_path' => '',
+            'auth_type' => 'key-secret',
+            'icon' => 'fab fa-aws',
+            'disabled' => true,
+        ],
+
+        'linode' => [
+            'base_path' => '',
+            'auth_type' => 'token',
+            'icon' => 'fab fa-linode',
+            'disabled' => true,
+        ],
+
+    ]
 
 ];
