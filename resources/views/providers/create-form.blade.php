da20af4650da6317da48d4a7cd6546837230f2e5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-07 11:17:52 +0300

    Refactor provider creation form

--- resources/views/providers/create-form.blade.php
+++ resources/views/providers/create-form.blade.php
@@ -43,24 +43,7 @@
                 </div>
             </x-field>
 
-            <div class="space-y-4">
-
-                <x-message colors="info">
-                    <x-lang key="providers.digital-ocean-info" />
-                </x-message>
-
-                <x-field>
-                    <x-label>{{ __('account.providers.token.label') }}</x-label>
-                    <x-inputs.text name="token" />
-                </x-field>
-
-                <x-field>
-                    <x-label>{{ __('account.providers.name.label') }}</x-label>
-                    <x-inputs.text name="name" />
-                    <x-help>{{ __('account.providers.name.help') }}</x-help>
-                </x-field>
-
-            </div>
+            @include('providers._token-form')
 
         </div>
     </x-slot>
