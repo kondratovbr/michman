f70ec719e5a6bfe83831e491ff15838881175eee
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-07 14:10:12 +0300

    Update provider creation form

--- app/Http/Livewire/Providers/CreateForm.php
+++ app/Http/Livewire/Providers/CreateForm.php
@@ -7,12 +7,20 @@ use Livewire\Component;
 
 class CreateForm extends Component
 {
+    public string $provider = 'digital_ocean_v2';
+    public string $token = '';
+    public string $key = '';
+    public string $secret = '';
+    public string $name = '';
+
     /**
      * Store a new server provider credentials.
      */
     public function store(): void
     {
         //
+        // This event is used to show the success message.
+        $this->emit('saved');
     }
 
     /**
--- resources/views/providers/create-form.blade.php
+++ resources/views/providers/create-form.blade.php
@@ -11,7 +11,10 @@
     </x-slot>
 
     <x-slot name="form">
-        <div class="space-y-6">
+        <div
+            class="space-y-6"
+            x-data="{ formType: 'token', provider: '{{ config('providers.default') }}' }"
+        >
 
             <x-field class="space-y-2">
                 <div>{{ __('account.providers.provider.label') }}</div>
@@ -19,32 +22,82 @@
                 {{-- Check-cards container --}}
                 <div class="flex space-x-6">
 
-                    <x-radio-card class="h-32 w-32" name="provider" value="digital-ocean" checked>
-                        <x-slot name="content">
-                            <x-icon size="16"><i class="fab fa-digital-ocean fa-3x"></i></x-icon>
-                            <span class="mt-1">DigitalOcean</span>
-                        </x-slot>
-                    </x-radio-card>
-
-                    <x-radio-card class="h-32 w-32" name="provider" value="aws">
-                        <x-slot name="content">
-                            <x-icon size="16"><i class="fab fa-aws fa-3x"></i></x-icon>
-                            <span class="mt-1">AWS</span>
-                        </x-slot>
-                    </x-radio-card>
-
-                    <x-radio-card class="h-32 w-32" name="provider" value="linode">
-                        <x-slot name="content">
-                            <x-icon size="16"><i class="fab fa-linode fa-3x"></i></x-icon>
-                            <span class="mt-1">Linode</span>
-                        </x-slot>
-                    </x-radio-card>
+                    @foreach(config('providers.list') as $providerName => $providerConfig)
+                        <x-radio-card
+                            class="h-32 w-32"
+                            name="provider"
+                            value="{{ $providerName }}"
+                            wire:model="provider"
+                            x-model="provider"
+{{--                            :disabled="(bool) $providerConfig['disabled']"--}}
+                            x-on:click="formType = '{{ $providerConfig['auth_type'] }}'"
+                        >
+                            <x-slot name="content">
+                                <x-icon size="16"><i class="{{ $providerConfig['icon'] }} fa-3x"></i></x-icon>
+                                <span class="mt-1">{{ __('account.providers.' . $providerName . '.name') }}</span>
+                            </x-slot>
+                        </x-radio-card>
+                    @endforeach
 
                 </div>
             </x-field>
 
-            @include('providers._token-form')
+            <div class="space-y-4">
 
+{{--                TODO: Don't forget to add similar helpful messages for other providers.--}}
+{{--                TODO: Is it possible to DRY this ridiculous Alpine transition declaration?--}}
+                <x-message
+                    class="max-w-prose"
+                    colors="info"
+                    x-show.transition.in.duration.300ms.origin.top.opacity.scale.95="provider === 'digital_ocean_v2'"
+                    x-cloak
+                >
+                    <x-lang key="providers.digital-ocean-info" />
+                </x-message>
+
+                <x-field
+                    x-show.transition.in.duration.300ms.origin.top.opacity.scale.95="formType === 'token'"
+                    x-cloak
+                >
+                    <x-label>{{ __('account.providers.token.label') }}</x-label>
+                    <x-inputs.text
+                        name="token"
+                        wire:model.defer="token"
+                    />
+                </x-field>
+
+                <x-field
+                    x-show.transition.in.duration.300ms.origin.top.opacity.scale.95="formType === 'key-secret'"
+                    x-cloak
+                >
+                    <x-label>{{ __('account.providers.key.label') }}</x-label>
+                    <x-inputs.text
+                        name="key"
+                        wire:model.defer="key"
+                    />
+                </x-field>
+
+                <x-field
+                    x-show.transition.in.duration.300ms.origin.top.opacity.scale.95="formType === 'key-secret'"
+                    x-cloak
+                >
+                    <x-label>{{ __('account.providers.secret.label') }}</x-label>
+                    <x-inputs.text
+                        name="secret"
+                        wire:model.defer="secret"
+                    />
+                </x-field>
+
+                <x-field>
+                    <x-label>{{ __('account.providers.name.label') }}</x-label>
+                    <x-inputs.text
+                        name="name"
+                        wire:model.defer="name"
+                    />
+                    <x-help>{{ __('account.providers.name.help') }}</x-help>
+                </x-field>
+
+            </div>
         </div>
     </x-slot>
 
