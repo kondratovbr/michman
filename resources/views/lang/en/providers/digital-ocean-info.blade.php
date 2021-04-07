f386655ffe41fccf6e675ddd8f520f67ea5172a4
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-07 11:01:20 +0300

    Lang fix

--- resources/views/lang/en/providers/digital-ocean-info.blade.php
+++ resources/views/lang/en/providers/digital-ocean-info.blade.php
@@ -1,5 +1,5 @@
 <p>
-    Go to
+    Go to the
     <x-link href="https://cloud.digitalocean.com/settings/api/tokens">DigitalOcean API Panel</x-link>
     to create a new DigitalOcean API token.
 </p>
