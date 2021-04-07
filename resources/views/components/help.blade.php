0821072b2fe77d59e4d25c47abeac09d1db04e40
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-07 11:21:21 +0300

    Refactor input help component

--- resources/views/components/help.blade.php
+++ resources/views/components/help.blade.php
@@ -1,3 +1,3 @@
-<p {{ $attributes->merge([
-    'class' => 'block mt-1.5 max-w-prose text-sm',
+<p {{ $attributes->class([
+    'block mt-1.5 max-w-prose text-sm leading-normal'
 ]) }}>{{ $slot }}</p>
