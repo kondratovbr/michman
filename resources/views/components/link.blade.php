23752cddf599a6e8ac435066e251915e8d7395a0
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-09 14:11:48 +0300

    Add an option to have an external link with the appropriate icon

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,17 @@
-<a {{ $attributes->merge([
-    'class' => 'cursor-pointer underline text-gray-300 hover:text-gray-200 active:text-gray-100 transition-text ease-in-out duration-quick',
-]) }}>{{ $slot }}</a>
+@props(['external' => false, 'icon' => true])
+
+<a {{ $attributes->class([
+    'cursor-pointer underline text-gray-300',
+    'hover:text-gray-200',
+    'active:text-gray-100',
+    'transition-text ease-in-out duration-quick',
+]) }}
+    @if($external)
+        target="_blank"
+    @endif
+>
+    {{ $slot }}
+    @if($external && $icon)
+        <x-icon><i class="fas fa-external-link-alt"></i></x-icon>
+    @endif
+</a>
