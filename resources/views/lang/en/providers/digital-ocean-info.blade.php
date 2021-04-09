c09e858ba2368806b8e34578d883e3018435bd24
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-09 14:12:10 +0300

    Update digital-ocean-info.blade.php

--- resources/views/lang/en/providers/digital-ocean-info.blade.php
+++ resources/views/lang/en/providers/digital-ocean-info.blade.php
@@ -1,5 +1,9 @@
 <p>
     Go to the
-    <x-link href="https://cloud.digitalocean.com/settings/api/tokens">DigitalOcean API Panel</x-link>
+    <x-link
+        href="https://cloud.digitalocean.com/settings/api/tokens"
+        :external="true"
+        :icon="false"
+    >DigitalOcean API Panel</x-link>
     to create a new DigitalOcean API token.
 </p>
