82619320378c4973141c5d1a3cb190397d973684
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-12 13:43:34 +0300

    Update AbstractFeatureTest.php

--- tests/AbstractFeatureTest.php
+++ tests/AbstractFeatureTest.php
@@ -4,8 +4,9 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class AbstractFeatureTest extends TestCase
 {
-    use CreatesApplication, RefreshDatabase;
+    use CreatesApplication, RefreshDatabase, WithFaker;
 }
