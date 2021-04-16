70dd1466c1b5eeedf7baa302d97c05a1e4f39ef4
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-16 12:09:33 +0300

    Update Server model

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -2,13 +2,42 @@
 
 namespace App\Models;
 
+use Carbon\CarbonInterface;
+use Database\Factories\ProviderFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Server Eloquent model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $ip
+ * @property CarbonInterface $updated_at
+ * @property CarbonInterface $created_at
+ *
+ * @property-read Provider $provider
+ *
+ * @method static ProviderFactory factory(...$parameters)
  */
 class Server extends AbstractModel
 {
     use HasFactory;
+
+    /** @var string[] The attributes that are mass assignable. */
+    protected $fillable = [
+        'name',
+        'ip',
+    ];
+
+    /** @var string[] The attributes that should be visible in arrays and JSON. */
+    protected $visible = [];
+
+    /**
+     * Get a relation to the provider that runs this server.
+     */
+    public function provider(): BelongsTo
+    {
+        return $this->belongsTo(Provider::class);
+    }
 }
