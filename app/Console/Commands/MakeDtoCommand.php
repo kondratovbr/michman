769d511ba69d8de035ba4db226cdd33d2e723e70
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-16 12:08:58 +0300

    Create make:dto artisan command

---
+++ app/Console/Commands/MakeDtoCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeDtoCommand extends GeneratorCommand
+{
+    protected $type = 'Data transfer object';
+
+    /** @var string The name and signature of the console command. */
+    protected $signature = 'make:dto {name}';
+
+    /** @var string The console command description. */
+    protected $description = 'Create a new data transfer object class';
+
+    /**
+     * Get the stub file for the generator.
+     */
+    protected function getStub(): string
+    {
+        return base_path() . '/stubs/dto.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\DataTransferObjects';
+    }
+}
