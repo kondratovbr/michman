f140940811e2678e6edbc29ad845759a48a4e879
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-16 15:24:21 +0300

    Servers-related lang strings

---
+++ resources/lang/en/servers.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'index' => [
+
+        'title' => 'Servers',
+
+        'table' => [
+            'server' => 'Server',
+            'ip' => 'IP Address',
+        ],
+
+    ],
+
+    'create' => [
+
+        'title' => 'Provision New Server',
+
+    ],
+
+];
