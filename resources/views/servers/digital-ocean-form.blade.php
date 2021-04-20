a48903e8018d2c8e02f9ddf43a8b169a355e0e23
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-20 16:57:08 +0300

    Update digital-ocean-form.blade.php

--- resources/views/servers/digital-ocean-form.blade.php
+++ resources/views/servers/digital-ocean-form.blade.php
@@ -1,10 +1,21 @@
 <div class="space-y-6">
 
     <x-field>
-        <x-label>Server Size</x-label>
-        <x-search-select></x-search-select>
+        <x-label>API Credentials</x-label>
+        <x-select/>
     </x-field>
 
+    <x-field>
+        <x-label>Type</x-label>
+        <x-search-select/>
+    </x-field>
+
+    <x-field>
+        <x-label>Server Size</x-label>
+        <x-search-select/>
+    </x-field>
+
+    {{-- TODO: Don't forget to add an explanation here. Not everyone knows where the name will be used and even WTF is it. --}}
     <x-field>
         <x-label>Name</x-label>
         <x-inputs.text name="name"/>
