cd6dde4ec5f7b773141b20bd1b9a3145fa154520
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-20 16:56:56 +0300

    Implement a standard (non-searchable) select input component

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,6 @@ window.focusableDialog = focusableDialog;
 
 import searchSelect from './components/SearchSelect';
 window.searchSelect = searchSelect;
+
+import select from './components/Select'
+window.select = select;
