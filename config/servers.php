417c872accfc8694b7d5a0d76d0056aa5fec7a63
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-20 16:57:24 +0300

    Create server config scaffolding

---
+++ config/servers.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    // A list of basic server types supported by this app
+    'types' => [
+        'app' => [
+            'disabled' => false,
+        ],
+        'web' => [
+            'disabled' => false,
+        ],
+        'worker' => [
+            'disabled' => true,
+        ],
+        'database' => [
+            'disabled' => true,
+        ],
+        'cache' => [
+            'disabled' => true,
+        ],
+    ],
+
+];
