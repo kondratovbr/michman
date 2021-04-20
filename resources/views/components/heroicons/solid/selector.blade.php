1bc6672aa52c7afe2b206fc6935b0cb141038f7b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-20 16:57:03 +0300

    Create selector.blade.php

---
+++ resources/views/components/heroicons/solid/selector.blade.php
@@ -0,0 +1,14 @@
+<svg
+    {{ $attributes->merge([
+        'viewBox' => '0 0 20 20',
+        'fill' => 'none',
+        'stroke' => 'currentColor',
+    ]) }}
+>
+    <path
+        d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></path>
+</svg>
