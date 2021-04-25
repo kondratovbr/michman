6eeef5b0705492a8544fc7da6ab2380d69fae4e2
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-25 19:45:27 +0300

    Update servers config

--- config/servers.php
+++ config/servers.php
@@ -2,22 +2,46 @@
 
 return [
 
-    // A list of basic server types supported by this app
+    /*
+     * A list of basic server types supported by this app
+     * with their default configuration.
+     */
     'types' => [
         'app' => [
             'disabled' => false,
+            'install' => [
+                'nginx',
+                'gunicorn',
+                'python',
+                'database',
+                'cache',
+            ],
         ],
         'web' => [
             'disabled' => false,
+            'install' => [
+                'nginx',
+                'gunicorn',
+                'python',
+            ],
         ],
         'worker' => [
             'disabled' => true,
+            'install' => [
+                'python',
+            ],
         ],
         'database' => [
             'disabled' => true,
+            'install' => [
+                'database',
+            ],
         ],
         'cache' => [
             'disabled' => true,
+            'install' => [
+                'cache',
+            ],
         ],
     ],
 
