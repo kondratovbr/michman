9704f42c8dd0513064113ed9dc534d7db65751bf
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-26 22:26:47 +0300

    Scaffold server creation implementation

--- app/Actions/Servers/StoreServerAction.php
+++ app/Actions/Servers/StoreServerAction.php
@@ -3,12 +3,22 @@
 namespace App\Actions\Servers;
 
 use App\DataTransferObjects\NewServerData;
+use App\Jobs\Servers\RequestNewServerFromProviderJob;
 use App\Models\Server;
+use Illuminate\Support\Facades\Bus;
 
 class StoreServerAction
 {
     public function execute(NewServerData $data): Server
     {
-        //
+        /** @var Server $server */
+        $server = $data->provider->servers()->create($data->toArray());
+
+        Bus::chain([
+            new RequestNewServerFromProviderJob($server),
+            //
+        ])->dispatch();
+
+        return $server;
     }
 }
---
+++ app/Jobs/Servers/RequestNewServerFromProviderJob.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Models\Server;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+/*
+ * TODO: CRITICAL! This job (and others interacting with third-party APIs should be:
+ *       1. Rate-limited
+ *       2. Able to backoff when the API is unresponsive
+ *       Laravel has built-in features for this - see docs.
+ */
+class RequestNewServerFromProviderJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Server $server;
+
+    public function __construct(Server $server)
+    {
+        $this->onQueue('servers');
+
+        $this->server = $server->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
