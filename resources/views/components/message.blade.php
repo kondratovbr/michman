e560d61275d713e2f5f5355728ba2a6ea20e9843
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-28 15:32:45 +0300

    Add more color options

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,10 +1,14 @@
-@props(['colors' => 'info'])
+@props(['colors'])
 
 <div {{ $attributes->class([
     'py-5 px-6 rounded-lg',
-    match ($colors) {
-        'info' => 'bg-navy-500 text-blue-100',
-        'danger' => 'bg-red-700 text-red-100',
+    match ($colors ?? null) {
+        'info'      => 'bg-navy-500 text-blue-100',
+        'success'   => 'bg-green-800 text-green-100',
+        'warning'   => 'bg-yellow-400 text-yellow-900',
+        'danger'    => 'bg-red-700 text-red-100',
+        'none'      => '',
+         default    => 'bg-gray-700 text-gray-100',
     },
 ]) }}>
     {{ $slot }}
