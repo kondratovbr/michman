b7ab3fa63473e01de765e5fbd3d25ccad0cff754
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-28 17:17:28 +0300

    Update help.blade.php

--- resources/views/components/help.blade.php
+++ resources/views/components/help.blade.php
@@ -1,3 +1,6 @@
+@props(['margins' => true])
+
 <p {{ $attributes->class([
-    'block mt-1.5 max-w-prose text-sm leading-normal'
+    'block max-w-prose text-sm leading-normal',
+    'mt-1.5' => $margins,
 ]) }}>{{ $slot }}</p>
