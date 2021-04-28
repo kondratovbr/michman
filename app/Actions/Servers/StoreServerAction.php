686dbabd1ba86afbeba776c245ae2dc963496741
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-28 22:08:08 +0300

    Server storing logic WIP

--- app/Actions/Servers/StoreServerAction.php
+++ app/Actions/Servers/StoreServerAction.php
@@ -2,17 +2,32 @@
 
 namespace App\Actions\Servers;
 
+use App\Actions\WorkerSshKeys\CreateWorkerSshKeyAction;
 use App\DataTransferObjects\NewServerData;
 use App\Jobs\Servers\RequestNewServerFromProviderJob;
 use App\Models\Server;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
 
 class StoreServerAction
 {
+    public function __construct(
+        protected CreateWorkerSshKeyAction $createWorkerSshKeyAction
+    ) {}
+
     public function execute(NewServerData $data): Server
     {
+        // TODO: CRITICAL! CONTINUE!
+
+        DB::beginTransaction();
+
         /** @var Server $server */
-        $server = $data->provider->servers()->create($data->toArray());
+        $server = $data->provider->servers()->make($data->toArray());
+
+        // TODO: Move this to a job?
+        $this->createWorkerSshKeyAction->execute($server);
+
+        DB::commit();
 
         Bus::chain([
             new RequestNewServerFromProviderJob($server, $data),
