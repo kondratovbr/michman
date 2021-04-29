0638fabea3270c7a28ee3bc40d6ce685f279d27b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-04-29 12:47:47 +0300

    Create GetServerPublicIpJob.php

---
+++ app/Jobs/Servers/GetServerPublicIpJob.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Models\Server;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class GetServerPublicIpJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Server $server;
+
+    public function __construct(Server $server)
+    {
+        $this->onQueue('servers');
+
+        $this->server = $server->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            $ip = $server->provider->api()->getServerPublicIp4($server->externalId);
+
+            // TODO: CRITICAL! CONTINUE! Figure out how to re-run the job after some timeout if the IP isn't issued yet.
+        }, 5);
+    }
+}
