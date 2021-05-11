01d25e2f7a52538508174f30f41acd460501a950
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-11 16:54:58 +0300

    Update WorkerSshKeyFactory.php

--- database/factories/WorkerSshKeyFactory.php
+++ database/factories/WorkerSshKeyFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\WorkerSshKey;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use phpseclib3\Crypt\EC;
 
 class WorkerSshKeyFactory extends Factory
 {
@@ -15,8 +16,15 @@ class WorkerSshKeyFactory extends Factory
      */
     public function definition(): array
     {
+        $key = EC::createKey('Ed25519');
+
+        // TODO: CRITICAL! CONTINUE! Doesn't work - tries to access ->server->name when there's no server assigned yet.
+
         return [
-            //
+            'private_key' => $key,
+            'public_key' => $key->getPublicKey(),
+            'name' => $this->faker->domainName,
+            'external_id' => null,
         ];
     }
 }
