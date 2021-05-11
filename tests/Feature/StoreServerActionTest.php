15a18e702128caea8726953547f5f24d77cc0a29
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-11 13:19:42 +0300

    Create StoreServerActionTest.php

---
+++ tests/Feature/StoreServerActionTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\Servers\StoreServerAction;
+use App\DataTransferObjects\NewServerData;
+use App\Jobs\Providers\AddServerSshKeyToProviderJob;
+use App\Jobs\Servers\CreateWorkerSshKeyForServerJob;
+use App\Jobs\Servers\GetServerPublicIpJob;
+use App\Jobs\Servers\PrepareRemoteServerJob;
+use App\Jobs\Servers\RequestNewServerFromProviderJob;
+use App\Jobs\Servers\VerifyRemoteServerIsSuitableJob;
+use App\Models\Provider;
+use App\Models\Server;
+use App\Models\User;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Bus;
+use Tests\AbstractFeatureTest;
+
+class StoreServerActionTest extends AbstractFeatureTest
+{
+    public function test_server_gets_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        /** @var StoreServerAction $action */
+        $action = App::make(StoreServerAction::class);
+
+        $data = new NewServerData(
+            provider: $provider,
+            name: 'Server Name',
+            region: 'region_1',
+            size: 'size_1',
+            type: 'app',
+            pythonVersion: '3_9',
+            database: 'mysql-8_0',
+            dbName: 'app_db',
+            cache: 'redis',
+            addSshKeyToVcs: true,
+        );
+
+        Bus::fake();
+
+        $server = $action->execute($data);
+
+        $this->assertNotNull($server);
+        $this->assertEquals($provider->id, $server->provider->id);
+        $this->assertEquals('Server Name', $server->name);
+        $this->assertEquals('app', $server->type);
+        $this->assertNull($server->externalId);
+        $this->assertNull($server->publicIp);
+        $this->assertNull($server->sshHostKey);
+        $this->assertEquals('22', $server->sshPort);
+        $this->assertCount(1, $provider->servers);
+        $this->assertDatabaseHas(Server::tableName(), [
+            'provider_id' => $provider->id,
+            'name' => 'Server Name',
+            'type' => 'app',
+            'external_id' => null,
+            'public_ip' => null,
+            'ssh_port' => '22',
+            'ssh_host_key' => null,
+        ]);
+
+        Bus::assertChained([
+            CreateWorkerSshKeyForServerJob::class,
+            AddServerSshKeyToProviderJob::class,
+            RequestNewServerFromProviderJob::class,
+            GetServerPublicIpJob::class,
+            VerifyRemoteServerIsSuitableJob::class,
+            PrepareRemoteServerJob::class,
+        ]);
+    }
+}
