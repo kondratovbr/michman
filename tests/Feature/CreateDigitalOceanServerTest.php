be943af91f5b3e92516af7a559f6d3524f9c360e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-11 12:45:28 +0300

    Create CreateDigitalOceanServerTest.php

---
+++ tests/Feature/CreateDigitalOceanServerTest.php
@@ -0,0 +1,389 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\Servers\StoreServerAction;
+use App\DataTransferObjects\NewServerData;
+use App\Http\Livewire\Servers\DigitalOceanForm;
+use App\Models\Provider;
+use App\Models\Server;
+use App\Models\User;
+use App\Policies\ServerPolicy;
+use App\Support\Str;
+use Livewire\Livewire;
+use Tests\AbstractFeatureTest;
+use Mockery;
+use Mockery\MockInterface;
+
+class CreateDigitalOceanServerTest extends AbstractFeatureTest
+{
+    public function test_server_can_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $serverName = Str::random();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => $serverName,
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $serverData = new NewServerData(
+            provider: $provider,
+            name: $serverName,
+            region: 'nyc_1',
+            size: 'size_1',
+            type: 'app',
+            pythonVersion: '3_9',
+            database: 'mysql-8_0',
+            dbName: 'app_db',
+            cache: 'redis',
+            addSshKeyToVcs: true,
+        );
+
+        $this->actingAs($user);
+
+        $this->mock(ServerPolicy::class, function (MockInterface $mock) use ($user) {
+            $mock->shouldReceive('create')
+                ->with($user)
+                ->once()
+                ->andReturnTrue();
+        });
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class,
+                function (MockInterface $mock) use ($serverData) {
+                    $mock->expects('execute')
+                        ->withAnyArgs()
+                        ->once()
+                        ->andReturn(new Server);
+                }
+            ))
+            ->assertHasNoErrors();
+    }
+
+    public function test_server_with_invalid_provider_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => 100500,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.provider_id']);
+    }
+
+    public function test_server_with_invalid_region_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'inv_reg',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.region']);
+    }
+
+    public function test_server_with_invalid_size_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'inv_size',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.size']);
+    }
+
+    public function test_server_with_invalid_type_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->set('state.type', 'foobar')
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.type']);
+    }
+
+    public function test_server_with_invalid_python_version_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '666',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.python_version']);
+    }
+
+    public function test_server_with_invalid_database_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'yandere_simulator_1.2',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.database']);
+    }
+
+    public function test_server_without_db_name_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => null,
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.db_name']);
+    }
+
+    public function test_server_with_invalid_cache_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => Str::random(),
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'i_am_rich',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.cache']);
+    }
+
+    public function test_server_without_name_cannot_be_created()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var Provider $provider */
+        $provider = Provider::factory([
+            'user_id' => $user->id,
+        ])->create();
+
+        $state = [
+            'provider_id' => $provider->id,
+            'name' => null,
+            'region' => 'nyc_1',
+            'size' => 'size_1',
+            'type' => 'app',
+            'python_version' => '3_9',
+            'database' => 'mysql-8_0',
+            'db_name' => 'app_db',
+            'cache' => 'redis',
+            'add_ssh_key_to_vcs' => true,
+        ];
+
+        $this->actingAs($user);
+
+        Livewire::test(DigitalOceanForm::class)
+            ->set('providers', [$provider->id => 'Provider 1', 666 => 'Provider 666'])
+            ->set('availableRegions', ['nyc_1' => 'New York 1', 'nyc_2' => 'New York 2'])
+            ->set('availableSizes', ['size_1' => 'Size 1', 'size_2' => 'Size 2'])
+            ->set('state', $state)
+            ->call('store', Mockery::mock(StoreServerAction::class))
+            ->assertHasErrors(['state.name']);
+    }
+}
