79afc0fbacc9aa54a5e7fdb0c603762a0a7fe6b5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-11 16:20:45 +0300

    Create CreateWorkerSshKeyForServerJobTest.php

---
+++ tests/Feature/CreateWorkerSshKeyForServerJobTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\WorkerSshKeys\CreateWorkerSshKeyAction;
+use App\Jobs\Servers\CreateWorkerSshKeyForServerJob;
+use App\Models\Server;
+use Mockery\MockInterface;
+use Tests\AbstractFeatureTest;
+
+class CreateWorkerSshKeyForServerJobTest extends AbstractFeatureTest
+{
+    public function test_job_has_correct_parameters_and_calls_action()
+    {
+        $server = Server::factory()->create();
+
+        $job = new CreateWorkerSshKeyForServerJob($server);
+
+        $this->assertEquals('servers', $job->queue);
+
+        $this->mock(CreateWorkerSshKeyAction::class, function (MockInterface $mock) use ($server) {
+            $mock->shouldReceive('execute')->withAnyArgs()->once();
+        });
+
+        app()->call([$job, 'handle']);
+    }
+}
