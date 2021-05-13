2a3674db8aed48f524b2b882d076e50f18a4925f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-13 13:48:29 +0300

    Create GetServerPublicIpJobTest.php

---
+++ tests/Feature/GetServerPublicIpJobTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Jobs\Servers\GetServerPublicIpJob;
+use App\Models\Provider;
+use App\Models\Server;
+use App\Models\WorkerSshKey;
+use App\Services\ServerProviderInterface;
+use Mockery\MockInterface;
+use Tests\AbstractFeatureTest;
+
+class GetServerPublicIpJobTest extends AbstractFeatureTest
+{
+    public function test_job_parameters_and_logic()
+    {
+        /** @var Server $server */
+        $server = Server::factory([
+            'external_id' => '100500',
+        ])
+            ->has(WorkerSshKey::factory(), 'workerSshKey')
+            ->for(Provider::factory()->digitalOceanV2()->withOwner(), 'provider')
+            ->create();
+
+        $job = new GetServerPublicIpJob($server);
+
+        $this->assertEquals('providers', $job->queue);
+
+        $this->mockBind('digital_ocean_v2', ServerProviderInterface::class,
+            function (MockInterface $mock) use ($server) {
+                $mock->shouldReceive('getServerPublicIp4')
+                    ->with($server->externalId)
+                    ->once()
+                    ->andReturn('192.168.1.1');
+            }
+        );
+
+        app()->call([$job, 'handle']);
+
+        $server->refresh();
+
+        $this->assertEquals('192.168.1.1', $server->publicIp);
+    }
+}
