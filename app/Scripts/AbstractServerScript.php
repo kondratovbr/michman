9dd9ae4816ad9e8157a83b956ef13b7a7293e532
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-21 16:27:09 +0300

    Create AbstractServerScript.php

---
+++ app/Scripts/AbstractServerScript.php
@@ -0,0 +1,166 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts;
+
+use App\Models\Server;
+use phpseclib3\Net\SFTP;
+use phpseclib3\Net\SSH2;
+
+abstract class AbstractServerScript
+{
+    /** Server to run command on. */
+    private Server $server;
+    /** SSH session to a remote server with SFTP support. */
+    private SFTP $ssh;
+
+    /**
+     * Create a new SSH session to a remote server with SFTP support.
+     *
+     * Override this method to create an SSH session using a method.
+     */
+    protected function ssh(): SFTP|null
+    {
+        return null;
+    }
+
+    /**
+     * Set a server attribute.
+     */
+    protected function setServer($server): void
+    {
+        $this->server = $server;
+    }
+
+    /**
+     * Set a current SSH session attribute;
+     */
+    protected function setSsh(SFTP $ssh): void
+    {
+        $this->ssh = $ssh;
+    }
+
+    /**
+     * Initialize an SSH session.
+     */
+    private function initialize(): SFTP
+    {
+        if (! isset($this->server))
+            throw new \RuntimeException('Server model instance is not set. It is required to perform any actions over SSH.');
+
+        $this->ssh ??= $this->ssh() ?? $this->server->sftp();
+
+        return $this->ssh;
+    }
+
+    /**
+     * Execute a command on a remote server over SSH.
+     */
+    protected function exec(string $command): string|bool
+    {
+        $this->initialize();
+
+        if ($this->ssh->isPTYEnabled())
+            $this->disablePty();
+
+        try {
+            return $result = $this->ssh->exec($command);
+        } finally {
+            $this->server->log(
+                type: 'exec',
+                command: $command,
+                exitCode: $this->ssh->getExitStatus(),
+                content: $result ?? null,
+            );
+        }
+    }
+
+    /**
+     * Execute a command on a remote server over SSH using a PTY mode.
+     */
+    protected function execPty(string $command): bool
+    {
+        $this->initialize();
+
+        if (! $this->ssh->isPTYEnabled())
+            $this->enablePty();
+
+        try {
+            return $this->ssh->exec($command);
+        } finally {
+            $this->server->log(
+                type: 'exec_pty',
+                command: $command,
+            );
+        }
+    }
+
+    protected function read(string $expected = '', $mode = SSH2::READ_SIMPLE): string
+    {
+        $this->initialize();
+
+        if (! $this->ssh->isPTYEnabled())
+            $this->enablePty();
+
+        try {
+            return $result = $this->ssh->read($expected, $mode);
+        } finally {
+            $this->server->log(
+                type: 'read',
+                content: $result ?? null,
+            );
+        }
+    }
+
+    /**
+     * Enable PTY on the current SSH session.
+     */
+    protected function enablePty(): void
+    {
+        $this->initialize();
+
+        $this->ssh->enablePTY();
+    }
+
+    /**
+     * Disable PTY on the current SSH session.
+     */
+    protected function disablePty(): void
+    {
+        $this->initialize();
+
+        $this->ssh->disablePTY();
+    }
+
+    /**
+     * Set a command timeout for the current SSH session.
+     */
+    protected function setTimeout(int $seconds = 0): void
+    {
+        $this->initialize();
+
+        $this->ssh->setTimeout($seconds);
+    }
+
+    /**
+     * Send a local file to the server over SFTP.
+     */
+    protected function sendFile(string $remotePath, string $localPath): void
+    {
+        $this->initialize();
+
+        try {
+            $success = $this->ssh->put(
+                $remotePath,
+                $localPath,
+                SFTP::SOURCE_LOCAL_FILE,
+            );
+        } finally {
+            $this->server->log(
+                type: 'send_file',
+                localFile: $localPath,
+                remoteFile: $remotePath,
+                success: $success ?? false,
+            );
+        }
+    }
+}
