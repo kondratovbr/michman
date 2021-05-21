a445ebba50febe0b57d66f7c7ff2a8a6edf4cf3b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-21 16:27:03 +0300

    Create log method for server model

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -134,6 +134,31 @@ class Server extends AbstractModel
     }
 
     /**
+     * Create a ServerLog entry for this server.
+     */
+    public function log(
+        string $type,
+        string $command = null,
+        int $exitCode = null,
+        string $content = null,
+        string $localFile = null,
+        string $remoteFile = null,
+        bool $success = null,
+        CarbonInterface $timestamp = null,
+    ): void {
+        $this->logs()->create([
+            'type' => $type,
+            'command' => $command,
+            'exit_code' => $exitCode,
+            'content' => $content,
+            'local_file' => $localFile,
+            'remote_file' => $remoteFile,
+            'success' => $success,
+            'created_at' => $timestamp ?? now(),
+        ]);
+    }
+
+    /**
      * Get a relation to the provider that runs this server.
      */
     public function provider(): BelongsTo
