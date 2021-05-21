1f02a7421aba22403c873dc7ff61afc300c12ef7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-21 16:27:33 +0300

    Create ServerLogFactory.php

---
+++ database/factories/ServerLogFactory.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\ServerLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ServerLogFactory extends Factory
+{
+    /** @var string The name of the factory's corresponding model. */
+    protected $model = ServerLog::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
