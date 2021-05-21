7e22f9966b7e35005b280978f6db17f0d62bf3f8
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-21 16:27:25 +0300

    Create DisableSshAccessForUserScript.php

---
+++ app/Scripts/Root/DisableSshAccessForUserScript.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+
+class DisableSshAccessForUserScript extends AbstractServerScript
+{
+    public function execute(Server $server, string $username, SFTP $ssh = null): void
+    {
+        $this->setServer($server);
+        $this->setSsh($ssh ?? $server->sftp('root'));
+
+        $directory = $username === 'root'
+            ? '/root/.ssh'
+            : "/home/{$username}/.ssh";
+
+        $this->exec("rm -rf {$directory}");
+    }
+}
