42f607826c08005d6d1cb024670a0fa00e0f833b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-21 16:27:17 +0300

    Create ConfigureSshServerScript.php

---
+++ app/Scripts/Root/ConfigureSshServerScript.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+
+class ConfigureSshServerScript extends AbstractServerScript
+{
+    public function execute(Server $server, SFTP $ssh = null): void
+    {
+        $this->setServer($server);
+        $this->setSsh($ssh ?? $server->sftp('root'));
+
+        $this->sendFile(
+            '/etc/ssh/sshd_config',
+            base_path('servers/ssh/sshd_config'),
+        );
+    }
+}
