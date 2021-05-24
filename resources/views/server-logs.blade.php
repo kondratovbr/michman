487bbf821bff306b64ec062999b5011935198a92
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-24 17:37:48 +0300

    Create server-logs.blade.php

---
+++ resources/views/server-logs.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="referrer" content="always">
+
+        @livewireStyles
+
+        <title>Blank Page</title>
+    </head>
+    <body>
+
+        <pre>
+            @foreach($logs as $log)
+                @isset($log->command)
+                    <p>{!! $log->command !!}</p>
+                @endisset
+                @isset($log->content)
+                    <p>{!! $log->content !!}</p>
+                @endisset
+                @isset($log->localFile)
+                    <p>{!! $log->localFile . ' -> ' . $log->remoteFile !!}</p>
+                @endisset
+            @endforeach
+        </pre>
+
+        @livewireScripts
+    </body>
+</html>
