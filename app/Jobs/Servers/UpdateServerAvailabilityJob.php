fa3d8998613a7516c1ffac4a687b0b365044da21
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-25 13:12:40 +0300

    Update UpdateServerAvailabilityJob.php

--- app/Jobs/Servers/UpdateServerAvailabilityJob.php
+++ app/Jobs/Servers/UpdateServerAvailabilityJob.php
@@ -2,16 +2,25 @@
 
 namespace App\Jobs\Servers;
 
+use App\Exceptions\SshAuthFailedException;
 use App\Models\Server;
+use App\Scripts\Sudo\VerifyServerAvailabilityScript;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\ThrottlesExceptions;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use DateTimeInterface;
+use Throwable;
 
 class UpdateServerAvailabilityJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /** @var int The amount of seconds to wait between retries if a server isn't accessible yet. */
+    protected const SECONDS_BETWEEN_RETRIES = 10;
 
     protected Server $server;
 
@@ -22,13 +31,64 @@ class UpdateServerAvailabilityJob implements ShouldQueue
         $this->server = $server->withoutRelations();
     }
 
+    /** Get the middleware the job should pass through. */
+    public function middleware(): array
+    {
+        return [
+            (new ThrottlesExceptions(3, 1))->backoff(1),
+        ];
+    }
+
+    /** Determine the time at which the job should timeout. */
+    public function retryUntil(): DateTimeInterface
+    {
+        return now()->addMinutes(5);
+    }
+
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(VerifyServerAvailabilityScript $verifyServerAvailability): void
     {
-        // TODO: CRITICAL! CONTINUE!
+        DB::transaction(function () use ($verifyServerAvailability) {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
 
-        //
+            try {
+                $ssh = $server->sftp();
+            } catch (SshAuthFailedException $exception) {
+                $server->available = false;
+                $server->save();
+                return;
+            }
+
+            if (! $ssh->isConnected()) {
+                $this->release(static::SECONDS_BETWEEN_RETRIES);
+                return;
+            }
+
+            $server->available = $verifyServerAvailability->execute($server, $ssh);
+            $server->save();
+        }, 5);
+    }
+
+    /**
+     * Handle a job failure.
+     */
+    public function failed(Throwable $exception): void
+    {
+        DB::transaction(function () {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            $server->available = false;
+            $server->save();
+        }, 5);
     }
 }
