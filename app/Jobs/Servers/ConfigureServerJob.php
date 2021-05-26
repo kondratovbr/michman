d7dc92c47802930430e920f3ea7978c249a264b1
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-26 15:53:28 +0300

    Update ConfigureServerJob.php

--- app/Jobs/Servers/ConfigureServerJob.php
+++ app/Jobs/Servers/ConfigureServerJob.php
@@ -4,21 +4,24 @@ namespace App\Jobs\Servers;
 
 use App\Models\Server;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class ConfigureServerJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    /** The number of times the job may be attempted. */
+    public int $tries = 5;
+
     protected Server $server;
 
     public function __construct(Server $server)
     {
-        $this->onQueue('servers');
+        $this->onQueue('default');
 
         $this->server = $server->withoutRelations();
     }
@@ -28,6 +31,19 @@ class ConfigureServerJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        DB::transaction(function () {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            $jobClass = (string) config('servers.types.' . $server->type . '.configuration_job_class');
+
+            if (empty($jobClass))
+                throw new \RuntimeException('No job class for this server type configured.');
+
+            $jobClass::dispatch($server);
+        }, 5);
     }
 }
