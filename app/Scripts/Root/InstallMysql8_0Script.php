c4807330cd52afea7d6ae9e51b088129e11643d9
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-26 15:52:53 +0300

    Create InstallMysql8_0Script.php

---
+++ app/Scripts/Root/InstallMysql8_0Script.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+
+class InstallMysql8_0Script extends AbstractServerScript
+{
+    public function execute(Server $server, SFTP $ssh = null): void
+    {
+        // TODO: CRITICAL! Continue!
+
+        $this->init($server, $ssh);
+
+        $this->enablePty();
+        $this->setTimeout(60 * 30); // 30 min
+        $this->execPty('DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-8.0');
+        $this->read();
+        $this->disablePty();
+    }
+}
