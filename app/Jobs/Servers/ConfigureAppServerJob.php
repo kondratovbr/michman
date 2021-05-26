38ac326aa771c5a2eca306aff0a75640b52e66ce
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-26 15:52:58 +0300

    Create ConfigureAppServerJob.php

---
+++ app/Jobs/Servers/ConfigureAppServerJob.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Models\Server;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class ConfigureAppServerJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected Server $server;
+
+    public function __construct(Server $server)
+    {
+        $this->onQueue('servers');
+
+        $this->server = $server->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            //
+        }, 5);
+    }
+}
