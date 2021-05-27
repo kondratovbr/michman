c9c35611a5bcfa80d7d8805db764c461929fe511
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-27 16:44:54 +0300

    Update CreateDatabaseJob.php

--- app/Jobs/Servers/CreateDatabaseJob.php
+++ app/Jobs/Servers/CreateDatabaseJob.php
@@ -3,12 +3,15 @@
 namespace App\Jobs\Servers;
 
 use App\Jobs\Traits\InteractsWithRemoteServers;
+use App\Models\Database;
 use App\Models\Server;
+use App\Scripts\Root\Mysql8_0\CreateDatabaseScript;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class CreateDatabaseJob implements ShouldQueue
 {
@@ -28,10 +31,24 @@ class CreateDatabaseJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(CreateDatabaseScript $createDatabase): void
     {
         // TODO: CRITICAL! Implement.
 
-        //
+        DB::transaction(function() use ($createDatabase) {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            /** @var Database $database */
+            $database = $server->databases()->create([
+                'name' => $this->dbName,
+            ]);
+
+            $createDatabase->execute($server, $database->name);
+
+        }, 5);
     }
 }
