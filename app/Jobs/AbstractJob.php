28218933052b64ffbf46278d5aeda92d5324b91e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-28 15:04:56 +0300

    Create AbstractJob.php

---
+++ app/Jobs/AbstractJob.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Support\Arr;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use RuntimeException;
+
+class AbstractJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Set a queue for this job.
+     */
+    protected function queue(string $queueName): void
+    {
+        if (! Arr::hasValue(config('queue.queues'), $queueName))
+            throw new RuntimeException("Queue {$queueName} is not configured.");
+
+        $this->onQueue($queueName);
+    }
+}
