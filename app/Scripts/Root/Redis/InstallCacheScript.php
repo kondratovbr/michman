b4b83a12859da8c6f6db1fc66e8b45880e0a0152
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-05-28 16:46:28 +0300

    Create InstallCacheScript.php

---
+++ app/Scripts/Root/Redis/InstallCacheScript.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root\Redis;
+
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use App\Support\Str;
+use phpseclib3\Net\SFTP;
+use RuntimeException;
+
+class InstallCacheScript extends AbstractServerScript
+{
+    public function execute(Server $server, SFTP $ssh = null): void
+    {
+        $this->init($server, $ssh);
+
+        $this->enablePty();
+        $this->setTimeout(60 * 30); // 30 min
+        $this->execPty('DEBIAN_FRONTEND=noninteractive apt-get update -y');
+        $this->read();
+        $this->execPty('DEBIAN_FRONTEND=noninteractive apt-get install -y redis-server');
+        $this->read();
+        $this->disablePty();
+
+        $this->sendFile(
+            '/etc/redis/redis.conf',
+            base_path('servers/redis/redis.conf'),
+        );
+
+        $this->exec('systemctl restart redis.service');
+        // Wait a bit in case it doesn't start immediately.
+        $this->setTimeout(60);
+        $this->exec('sleep 30');
+
+        $output = $this->exec('systemctl status redis');
+        if (
+            ! Str::contains(Str::lower($output), 'active (running)')
+            || $this->getExitStatus() !== 0
+        ) {
+            throw new RuntimeException('Redis failed to start.');
+        }
+    }
+}
