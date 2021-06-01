9a2534bf0b0315f3e66b90ad72c6c6348912db7a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-01 16:55:27 +0300

    Create StoreFirewallRuleAction.php

---
+++ app/Actions/Servers/StoreFirewallRuleAction.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Servers;
+
+use App\DataTransferObjects\FirewallRuleData;
+use App\Models\FirewallRule;
+
+class StoreFirewallRuleAction
+{
+    public function execute(FirewallRuleData $data): FirewallRule
+    {
+        // TODO: CRITICAL! Don't forget to implement.
+
+        //
+    }
+}
