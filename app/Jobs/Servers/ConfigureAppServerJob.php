f429208be620b4ff9022dbb7047c0f33fcb6742e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-01 16:56:23 +0300

    Update ConfigureAppServerJob.php

--- app/Jobs/Servers/ConfigureAppServerJob.php
+++ app/Jobs/Servers/ConfigureAppServerJob.php
@@ -2,9 +2,12 @@
 
 namespace App\Jobs\Servers;
 
+use App\Actions\Servers\StoreFirewallRuleAction;
+use App\DataTransferObjects\FirewallRuleData;
 use App\Jobs\AbstractJob;
 use App\Jobs\Traits\InteractsWithRemoteServers;
 use App\Models\Server;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\DB;
 
 class ConfigureAppServerJob extends AbstractJob
@@ -23,16 +26,36 @@ class ConfigureAppServerJob extends AbstractJob
     /**
      * Execute the job.
      */
-    public function handle(): void
-    {
-        DB::transaction(function () {
+    public function handle(StoreFirewallRuleAction $storeFirewallRuleAction): void {
+        DB::transaction(function () use ($storeFirewallRuleAction) {
             /** @var Server $server */
             $server = Server::query()
                 ->whereKey($this->server->getKey())
                 ->lockForUpdate()
                 ->firstOrFail();
 
-            //
+            Bus::chain([
+                new InstallNginxJob($server),
+                new InstallGunicornJob($server),
+
+                // TODO: CRITICAL! Don't forget the rest of the stuff I should do here!
+
+                //
+
+            ])->dispatch();
+
+            $storeFirewallRuleAction->execute(new FirewallRuleData(
+                server: $server,
+                name: 'HTTP',
+                port: '80',
+            ));
+
+            $storeFirewallRuleAction->execute(new FirewallRuleData(
+                server: $server,
+                name: 'HTTPS',
+                port: '443',
+            ));
+
         }, 5);
     }
 }
