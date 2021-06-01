5ac11a907e9d4cf9b7275af551336b165ab85c7a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-01 16:55:31 +0300

    Create FirewallRuleData.php

---
+++ app/DataTransferObjects/FirewallRuleData.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\DataTransferObjects;
+
+use App\Models\Server;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class FirewallRuleData extends DataTransferObject
+{
+    public Server $server;
+    public string $name;
+    public string $port;
+    public string|null $fromIp = null;
+}
