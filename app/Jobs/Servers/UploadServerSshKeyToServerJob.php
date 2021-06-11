8340b2b72ccdb7e717886fc15a5f94bf91b0a743
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-11 12:08:23 +0300

    Update UploadServerSshKeyToServerJob.php

--- app/Jobs/Servers/UploadServerSshKeyToServerJob.php
+++ app/Jobs/Servers/UploadServerSshKeyToServerJob.php
@@ -3,21 +3,41 @@
 namespace App\Jobs\Servers;
 
 use App\Jobs\AbstractJob;
+use App\Jobs\Traits\InteractsWithRemoteServers;
+use App\Models\Server;
+use App\Scripts\Root\UploadServerSshKeyScript;
+use Illuminate\Support\Facades\DB;
 
 class UploadServerSshKeyToServerJob extends AbstractJob
 {
-    public function __construct()
-    {
-        $this->setQueue('default');
+    use InteractsWithRemoteServers;
 
-        //
+    protected Server $server;
+
+    public function __construct(Server $server)
+    {
+        $this->setQueue('servers');
+
+        $this->server = $server->withoutRelations();
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(UploadServerSshKeyScript $uploadServerSshKey): void
     {
-        //
+        DB::transaction(function () use ($uploadServerSshKey) {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            $uploadServerSshKey->execute(
+                $server,
+                $server->serverSshKey,
+                (string) config('servers.worker_user'),
+            );
+        }, 5);
     }
 }
