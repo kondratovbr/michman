4d9b768f5b1e6cc3c1cb4942f82b6223c7919acd
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-11 12:08:27 +0300

    Update CreateServerSshKeyJob.php

--- app/Jobs/Servers/CreateServerSshKeyJob.php
+++ app/Jobs/Servers/CreateServerSshKeyJob.php
@@ -2,22 +2,35 @@
 
 namespace App\Jobs\Servers;
 
+use App\Actions\ServerSshKeys\CreateServerSshKeyAction;
 use App\Jobs\AbstractJob;
+use App\Models\Server;
+use Illuminate\Support\Facades\DB;
 
 class CreateServerSshKeyJob extends AbstractJob
 {
-    public function __construct()
+    protected Server $server;
+
+    public function __construct(Server $server)
     {
         $this->setQueue('default');
 
-        //
+        $this->server = $server->withoutRelations();
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(CreateServerSshKeyAction $action): void
     {
-        //
+        DB::transaction(function () use ($action) {
+            /** @var Server $server */
+            $server = Server::query()
+                ->whereKey($this->server->getKey())
+                ->lockForUpdate()
+                ->firstOrFail();
+
+            $action->execute($server);
+        }, 5);
     }
 }
