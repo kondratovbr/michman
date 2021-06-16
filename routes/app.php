ae32b6ef1e1d68c94c36b708b3b1459f0a137653
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-16 09:39:16 +0300

    Remove unused routes

--- routes/app.php
+++ routes/app.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('servers', Controllers\ServerController::class)
-    ->only(['index', 'show', 'create', 'store']);
+    ->only(['index', 'show']);
