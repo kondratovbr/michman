c3b837f9a4d0d53db3b2a54727d7b1e22a7a9362
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-17 17:27:16 +0300

    Create UpdateVcsProviderAction.php

---
+++ app/Actions/VcsProviders/UpdateVcsProviderAction.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\VcsProviders;
+
+use App\DataTransferObjects\VcsProviderData;
+use App\Models\VcsProvider;
+
+class UpdateVcsProviderAction
+{
+    public function execute(VcsProvider $vcsProvider, VcsProviderData $data): VcsProvider
+    {
+        // TODO: CRITICAL! CONTINUE!
+
+        //
+    }
+}
