d6b8b057fdba44f781f9e3b76c8d63ba8329f623
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-17 12:33:18 +0300

    Bugfix

--- resources/views/components/button-new.blade.php
+++ resources/views/components/button-new.blade.php
@@ -3,7 +3,7 @@
 {{--TODO: Maybe use cursor-wait option for when the loading spinner is showing.--}}
 {{--TODO: See how it looks when "disabled". Maybe needs an update.--}}
 
-@props(['textClasses', 'link' => false])
+@props(['border', 'paddingY', 'textClasses', 'capitalize', 'link' => false])
 
 @php
     $classes = implode(' ', [
