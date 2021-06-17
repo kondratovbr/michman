2a17a763700494560fb6beeff27da303d12dd511
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-17 15:17:29 +0300

    Create VcsProviderPolicy.php

---
+++ app/Policies/VcsProviderPolicy.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\VcsProvider;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VcsProviderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine if a user can create a VcsProvider for himself.
+     */
+    public function create(User $user, string $provider): bool
+    {
+        return is_null($user->vcs($provider));
+    }
+
+    /**
+     * Determine if a user can delete a VcsProvider.
+     */
+    public function delete(User $user, VcsProvider $vcsProvider): bool
+    {
+        return false;
+    }
+}
