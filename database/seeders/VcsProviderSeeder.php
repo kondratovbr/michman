a4c97b15a0f408a8d9be1ba8aed44aae677ec41b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-18 11:21:48 +0300

    Update VcsProviderSeeder.php

--- database/seeders/VcsProviderSeeder.php
+++ database/seeders/VcsProviderSeeder.php
@@ -2,15 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\VcsProvider;
 use Illuminate\Database\Seeder;
 
 class VcsProviderSeeder extends Seeder
 {
+    /** @var int Number of fake providers to seed. */
+    private const NUM_PROVIDERS = 10;
+
     /**
      * Seed the database.
      */
     public function run(): void
     {
-        //
+        VcsProvider::factory()
+            ->forRandomUserFrom(User::all())
+            ->count(static::NUM_PROVIDERS)
+            ->create();
     }
 }
