bb5e3872b2e53aa6d0945a0eeae89aec136e0ffa
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-18 16:00:08 +0300

    TODO

--- app/Actions/VcsProviders/StoreVcsProviderAction.php
+++ app/Actions/VcsProviders/StoreVcsProviderAction.php
@@ -3,13 +3,17 @@
 namespace App\Actions\VcsProviders;
 
 use App\DataTransferObjects\VcsProviderData;
-use App\Models\User;
 use App\Models\VcsProvider;
 
 class StoreVcsProviderAction
 {
     public function execute(VcsProviderData $data): VcsProvider
     {
+        /*
+         * TODO: CRITICAL! I should somehow handle a situation when the third-party account is already linked to some other user.
+         *       Is it normal? Can we work like that? Maybe just warn the user?
+         */
+
         /** @var VcsProvider $vcsProvider */
         $vcsProvider = $data->user->vcsProviders()->create($data->toArray());
 
