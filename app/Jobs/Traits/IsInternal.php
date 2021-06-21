4c8cfac4ea24c7df7e971b534cab82a6501f11ca
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-21 13:22:52 +0300

    Create IsInternal.php

---
+++ app/Jobs/Traits/IsInternal.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Traits;
+
+trait IsInternal
+{
+    /** The number of times the job may be attempted. */
+    public int $tries = 5;
+
+    /** The number of seconds to wait before retrying the job. */
+    public int $backoff = 5;
+}
