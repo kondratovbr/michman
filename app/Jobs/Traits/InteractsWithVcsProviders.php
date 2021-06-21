7ae557f791da0dd1a1070461689cbc840c3d1788
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-21 13:23:08 +0300

    Create InteractsWithVcsProviders.php

---
+++ app/Jobs/Traits/InteractsWithVcsProviders.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Traits;
+
+use DateTimeInterface;
+
+trait InteractsWithVcsProviders
+{
+    /** The number of seconds the job can run before timing out. */
+    public int $timeout = 60 * 5; // 5 min
+
+    /** The number of seconds to wait before retrying the job. */
+    public int $backoff = 60; // 1 min
+
+    /** Determine the time at which the job should timeout. */
+    public function retryUntil(): DateTimeInterface
+    {
+        return now()->addMinutes(30);
+    }
+}
