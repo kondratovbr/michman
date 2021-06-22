bcfb3048c8fa3674cb8b4bb97875120f40ac36bc
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-22 16:48:31 +0300

    Create update-config-form.blade.php

---
+++ resources/views/pythons/update-config-form.blade.php
@@ -0,0 +1,20 @@
+{{--TODO: CRITICAL! Scaffolding - implement.--}}
+
+<x-form-section submit="store">
+
+    <x-slot name="title">
+        Config
+    </x-slot>
+
+    <x-slot name="description">
+        Update Python config values on the server.
+    </x-slot>
+
+    <x-slot name="form">
+        Form
+    </x-slot>
+
+    <x-slot name="actions">
+    </x-slot>
+
+</x-form-section>
