b1786de58213db2f736f41fe7df6ae4a357d0aed
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-06-25 11:46:49 +0300

    Protect WebSockets dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Team;
+use App\Models\User;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
+
+        // TODO: CRITICAL! Make sure to protect all admin/support routes and properly configure this one as well. This one is a temporary solution.
+        // Authorization gate used by beyondcode/laravel-websockets for restricting access to the stats dashboard.
+        Gate::define('viewWebSocketsDashboard', function (User $user = null) {
+            return config('app.env') == 'local'
+                && in_array($user->email, [
+                    'kondratovbr@gmail.com',
+                    'admin@example.com',
+                ])
+                && ! is_null($user->emailVerifiedAt);
+        });
 
         //
     }
