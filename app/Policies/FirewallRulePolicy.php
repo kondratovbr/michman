4e6c11107f7c5aa345366ec50df588759d7d33a3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-01 14:22:03 +0300

    Update FirewallRulePolicy.php

--- app/Policies/FirewallRulePolicy.php
+++ app/Policies/FirewallRulePolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\FirewallRule;
 use App\Models\Server;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -16,5 +17,21 @@ class FirewallRulePolicy
     public function index(User $user, Server $server): bool
     {
         return $user->is($server->provider->owner);
+    }
+
+    /**
+     * Determine whether a user is allowed to create a firewall rule for a server.
+     */
+    public function create(User $user, Server $server): bool
+    {
+        return $user->is($server->user);
+    }
+
+    /**
+     * Determine whether a user is allowed to delete a firewall rule from a server.
+     */
+    public function delete(User $user, FirewallRule $rule): bool
+    {
+        return $user->is($rule->user);
     }
 }
