935388b801720542e36f693a37d76d10b3ac284b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-01 14:22:24 +0300

    Move buttons in forms to the right

--- resources/views/components/box/footer.blade.php
+++ resources/views/components/box/footer.blade.php
@@ -1,5 +1,5 @@
 <div
     {{ $attributes->merge([
-        'class' => 'flex items-center px-4 py-3 bg-navy-200 sm:px-6 sm:rounded-b-lg',
+        'class' => 'flex justify-end items-center px-4 py-3 bg-navy-200 sm:px-6 sm:rounded-b-lg',
     ]) }}
 >{{ $slot }}</div>
