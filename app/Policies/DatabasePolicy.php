4048681068ed43c817cb8a5eaaab96553f4d2fca
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 10:34:00 +0300

    Create DatabasePolicy.php

---
+++ app/Policies/DatabasePolicy.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Server;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class DatabasePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether a user is allowed to see the index of the database of a server.
+     */
+    public function index(User $user, Server $server): bool
+    {
+        return $user->is($server->user);
+    }
+
+    /**
+     * Determine whether a user is allowed to create a new database for a server.
+     */
+    public function create(User $user, Server $server): bool
+    {
+        return $user->is($server->user);
+    }
+}
