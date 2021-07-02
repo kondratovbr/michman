b0f0a04190cca86a21686a2605c7b357b2b1d909
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:38:46 +0300

    Update create-form.blade.php

--- resources/views/databases/create-form.blade.php
+++ resources/views/databases/create-form.blade.php
@@ -5,25 +5,31 @@
     <x-slot name="title">{{ __('servers.database.form.title') }}</x-slot>
 
     <x-slot name="form">
+        <div class="space-y-6">
 
-        <x-field>
-            <x-label>{{ __('servers.database.form.name') }}</x-label>
-            <x-inputs.text
-                name="name"
-                wire:model.defer="name"
-            />
-            <x-input-error for="name" />
-        </x-field>
-
-        @if(! $databaseUsers->isEmpty())
-            <x-field class="space-y-1">
-                @foreach($databaseUsers as $databaseUser)
-                    <x-checkbox-new
-                        name="grantedUsers.{{ $databaseUser->getKey() }}"
-                    >{{ $databaseUser->name }}</x-checkbox-new>
-                @endforeach
+            <x-field class="max-w-sm">
+                <x-label>{{ __('servers.database.form.name') }}</x-label>
+                <x-inputs.text
+                    name="name"
+                    wire:model.defer="name"
+                />
+                <x-input-error for="name" />
             </x-field>
-        @endif
+
+            @if(! $databaseUsers->isEmpty())
+                <x-field class="max-w-xs">
+                    <x-label>{{ __('servers.database.form.grant-access') }}</x-label>
+                    <div class="flex flex-col space-y-1">
+                        @foreach($databaseUsers as $databaseUser)
+                            <x-checkbox-new
+                                name="grantedUsers.{{ $databaseUser->getKey() }}"
+                            >{{ $databaseUser->name }}</x-checkbox-new>
+                        @endforeach
+                    </div>
+                </x-field>
+            @endif
+
+        </div>
     </x-slot>
 
     <x-slot name="actions">
