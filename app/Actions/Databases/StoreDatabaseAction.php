437d14ed1d9d992990c6a3bce4c47d9b9c2bca3f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:12:23 +0300

    Create StoreDatabaseAction.php

---
+++ app/Actions/Databases/StoreDatabaseAction.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Databases;
+
+use App\DataTransferObjects\DatabaseData;
+use App\Models\Database;
+use App\Models\Server;
+
+class StoreDatabaseAction
+{
+    public function execute(DatabaseData $data, Server $server): Database
+    {
+        // TODO: CRITICAL! Implement.
+
+        //
+    }
+}
