7dddb01c9c9caa5543eb534e7cf57415dd571c09
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:13:08 +0300

    Update FirewallRuleFactory.php

--- database/factories/FirewallRuleFactory.php
+++ database/factories/FirewallRuleFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\FirewallRule;
+use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FirewallRuleFactory extends Factory
@@ -16,7 +18,33 @@ class FirewallRuleFactory extends Factory
     public function definition(): array
     {
         return [
+            'name' => $this->faker->domainName,
+            'port' => (string) rand(1, 1024),
+            'from_ip' => rand(0, 1) ? $this->faker->ipv4 : null,
             'can_delete' => true,
+            'status' => FirewallRule::STATUS_ADDED,
         ];
+    }
+
+    /**
+     * Attach firewall rules to random servers from a collection.
+     *
+     * @return $this
+     */
+    public function forRandomServerFrom(Collection $servers): static
+    {
+        return $this->afterMaking(
+            fn (FirewallRule $rule) => $this->associateServer($rule, $servers->random())
+        );
+    }
+
+    /**
+     * Attach a firewall rule to a server.
+     *
+     * @return $this
+     */
+    protected function associateServer(FirewallRule $rule, Server $server): void
+    {
+        $rule->server()->associate($server);
     }
 }
