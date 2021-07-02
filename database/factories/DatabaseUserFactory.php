2592728ab92765b1a40374b31a9878db93a3a536
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:13:05 +0300

    Update DatabaseUserFactory.php

--- database/factories/DatabaseUserFactory.php
+++ database/factories/DatabaseUserFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\DatabaseUser;
+use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DatabaseUserFactory extends Factory
@@ -16,7 +18,43 @@ class DatabaseUserFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->userName,
+            'status' => DatabaseUser::STATUS_CREATED,
         ];
+    }
+
+    /**
+     * Attach database users to random servers from a collection.
+     *
+     * @return $this
+     */
+    public function forRandomServerFrom(Collection $servers): static
+    {
+        return $this->afterMaking(
+            fn (DatabaseUser $databaseUser) => $this->associateServer($databaseUser, $servers->random())
+        );
+    }
+
+    /**
+     * Attach database users to random databases on their respective servers, if any exist.
+     *
+     * @return $this
+     */
+    public function attachToRandomDatabase(): static
+    {
+        return $this->afterCreating(function (DatabaseUser $databaseUser) {
+            $databases = $databaseUser->server->databases;
+
+            if ($databases->isNotEmpty())
+                $databaseUser->databases()->attach($databases->random());
+        });
+    }
+
+    /**
+     * Attach a database user to a server.
+     */
+    protected function associateServer(DatabaseUser $databaseUser, Server $server): void
+    {
+        $databaseUser->server()->associate($server);
     }
 }
