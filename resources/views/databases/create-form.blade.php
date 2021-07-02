f1f717ce5f89bf6c1a108c4501bdc06dfdcb2bcc
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:12:36 +0300

    Update create-form.blade.php

--- resources/views/databases/create-form.blade.php
+++ resources/views/databases/create-form.blade.php
@@ -1,9 +1,38 @@
+{{--TODO: CRITICAL! Unfinished. Need loading animations, etc.--}}
+
 <x-form-section submit="store">
 
     <x-slot name="title">{{ __('servers.database.form.title') }}</x-slot>
 
-    <x-slot name="form"></x-slot>
+    <x-slot name="form">
 
-    <x-slot name="actions"></x-slot>
+        <x-field>
+            <x-label>{{ __('servers.database.form.name') }}</x-label>
+            <x-inputs.text
+                name="name"
+                wire:model.defer="name"
+            />
+            <x-input-error for="name" />
+        </x-field>
+
+        @if(! $databaseUsers->isEmpty())
+            <x-field class="space-y-1">
+                @foreach($databaseUsers as $databaseUser)
+                    <x-checkbox-new
+                        name="grantedUsers.{{ $databaseUser->getKey() }}"
+                    >{{ $databaseUser->name }}</x-checkbox-new>
+                @endforeach
+            </x-field>
+        @endif
+    </x-slot>
+
+    <x-slot name="actions">
+        <x-buttons.primary
+            wire:click.prevent="store"
+            wire:loading.attr="disabled"
+        >
+            {{ __('servers.database.form.button') }}
+        </x-buttons.primary>
+    </x-slot>
 
 </x-form-section>
