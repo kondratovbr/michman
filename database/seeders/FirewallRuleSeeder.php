b78e1bfdaf293c7147e0e8bfe728a109c6c677a7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:13:17 +0300

    Update FirewallRuleSeeder.php

--- database/seeders/FirewallRuleSeeder.php
+++ database/seeders/FirewallRuleSeeder.php
@@ -2,15 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\FirewallRule;
+use App\Models\Server;
 use Illuminate\Database\Seeder;
 
 class FirewallRuleSeeder extends Seeder
 {
+    /** @var int Total number of firewall rules to seed. */
+    protected const NUM_RULES = 50;
+
     /**
      * Seed the database.
      */
     public function run(): void
     {
-        //
+        // Make sure something is seeded for the first server with which I'm usually working during development.
+        FirewallRule::factory()
+            ->for(Server::first())
+            ->count(3)
+            ->create();
+
+        // Seed generic firewall rules.
+        FirewallRule::factory()
+            ->forRandomServerFrom(Server::all())
+            ->count(static::NUM_RULES)
+            ->create();
     }
 }
