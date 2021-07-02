0fa1f53ec1463034baba2967b4d79daea38ea973
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 15:49:33 +0300

    Create GrantDatabaseUserAccessToDatabase.php

---
+++ app/Actions/DatabaseUsers/GrantDatabaseUserAccessToDatabase.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\DatabaseUsers;
+
+use App\Models\Database;
+use App\Models\DatabaseUser;
+
+class GrantDatabaseUserAccessToDatabase
+{
+    public function execute(DatabaseUser $databaseUser, Database $database): void
+    {
+        // TODO: CRITICAL! Implement.
+
+        //
+    }
+}
