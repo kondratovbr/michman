d2025dcd290509c99c91cf7d10e810cbd57580d3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:13:12 +0300

    Update DatabaseModelSeeder.php

--- database/seeders/DatabaseModelSeeder.php
+++ database/seeders/DatabaseModelSeeder.php
@@ -2,15 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\Database;
+use App\Models\Server;
 use Illuminate\Database\Seeder;
 
 class DatabaseModelSeeder extends Seeder
 {
+    /** @var int Total number of databases to seed. */
+    protected const NUM_DATABASES = 30;
+
     /**
      * Seed the database.
      */
     public function run(): void
     {
-        //
+        // Make sure something is seeded for the first server with which I'm usually working during development.
+        Database::factory()
+            ->for(Server::first())
+            ->count(3)
+            ->create();
+
+        // Seed generic databases.
+        Database::factory()
+            ->forRandomServerFrom(Server::all())
+            ->count(static::NUM_DATABASES)
+            ->create();
     }
 }
