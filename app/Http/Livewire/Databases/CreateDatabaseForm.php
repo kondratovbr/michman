dd4df019dbc31040ace6b228d1923b641e19b8ba
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-02 12:12:34 +0300

    Update CreateDatabaseForm.php

--- app/Http/Livewire/Databases/CreateDatabaseForm.php
+++ app/Http/Livewire/Databases/CreateDatabaseForm.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\Databases;
 
+use App\Actions\Databases\StoreDatabaseAction;
+use App\DataTransferObjects\DatabaseData;
 use App\Http\Livewire\Traits\TrimsInput;
 use App\Models\Database;
 use App\Models\Server;
 use App\Validation\Rules;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component as LivewireComponent;
 
@@ -17,7 +20,17 @@ class CreateDatabaseForm extends LivewireComponent
 
     public Server $server;
 
+    public Collection $databaseUsers;
+
     public string|null $name = '';
+    /** @var bool[] */
+    public array $grantedUsers = [];
+
+    /** @var string[] */
+    protected $listeners = [
+        // TODO: CRITICAL! Does this work? Test. The event is thrown from a different component on the same page.
+        'database-user-stored' => '$refresh',
+    ];
 
     /**
      * Get the validation rules.
@@ -26,6 +39,8 @@ class CreateDatabaseForm extends LivewireComponent
     {
         return [
             'name' => Rules::string(1, 255)->required(),
+            'grantedUser' => Rules::array()->required(),
+            'grantedUsers.*' => Rules::boolean(),
         ];
     }
 
@@ -40,11 +55,17 @@ class CreateDatabaseForm extends LivewireComponent
     /**
      * Store a new database.
      */
-    public function store(): void
+    public function store(StoreDatabaseAction $storeDatabase): void
     {
+        dd($this->name, $this->databaseUsers);
+
         $validated = $this->validate();
 
         $this->authorize('create', [Database::class, $this->server]);
+
+        $storeDatabase->execute(new DatabaseData(name: $validated['name']), $this->server);
+
+        // TODO: CRITICAL! Don't forge to grant user-chosen database users access to the new database.
 
         //
     }
@@ -54,6 +75,8 @@ class CreateDatabaseForm extends LivewireComponent
      */
     public function render(): View
     {
+        $this->databaseUsers = $this->server->databaseUsers()->oldest()->get();
+
         return view('databases.create-form');
     }
 }
