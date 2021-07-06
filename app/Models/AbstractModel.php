c8e314bdb417ee38066d37f758b735b7a1bf3f91
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-06 16:34:06 +0300

    Add custom Eloquent collection

--- app/Models/AbstractModel.php
+++ app/Models/AbstractModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Collections\EloquentCollection;
 use App\Models\Traits\HasModelHelpers;
 use App\Models\Traits\UsesCamelCaseAttributes;
 use Illuminate\Database\Eloquent\Model;
@@ -13,4 +14,12 @@ abstract class AbstractModel extends Model
 {
     use UsesCamelCaseAttributes,
         HasModelHelpers;
+
+    /**
+     * Create a new custom Eloquent Collection instance.
+     */
+    public function newCollection(array $models = []): EloquentCollection
+    {
+        return new EloquentCollection($models);
+    }
 }
