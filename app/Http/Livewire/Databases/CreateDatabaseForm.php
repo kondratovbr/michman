eb54c2b5e152ffb1c3a213bcad116aeb80b58645
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-06 16:35:12 +0300

    Update CreateDatabaseForm.php

--- app/Http/Livewire/Databases/CreateDatabaseForm.php
+++ app/Http/Livewire/Databases/CreateDatabaseForm.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire\Databases;
 
 use App\Actions\Databases\StoreDatabaseAction;
-use App\Actions\DatabaseUsers\GrantDatabaseUserAccessToDatabase;
 use App\DataTransferObjects\DatabaseData;
 use App\Http\Livewire\Traits\TrimsInput;
 use App\Models\Database;
@@ -14,9 +13,9 @@ use App\Validation\Rules;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 use Livewire\Component as LivewireComponent;
+
+// TODO: CRITICAL! Cover with tests!
 
 class CreateDatabaseForm extends LivewireComponent
 {
@@ -57,43 +56,36 @@ class CreateDatabaseForm extends LivewireComponent
     public function rules(): array
     {
         return [
-            // TODO: CRITICAL! Make sure users cannot use reserved words like "information_schema".
-            //       Those are different for each database as well.
+            /*
+             * TODO: CRITICAL! Make sure users cannot use reserved words like "information_schema".
+             *       Those are different for each database as well.
+             *       Here's an example for MySQL 8.0:
+             *       https://dev.mysql.com/doc/refman/8.0/en/keywords.html
+             *       Make sure to validate database user names in the same manner as well.
+             */
             'name' => Rules::alphaNumDashString(1, 255)->required(),
             'grantedUsers' => Rules::array(),
             'grantedUsers.*' => Rules::integer()
-                ->in($this->server->databaseUsers->pluck('id')->toArray()),
+                ->in($this->server->databaseUsers->pluck(DatabaseUser::keyName())->toArray()),
         ];
     }
 
     /**
      * Store a new database.
      */
-    public function store(
-        StoreDatabaseAction $storeDatabase,
-        GrantDatabaseUserAccessToDatabase $grantAccess,
-    ): void {
+    public function store(StoreDatabaseAction $storeDatabase): void {
         $validated = $this->validate();
 
         $this->authorize('create', [Database::class, $this->server]);
 
-        DB::beginTransaction();
-
-        // TODO: CRITICAL! Should these two actions be chained somehow? I cannot grant user privileges before the DB is actually created and jobs may complete in random order.
-
-        $database = $storeDatabase->execute(new DatabaseData(
-            name: $validated['name'],
-        ), $this->server);
-
-        // TODO: CRITICAL! CONTINUE! Implement and test this and the second one in DB user creation. Then go to covering stuff with tests, I skipped quite some.
-
-        foreach ($validated['grantedUsers'] as $grantedUserKey) {
-            /** @var DatabaseUser $databaseUser */
-            $databaseUser = DatabaseUser::query()->findOrFail($grantedUserKey);
-            $grantAccess->execute($databaseUser, $database);
-        }
-
-        DB::commit();
+        // TODO: CRITICAL! CONTINUE! Finish and test this. Don't forget to chain the jobs inside the action. And don't forget to do the same for the DatabaseUser creation part.
+        $storeDatabase->execute(
+            new DatabaseData(
+                name: $validated['name'],
+            ),
+            $this->server,
+            DatabaseUser::query()->findMany($validated['grantedUsers']),
+        );
 
         $this->reset(
             'name',
@@ -101,6 +93,7 @@ class CreateDatabaseForm extends LivewireComponent
         );
 
         $this->emit('database-stored');
+        $this->emit('database-user-updated');
     }
 
     /**
