0acb26e33619dfc85e9bf014949812d04d6ba10e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-06 16:34:58 +0300

    Update StoreDatabaseUserAction.php

--- app/Actions/DatabaseUsers/StoreDatabaseUserAction.php
+++ app/Actions/DatabaseUsers/StoreDatabaseUserAction.php
@@ -6,21 +6,42 @@ use App\DataTransferObjects\DatabaseUserData;
 use App\Jobs\Servers\CreateDatabaseUserOnServerJob;
 use App\Models\DatabaseUser;
 use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
 
 // TODO: CRITICAL! Cover with tests!
 
 class StoreDatabaseUserAction
 {
-    public function execute(DatabaseUserData $data, Server $server): DatabaseUser
+    public function __construct(
+        protected GrantDatabaseUsersAccessToDatabasesAction $grantAction,
+    ) {}
+
+    public function execute(DatabaseUserData $data, Server $server, Collection $grantedDatabases = null): DatabaseUser
     {
         $attributes = $data->toArray();
 
         $attributes['status'] = DatabaseUser::STATUS_CREATING;
 
+        DB::beginTransaction();
+
         /** @var DatabaseUser $databaseUser */
         $databaseUser = $server->databaseUsers()->create($attributes);
 
-        CreateDatabaseUserOnServerJob::dispatch($databaseUser);
+        if (! is_null($grantedDatabases) && $grantedDatabases->isNotEmpty()) {
+            $grantJob = $this->grantAction->execute(
+                collect([$databaseUser]),
+                $grantedDatabases,
+            );
+        }
+
+        DB::commit();
+
+        Bus::chain([
+            new CreateDatabaseUserOnServerJob($databaseUser),
+            $grantJob ?? null,
+        ])->dispatch();
 
         return $databaseUser;
     }
