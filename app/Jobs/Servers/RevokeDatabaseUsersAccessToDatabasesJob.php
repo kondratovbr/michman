89c3db67bbf473333c3c61951fff6ab1ba5785a3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-07 17:11:46 +0300

    Create RevokeDatabaseUsersAccessToDatabasesJob.php

---
+++ app/Jobs/Servers/RevokeDatabaseUsersAccessToDatabasesJob.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\HandlesDatabases;
+use App\Jobs\Traits\InteractsWithRemoteServers;
+use App\Models\Database;
+use App\Models\DatabaseUser;
+use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use RuntimeException;
+
+class RevokeDatabaseUsersAccessToDatabasesJob extends AbstractJob
+{
+    use InteractsWithRemoteServers,
+        HandlesDatabases;
+
+    protected Collection $databaseUsers;
+    protected Collection $databases;
+
+    public function __construct(Collection $databaseUsers, Collection $databases)
+    {
+        $this->setQueue('servers');
+
+        $this->databaseUsers = $databaseUsers;
+        $this->databases = $databases;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            $databaseUsers = DatabaseUser::query()
+                ->lockForUpdate()
+                ->findMany($this->databaseUsers->modelKeys());
+
+            $databases = Database::query()
+                ->lockForUpdate()
+                ->findMany($this->databases->modelKeys());
+
+            $this->runChecks($databaseUsers, $databases);
+
+            /** @var Server $server */
+            $server = Server::query()
+                ->lockForUpdate()
+                ->findOrFail($databases->first()->server->getKey());
+
+            $ssh = $server->sftp();
+            $script = $this->getDatabaseScript($server, 'RevokeDatabaseUserAccessToDatabaseScript');
+
+            /** @var DatabaseUser $databaseUser */
+            foreach ($databaseUsers as $databaseUser) {
+                /** @var Database $database */
+                foreach ($databases as $database) {
+                    $script->execute(
+                        $server,
+                        $database->name,
+                        $databaseUser->name,
+                        $ssh,
+                    );
+                }
+            }
+
+            $this->updateStatuses($databaseUsers, $databases);
+        }, 5);
+    }
+
+    /**
+     * Check that databases and database users aren't empty and all belong to the same server.
+     */
+    private function runChecks(Collection $databaseUsers, Collection $databases): void
+    {
+        if ($databaseUsers->isEmpty())
+            throw new RuntimeException('No database users found.');
+
+        if ($databases->isEmpty())
+            throw new RuntimeException('No databases found.');
+
+        if ($databaseUsers->pluck('server_id')->unique()->count() > 1)
+            throw new RuntimeException('The database users belong to different servers.');
+
+        if ($databases->pluck('server_id')->unique()->count() > 1)
+            throw new RuntimeException('The databases belong to different servers.');
+
+        if (! $databaseUsers->first()->server->is($databases->first()->server))
+            throw new RuntimeException('The databases and database users belong to different servers.');
+    }
+
+    /**
+     * Set statuses to CREATED, unless it is already DELETING.
+     */
+    private function updateStatuses(Collection $databaseUsers, Collection $databases): void
+    {
+        // We don't do mass updates here because we're in an asynchronous job
+        // and we need to send events anyway.
+        foreach ($databaseUsers as $databaseUser) {
+            if ($databaseUser->status !== DatabaseUser::STATUS_DELETING) {
+                $databaseUser->status = DatabaseUser::STATUS_CREATED;
+                $databaseUser->save();
+            }
+        }
+        foreach ($databases as $database) {
+            if ($database->status !== Database::STATUS_DELETING) {
+                $database->status = Database::STATUS_CREATED;
+                $database->save();
+            }
+        }
+    }
+}
