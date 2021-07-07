eb20665f0ad70a6d292d792ec6f5c3a833dd052b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-07 17:09:43 +0300

    Create DeleteDatabaseUserJob.php

---
+++ app/Jobs/Servers/DeleteDatabaseUserJob.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\HandlesDatabases;
+use App\Jobs\Traits\InteractsWithRemoteServers;
+use App\Models\DatabaseUser;
+use Illuminate\Support\Facades\DB;
+
+class DeleteDatabaseUserJob extends AbstractJob
+{
+    use InteractsWithRemoteServers,
+        HandlesDatabases;
+
+    protected DatabaseUser $databaseUser;
+
+    public function __construct(DatabaseUser $databaseUser)
+    {
+        $this->setQueue('servers');
+
+        $this->databaseUser = $databaseUser->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            /** @var DatabaseUser $databaseUser */
+            $databaseUser = DatabaseUser::query()
+                ->with('server')
+                ->lockForUpdate()
+                ->findOrFail($this->databaseUser->getKey());
+
+            $server = $databaseUser->server;
+
+            $this->getDatabaseScript(
+                $server,
+                'DeleteDatabaseUserScript',
+            )->execute(
+                $server,
+                $databaseUser->name,
+            );
+
+            $databaseUser->delete();
+        }, 5);
+    }
+}
