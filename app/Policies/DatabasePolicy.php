93df17b3c2527fd20029d944746cc9f0e3148fa0
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-07 17:09:29 +0300

    Update DatabasePolicy.php

--- app/Policies/DatabasePolicy.php
+++ app/Policies/DatabasePolicy.php
@@ -32,6 +32,6 @@ class DatabasePolicy
      */
     public function delete(User $user, Database $database): bool
     {
-        return $user->is($database->user);
+        return $user->is($database->user) && ! $database->isBusy();
     }
 }
