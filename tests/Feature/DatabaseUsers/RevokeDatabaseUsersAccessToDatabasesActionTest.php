4dd8d7b9d0b6e40029ba9a81a7477f16bf2c6f26
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-09 14:29:39 +0300

    Create RevokeDatabaseUsersAccessToDatabasesActionTest.php

---
+++ tests/Feature/DatabaseUsers/RevokeDatabaseUsersAccessToDatabasesActionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\DatabaseUsers;
+
+use Mockery;
+use Mockery\MockInterface;
+use Tests\AbstractFeatureTest;
+
+class RevokeDatabaseUsersAccessToDatabasesActionTest extends AbstractFeatureTest
+{
+    public function test_example()
+    {
+        //
+    }
+}
