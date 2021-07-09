fd0768ab7553ca62cc23020a6205a5f6397a94d3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-09 14:29:42 +0300

    Create StoreDatabaseUserActionTest.php

---
+++ tests/Feature/DatabaseUsers/StoreDatabaseUserActionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\DatabaseUsers;
+
+use Mockery;
+use Mockery\MockInterface;
+use Tests\AbstractFeatureTest;
+
+class StoreDatabaseUserActionTest extends AbstractFeatureTest
+{
+    public function test_database_user_gets_stored()
+    {
+        //
+    }
+}
