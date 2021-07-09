d8397489ee62265184fe6adf231d86721ea057e5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-09 13:14:53 +0300

    Create DeleteDatabaseUserTest.php

---
+++ tests/Feature/DatabaseUsers/DeleteDatabaseUserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\DatabaseUsers;
+
+use Tests\AbstractFeatureTest;
+
+class DeleteDatabaseUserTest extends AbstractFeatureTest
+{
+    // TODO: CRITICAL! Don't forget to implement this to include the confirmation dialog after it is made.
+
+    public function test_database_user_can_be_deleted()
+    {
+        //
+    }
+
+    public function test_database_user_from_different_server_cannot_be_deleted()
+    {
+        //
+    }
+}
