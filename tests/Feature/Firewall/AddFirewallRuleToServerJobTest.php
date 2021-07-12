d596ef90360f895bc735e6d4856df51a5a653058
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-12 12:33:53 +0300

    Create AddFirewallRuleToServerJobTest.php

---
+++ tests/Feature/Firewall/AddFirewallRuleToServerJobTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\Firewall;
+
+use App\Jobs\Servers\AddFirewallRuleToServerJob;
+use App\Models\FirewallRule;
+use App\Models\Server;
+use App\Scripts\Root\AddFirewallRuleScript;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
+use Mockery;
+use Mockery\MockInterface;
+use Tests\AbstractFeatureTest;
+
+class AddFirewallRuleToServerJobTest extends AbstractFeatureTest
+{
+    public function test_job_parameters_and_logic()
+    {
+        /** @var FirewallRule $rule */
+        $rule = FirewallRule::factory([
+            'from_ip' => '127.0.0.1',
+            'status' => FirewallRule::STATUS_ADDING,
+        ])->withServer()->create();
+
+        Bus::fake();
+        Event::fake();
+
+        $job = new AddFirewallRuleToServerJob($rule);
+
+        $this->assertEquals('servers', $job->queue);
+
+        $this->mock(AddFirewallRuleScript::class, function (MockInterface $mock) use ($rule) {
+            $mock->shouldReceive('execute')
+                ->withArgs(function (
+                    Server $serverArg,
+                    string $portArg,
+                    bool $limitArg,
+                    string $fromIpArg,
+                ) use ($rule) {
+                    return $serverArg->is($rule->server)
+                        && $portArg === $rule->port
+                        && $limitArg === false
+                        && $fromIpArg === $rule->fromIp;
+                })
+                ->once();
+        });
+
+        app()->call([$job, 'handle']);
+
+        $rule->refresh();
+
+        $this->assertEquals(FirewallRule::STATUS_ADDED, $rule->status);
+        $this->assertTrue($rule->isAdded());
+    }
+}
