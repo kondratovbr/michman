0fe230fc5d681f097adde0d9d8339eff3df6eda4
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-14 13:45:32 +0300

    Create ProviderPolicyTest.php

---
+++ tests/Feature/Providers/ProviderPolicyTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Providers;
+
+use App\Models\User;
+use App\Policies\ProviderPolicy;
+use Tests\AbstractFeatureTest;
+
+class ProviderPolicyTest extends AbstractFeatureTest
+{
+    public function test_successful_index_user_action()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var ProviderPolicy $policy */
+        $policy = $this->app->make(ProviderPolicy::class);
+
+        $result = $policy->indexUser($user, $user);
+
+        $this->assertTrue($result);
+    }
+
+    public function test_index_user_action_with_different_user()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+        /** @var User $subject */
+        $subject = User::factory()->withPersonalTeam()->create();
+
+        /** @var ProviderPolicy $policy */
+        $policy = $this->app->make(ProviderPolicy::class);
+
+        $result = $policy->indexUser($user, $subject);
+
+        $this->assertFalse($result);
+    }
+
+    public function test_successful_create_action()
+    {
+        /** @var User $user */
+        $user = User::factory()->withPersonalTeam()->create();
+
+        /** @var ProviderPolicy $policy */
+        $policy = $this->app->make(ProviderPolicy::class);
+
+        $result = $policy->create($user, $user);
+
+        $this->assertTrue($result);
+    }
+}
