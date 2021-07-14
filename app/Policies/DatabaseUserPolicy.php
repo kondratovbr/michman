1e088d509251445d2104a335b5d4374768686d2b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-14 13:33:12 +0300

    TODO

--- app/Policies/DatabaseUserPolicy.php
+++ app/Policies/DatabaseUserPolicy.php
@@ -7,8 +7,6 @@ use App\Models\Server;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-// TODO: CRITICAL! Cover with tests!
-
 class DatabaseUserPolicy
 {
     use HandlesAuthorization;
