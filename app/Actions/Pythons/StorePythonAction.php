fb831c833036337c07e4170000870a741680c6c0
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-14 13:01:39 +0300

    Update StorePythonAction.php

--- app/Actions/Pythons/StorePythonAction.php
+++ app/Actions/Pythons/StorePythonAction.php
@@ -5,19 +5,20 @@ namespace App\Actions\Pythons;
 use App\DataTransferObjects\PythonData;
 use App\Jobs\Servers\InstallPythonJob;
 use App\Models\Python;
+use App\Models\Server;
 
 // TODO: CRITICAL! Test and cover with tests!
 
 class StorePythonAction
 {
-    public function execute(PythonData $data): Python
+    public function execute(PythonData $data, Server $server): Python
     {
         $attributes = $data->toArray();
 
         $attributes['status'] = Python::STATUS_INSTALLING;
 
         /** @var Python $python */
-        $python = $data->server->pythons()->create($attributes);
+        $python = $server->pythons()->create($attributes);
 
         InstallPythonJob::dispatch($python);
 
