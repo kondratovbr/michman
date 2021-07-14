7edb2ed4640bb04bd83fba5f967253a93eae321e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-14 13:45:43 +0300

    TODOs

--- app/Policies/VcsProviderPolicy.php
+++ app/Policies/VcsProviderPolicy.php
@@ -6,8 +6,6 @@ use App\Models\User;
 use App\Models\VcsProvider;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-// TODO: CRITICAL! Cover with tests!
-
 class VcsProviderPolicy
 {
     use HandlesAuthorization;
