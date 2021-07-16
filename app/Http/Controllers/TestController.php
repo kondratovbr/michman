119296d29afddb350433d86b58e8af0fa7952930
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-16 15:44:49 +0300

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Servers\StoreServerAction;
+use App\Models\Server;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use phpseclib3\Net\SFTP;
 
 class TestController extends AbstractController
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, StoreServerAction $storeServer)
     {
-        //
+        /** @var Server $server */
+        $server = Server::query()
+            ->where('name', 'dried-grease')
+            ->firstOrFail();
 
-        return view('blank');
+        dd(
+            $server->databaseRootPassword,
+        );
+
+        die();
+
+        return view('debug.blank');
     }
 }
