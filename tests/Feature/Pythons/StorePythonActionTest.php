62a78623456236f36bd4330c353d7a9eed798e46
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-16 12:02:12 +0300

    Create StorePythonActionTest.php

---
+++ tests/Feature/Pythons/StorePythonActionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Pythons;
+
+use App\Actions\Pythons\StorePythonAction;
+use App\DataTransferObjects\PythonData;
+use App\Jobs\Servers\InstallPythonJob;
+use App\Models\Python;
+use App\Models\Server;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
+use Tests\AbstractFeatureTest;
+
+class StorePythonActionTest extends AbstractFeatureTest
+{
+    public function test_python_gets_stored()
+    {
+        /** @var Server $server */
+        $server = Server::factory()->withProvider()->create();
+
+        /** @var StorePythonAction $action */
+        $action = $this->app->make(StorePythonAction::class);
+
+        Bus::fake();
+        Event::fake();
+
+        $python = $action->execute(new PythonData(
+            version: '3_9'
+        ), $server);
+
+        $this->assertDatabaseHas('pythons', [
+            'id' => $python->id,
+            'version' => '3_9',
+            'status' => Python::STATUS_INSTALLING,
+        ]);
+
+        Bus::assertDispatched(InstallPythonJob::class);
+    }
+}
