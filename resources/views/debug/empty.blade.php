72ca0d3c1e3127f1e4fd385a23296c55a3092e47
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-16 16:30:47 +0300

    Create empty.blade.php

---
+++ resources/views/debug/empty.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="referrer" content="always">
+
+        <title>Empty Page</title>
+    </head>
+    <body></body>
+</html>
