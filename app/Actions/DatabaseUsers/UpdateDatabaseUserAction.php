5c386e2ffac96657e95e40167d22ec0d5516061c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-16 14:35:11 +0300

    Create UpdateDatabaseUserAction.php

---
+++ app/Actions/DatabaseUsers/UpdateDatabaseUserAction.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\DatabaseUsers;
+
+use App\Models\DatabaseUser;
+use Illuminate\Database\Eloquent\Collection;
+
+class UpdateDatabaseUserAction
+{
+    public function execute(DatabaseUser $databaseUser, string $newPassword, Collection $grantedDatabases): DatabaseUser
+    {
+        // TODO: CRITICAL! Implement and test!
+
+        //
+    }
+}
