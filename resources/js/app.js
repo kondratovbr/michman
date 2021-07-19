1126d4b95e614bd75a7f51a3afb02c81d72b2855
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-19 10:34:15 +0300

    Bugfix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
-Alpine.start()
 
 import focusableDialog from './components/FocusableDialog';
 window.focusableDialog = focusableDialog;
@@ -12,3 +11,5 @@ window.select = select;
 
 import searchSelect from './components/SearchSelect'
 window.searchSelect = searchSelect;
+
+Alpine.start()
