b342b0682847a020bb5bd1514bd7a4cca8f414a9
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-20 15:15:37 +0300

    Create projects.php

---
+++ resources/lang/en/projects.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'index' => [
+        'title' => 'Active Projects',
+
+        'table' => [
+            'domain' => 'Domain',
+
+            //
+        ],
+
+        //
+    ],
+
+    'create' => [
+        'title' => 'Create New Project',
+
+        'form' => [
+            'button' => 'Create Project',
+        ],
+
+        //
+    ],
+
+];
