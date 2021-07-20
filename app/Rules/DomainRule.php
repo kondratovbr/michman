36d9252bb8cb7d8c2f3f3cab8e4da550d0f2aec5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-20 15:15:32 +0300

    Create DomainRule.php

---
+++ app/Rules/DomainRule.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class DomainRule implements Rule
+{
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     */
+    public function passes($attribute, $value): bool
+    {
+        return preg_match(
+            '/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
+            $value
+        );
+    }
+
+    /**
+     * Get the validation error message.
+     */
+    public function message(): string
+    {
+        return __('validation.custom.domain');
+    }
+}
