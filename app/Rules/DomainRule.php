5c956a358d29f7cf51d067910e95f3bdcbcf7209
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-20 15:18:16 +0300

    Comment

--- app/Rules/DomainRule.php
+++ app/Rules/DomainRule.php
@@ -16,6 +16,7 @@ class DomainRule implements Rule
      */
     public function passes($attribute, $value): bool
     {
+        // https://regexr.com/3g5j0
         return preg_match(
             '/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
             $value
