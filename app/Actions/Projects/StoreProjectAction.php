26c79cb7c105c36d607d712bb226fcc5fe083df7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-21 11:55:08 +0300

    Update StoreProjectAction.php

--- app/Actions/Projects/StoreProjectAction.php
+++ app/Actions/Projects/StoreProjectAction.php
@@ -3,15 +3,43 @@
 namespace App\Actions\Projects;
 
 use App\DataTransferObjects\NewProjectData;
+use App\Jobs\Databases\CreateDatabaseJob;
+use App\Jobs\Projects\SetupProjectJob;
+use App\Jobs\Pythons\CreatePythonJob;
 use App\Models\Project;
 use App\Models\Server;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
+
+// TODO: CRITICAL! Cover with tests.
 
 class StoreProjectAction
 {
     public function execute(NewProjectData $data, Server $server): Project
     {
-        // TODO: CRITICAL! IMPLEMENT.
+        return DB::transaction(function () use ($data, $server) {
+            /** @var Server $server */
+            $server = Server::query()->lockForUpdate()->findOrFail($server->getKey());
 
-        //
+            /** @var Project $project */
+            $project = $server->projects()->create($data->toArray());
+
+            $jobs = [];
+
+            if ($server->pythons()->where('version', $data->python_version)->count() < 1) {
+                $jobs[] = new CreatePythonJob($server, $data->python_version, true);
+            }
+
+            if (
+                $data->create_database
+                && $server->databases()->where('name', $data->db_name)->count() < 1
+            ) {
+                $jobs[] = new CreateDatabaseJob($server, $data->db_name, true);
+            }
+
+            $jobs[] = new SetupProjectJob($project, $server);
+
+            Bus::chain($jobs)->dispatch();
+        }, 5);
     }
 }
