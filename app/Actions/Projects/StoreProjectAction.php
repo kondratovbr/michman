7e6ea4d9ddf411ee1df5de2682067555432e8585
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-21 11:02:47 +0300

    Create StoreProjectAction.php

---
+++ app/Actions/Projects/StoreProjectAction.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Projects;
+
+use App\DataTransferObjects\NewProjectData;
+use App\Models\Project;
+use App\Models\Server;
+
+class StoreProjectAction
+{
+    public function execute(NewProjectData $data, Server $server): Project
+    {
+        // TODO: CRITICAL! IMPLEMENT.
+
+        //
+    }
+}
