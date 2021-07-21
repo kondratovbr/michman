cb26ffcc23bf1dab735032ce53b9c922dc19ff6a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-21 14:19:54 +0300

    Update projects-index-table.blade.php

--- resources/views/projects/projects-index-table.blade.php
+++ resources/views/projects/projects-index-table.blade.php
@@ -1,3 +1,31 @@
-<div>
-    //
-</div>
+<x-table-section>
+
+    <x-slot name="title">{{ __('projects.index.title') }}</x-slot>
+
+    <x-slot name="header">
+        <x-tr-header>
+{{--            TODO: CRITICAL! Implement the SSL itself and don' forget this badge.--}}
+            <x-th>SSL</x-th>
+            <x-th>{{ __('projects.index.table.domain') }}</x-th>
+{{--            TODO: CRITICAL! Don't forget to implement these as well.--}}
+            <x-th>{{ __('projects.index.table.repo') }}</x-th>
+            <x-th>{{ __('projects.index.table.last-deployed') }}</x-th>
+        </x-tr-header>
+    </x-slot>
+
+    <x-slot name="body">
+        @foreach($projects as $project)
+            <x-tr>
+                <x-td></x-td>
+                <x-td>{{ $project->fullDomainName() }}</x-td>
+                <x-td></x-td>
+                <x-td></x-td>
+            </x-tr>
+        @endforeach
+    </x-slot>
+
+    @if($projects->isEmpty())
+        <x-slot name="title"></x-slot>
+    @endif
+
+</x-table-section>
