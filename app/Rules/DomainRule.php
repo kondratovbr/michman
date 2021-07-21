5688f6b0a2c2493d1d7a884172aa5f7926e769a2
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-21 11:02:08 +0300

    Update DomainRule.php

--- app/Rules/DomainRule.php
+++ app/Rules/DomainRule.php
@@ -4,6 +4,8 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
+// TODO: CRITICAL! Cover with tests!
+
 class DomainRule implements Rule
 {
     public function __construct()
@@ -17,7 +19,7 @@ class DomainRule implements Rule
     public function passes($attribute, $value): bool
     {
         // https://regexr.com/3g5j0
-        return preg_match(
+        return (bool) preg_match(
             '/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
             $value
         );
