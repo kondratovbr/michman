f11216b476b8f96d2da5346a2c1f75be46467664
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-21 11:02:20 +0300

    Create SupportedPythonVersionField.php

---
+++ app/Validation/Fields/SupportedPythonVersionField.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Validation\Fields;
+
+use App\Support\Arr;
+
+class SupportedPythonVersionField extends AbstractField
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this
+            ->lengthBetween(1, 10)
+            ->in(Arr::keys(config('servers.python')));
+    }
+}
