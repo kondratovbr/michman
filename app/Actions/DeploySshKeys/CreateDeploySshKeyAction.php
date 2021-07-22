8d2fb9672a2720f76fc7d3e10c028548165c8625
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-22 17:29:12 +0300

    Create CreateDeploySshKeyAction.php

---
+++ app/Actions/DeploySshKeys/CreateDeploySshKeyAction.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\DeploySshKeys;
+
+use App\Models\DeploySshKey;
+use App\Models\Project;
+use phpseclib3\Crypt\EC;
+
+class CreateDeploySshKeyAction
+{
+    public function execute(Project $project): DeploySshKey
+    {
+        $key = EC::createKey('Ed25519');
+
+        /** @var DeploySshKey $deployKey */
+        $deployKey = $project->deploySshKey()->make();
+
+        $deployKey->privateKey = $key;
+        $deployKey->publicKey = $key->getPublicKey();
+
+        $deployKey->save();
+
+        return $deployKey;
+    }
+}
