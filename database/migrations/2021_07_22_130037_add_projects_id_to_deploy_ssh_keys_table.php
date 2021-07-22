ee43a7ea41f3e8f9b653a7add02de1b7a09e0ed9
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-22 16:02:52 +0300

    Migrations bugfix

---
+++ database/migrations/2021_07_22_130037_add_projects_id_to_deploy_ssh_keys_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjectsIdToDeploySshKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('deploy_ssh_keys', function (Blueprint $table) {
+            $table->foreignId('project_id')->references('id')->on('projects');
+        });
+    }
+}
