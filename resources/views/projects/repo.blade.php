132def22bc90f6acf1de0ab9351334ea63d68d9b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-23 13:12:24 +0300

    Update repo.blade.php

--- resources/views/projects/repo.blade.php
+++ resources/views/projects/repo.blade.php
@@ -1,6 +1,12 @@
+{{--TODO: Can the interaction here be made nicer? Like some transition or an animation. Check out how Forge does.--}}
+
 <x-sub-page name="repo">
 
-    <livewire:projects.configure-repo-form :project="$project" />
+    @if(! $project->repoInstalled())
+        <livewire:projects.install-repo-form :project="$project" />
+    @else
+        <div>Repo installed!</div>
+    @endif
 
     <x-section-separator/>
 
