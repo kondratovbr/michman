96ee113fb8cd0cfdf98ee7c48a8c8410de9e49a5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-23 13:12:21 +0300

    Update ServerPolicy.php

--- app/Policies/ServerPolicy.php
+++ app/Policies/ServerPolicy.php
@@ -2,19 +2,24 @@
 
 namespace App\Policies;
 
+use App\Models\Server;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+
+// TODO: CRITICAL! Cover with tests!
 
 class ServerPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether a user is allowed to create a new server.
-     */
     public function create(User $user): bool
     {
         // For now - no limits. Later will add subscription plan based limits here.
         return true;
+    }
+
+    public function update(User $user, Server $server): bool
+    {
+        return $user->is($server->user);
     }
 }
