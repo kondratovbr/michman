4f939ca69594ec0139697f3e7b353fc409125771
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-26 12:06:14 +0300

    Update vcs.php

--- config/vcs.php
+++ config/vcs.php
@@ -26,6 +26,8 @@ return [
             'disabled' => false,
             'icon' => 'fab fa-github',
             'deploy_keys_page_url' => '',
+            // TODO: CRITICAL! I should have a scheduled command that will check that this is the current key and notify me on the emergency channel if it isn't.
+            'ssh_host_key' => 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
         ],
 
         'gitlab' => [
@@ -36,6 +38,7 @@ return [
             'disabled' => true,
             'icon' => 'fab fa-gitlab',
             'deploy_keys_page_url' => '',
+            'ssh_host_key' => null,
         ],
 
         'bitbucket' => [
@@ -46,6 +49,7 @@ return [
             'disabled' => true,
             'icon' => 'fab fa-bitbucket',
             'deploy_keys_page_url' => '',
+            'ssh_host_key' => null,
         ],
 
     ],
