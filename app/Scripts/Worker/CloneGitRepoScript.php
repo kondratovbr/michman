ca2a26858d4f9e80f5915d57212da0560ee2023c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-27 16:55:31 +0300

    Update CloneGitRepoScript.php

--- app/Scripts/Worker/CloneGitRepoScript.php
+++ app/Scripts/Worker/CloneGitRepoScript.php
@@ -5,37 +5,36 @@ namespace App\Scripts\Worker;
 use App\Models\Server;
 use App\Scripts\AbstractServerScript;
 use phpseclib3\Net\SFTP;
+use RuntimeException;
 
 class CloneGitRepoScript extends AbstractServerScript
 {
     public function execute(
         Server $server,
+        string $username,
         string $repoSshString,
         string $domain,
         string $sshHostKey,
         SFTP $ssh = null,
     ): void {
-        $user = (string) config('servers.worker_user');
-        $homedir = "/home/{$user}";
+        $homedir = "/home/{$username}";
         $knownHostsFile = "{$homedir}/.ssh/known_hosts";
         $projectDir = "{$homedir}/{$domain}";
 
-        $this->init($server, $ssh, $user);
+        $this->init($server, $ssh, $username);
 
         // Create the known_hosts file if it doesn't exist.
         $this->exec("touch {$knownHostsFile}");
         $this->exec("chmod 0644 {$knownHostsFile}");
 
         // Use grep to check if the VCS's SSH host key is already added to the known_hosts file
-        // and add if it doesn't.
+        // and add if it isn't.
         $this->exec("grep -qxF '{$sshHostKey}' {$knownHostsFile} || echo '{$sshHostKey}' >> {$knownHostsFile}");
-
-        // TODO:CRITICAL! CONTINUE. Implement. Need to figure out how to check that the repo is available from the server and show the user some feedback about it. Probably in a separate script before this one. Or rather implement a Server Alert system, like Forge does.
 
         $this->exec("git clone --single-branch --branch main --depth 1 {$repoSshString} {$projectDir}");
 
-        // code 128
-        // WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!
+        if ($this->getExitStatus() !== 0)
+            throw new RuntimeException('Cloning the project\'s git repo failed.');
 
         //
     }
