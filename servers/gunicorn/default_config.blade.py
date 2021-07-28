074e7cfc66ab84b6e5db23ec6649ddc31de4d746
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-28 17:33:07 +0300

    Create default_config.blade.py

---
+++ servers/gunicorn/default_config.blade.py
@@ -0,0 +1,8 @@
+# The number of worker processes to run.
+# Should generally be 2-4 times the server's core count,
+# if the server isn't used for anything else but running this project.
+workers = 3
+
+# A worker process will be reloaded after handling this many requests.
+# This provides basic protection from memory leaks.
+max_requests = 100
