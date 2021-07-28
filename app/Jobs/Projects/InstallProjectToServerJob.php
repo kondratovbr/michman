959c2ef3807f8295eef8805db0766e64fef9fd0d
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-28 17:33:27 +0300

    Update InstallProjectToServerJob.php

--- app/Jobs/Projects/InstallProjectToServerJob.php
+++ app/Jobs/Projects/InstallProjectToServerJob.php
@@ -6,6 +6,7 @@ use App\Jobs\AbstractJob;
 use App\Jobs\Traits\InteractsWithRemoteServers;
 use App\Models\Project;
 use App\Models\Server;
+use App\Scripts\Root\InstallGunicornScript;
 use App\Scripts\User\CloneGitRepoScript;
 use App\Scripts\User\CreateProjectVenvScript;
 use Illuminate\Support\Facades\DB;
@@ -35,9 +36,10 @@ class InstallProjectToServerJob extends AbstractJob
     public function handle(
         CloneGitRepoScript $cloneRepo,
         CreateProjectVenvScript $createVenv,
+        InstallGunicornScript $installGunicorn,
     ): void {
         DB::transaction(function () use (
-            $cloneRepo, $createVenv
+            $cloneRepo, $createVenv, $installGunicorn
         ) {
             /** @var Project $project */
             $project = Project::query()->lockForUpdate()->findOrFail($this->project->getKey());
@@ -54,10 +56,19 @@ class InstallProjectToServerJob extends AbstractJob
                 $ssh,
             );
 
+            /*
+             * TODO: CRITICAL! I'm creating a venv inside the project directory. Make sure git won't break it on git pull during deployment.
+             */
             $createVenv->execute(
                 $server,
                 $project,
                 $this->installDependencies,
+                $ssh,
+            );
+
+            $installGunicorn->execute(
+                $server,
+                $project,
                 $ssh,
             );
 
