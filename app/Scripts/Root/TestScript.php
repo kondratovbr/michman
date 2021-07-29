8ab9549b0bd3ac1a7e82f090b3a7d5e9be96e229
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-29 12:43:14 +0300

    Create TestScript.php

---
+++ app/Scripts/Root/TestScript.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+
+class TestScript extends AbstractServerScript
+{
+    public function execute(
+        Server $server,
+        Project $project,
+        SFTP $ssh = null,
+    ): void {
+        $username = $project->serverUsername;
+        $domain = $project->domain;
+        $workdir = "/home/{$username}/{$domain}";
+
+        $this->init($server, $ssh, $username);
+
+        $this->enablePty();
+        $this->setTimeout(60);
+
+        $this->execPty("cd {$workdir} && source venv/bin/activate && pip --quiet install -r requirements.txt");
+        dump($this->read());
+
+        $this->disablePty();
+    }
+}
