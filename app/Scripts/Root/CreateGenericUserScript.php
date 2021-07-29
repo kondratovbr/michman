9e8c915a97f1b73d606761c720d2afe513b300c7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-29 12:42:47 +0300

    Update CreateGenericUserScript.php

--- app/Scripts/Root/CreateGenericUserScript.php
+++ app/Scripts/Root/CreateGenericUserScript.php
@@ -5,6 +5,7 @@ namespace App\Scripts\Root;
 use App\Models\Server;
 use App\Scripts\AbstractServerScript;
 use phpseclib3\Net\SFTP;
+use RuntimeException;
 
 class CreateGenericUserScript extends AbstractServerScript
 {
@@ -12,7 +13,15 @@ class CreateGenericUserScript extends AbstractServerScript
     {
         $this->init($server, $ssh ?? $server->sftp('root'));
 
+        $michmanDir = "/home/{$username}/.michman";
+
         $this->setTimeout(60);
+
         $this->exec("useradd --create-home --shell /bin/bash {$username}");
+
+        if ($this->failed())
+            throw new RuntimeException('useradd has failed.');
+
+        $this->exec("mkdir -p {$michmanDir} && chown {$username}:{$username} {$michmanDir} && chmod 0755 {$michmanDir}");
     }
 }
