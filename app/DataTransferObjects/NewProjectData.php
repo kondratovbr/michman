e056beb3fbf64839fed1f8f40275a5942c36ddfc
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-07-29 17:39:08 +0300

    Update NewProjectData.php

--- app/DataTransferObjects/NewProjectData.php
+++ app/DataTransferObjects/NewProjectData.php
@@ -9,7 +9,6 @@ class NewProjectData extends DataTransferObject
     public string $domain;
     public array $aliases = [];
     public string $type;
-    public string $root;
     public string|null $python_version = null;
     public bool $allow_sub_domains;
     public bool $create_database = false;
