7e1ec181e8b0620ce48650dc8a1954912f2aa93d
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-02 14:36:12 +0300

    Create UpdateProjectEnvironmentOnServerScript.php

---
+++ app/Scripts/User/UpdateProjectEnvironmentOnServerScript.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\User;
+
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+use RuntimeException;
+
+class UpdateProjectEnvironmentOnServerScript extends AbstractServerScript
+{
+    public function execute(Server $server, Project $project, SFTP $ssh = null): void
+    {
+        $this->init($server, $ssh, $project->serverUsername);
+
+        $envFile = "/home/{$project->serverUsername}/{$project->domain}/.env";
+
+        if (! $this->sendString($envFile, $project->environment))
+            throw new RuntimeException("Failed to send string to file: {$envFile}");
+
+        $this->exec("chmod 0600 {$envFile}");
+
+        if ($this->failed())
+            throw new RuntimeException('chmod command failed.');
+    }
+}
