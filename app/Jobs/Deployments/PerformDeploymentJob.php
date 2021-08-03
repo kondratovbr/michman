56f765a0423f522ddf7cd66477a0822a709fa874
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-03 18:15:10 +0300

    Create PerformDeploymentJob.php

---
+++ app/Jobs/Deployments/PerformDeploymentJob.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Deployments;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\InteractsWithVcsProviders;
+use App\Models\Deployment;
+use App\Models\Server;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
+
+// TODO: CRITICAL! Cover with tests!
+
+class PerformDeploymentJob extends AbstractJob
+{
+    use InteractsWithVcsProviders;
+
+    protected Deployment $deployment;
+
+    public function __construct(Deployment $deployment)
+    {
+        $this->setQueue('providers');
+
+        $this->deployment = $deployment->withoutRelations();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            /** @var Deployment $deployment */
+            $deployment = Deployment::query()
+                ->with(['project', 'servers'])
+                ->lockForUpdate()
+                ->findOrFail($this->deployment->getKey());
+            $project = $deployment->project;
+
+            $deployment->commit = $project->vcsProvider->api()->getLatestCommitHash($project->repo, $project->branch);
+            $deployment->save();
+
+            Bus::batch($deployment->servers->map(
+                fn(Server $server) => new PerformDeploymentOnServerJob($deployment, $server)
+            ))->dispatch();
+        }, 5);
+    }
+}
