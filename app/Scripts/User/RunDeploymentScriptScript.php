538d662e915152e91b45b0d4c0892414d85e482e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-04 14:07:09 +0300

    Update RunDeploymentScriptScript.php

--- app/Scripts/User/RunDeploymentScriptScript.php
+++ app/Scripts/User/RunDeploymentScriptScript.php
@@ -5,10 +5,14 @@ namespace App\Scripts\User;
 use App\Models\Deployment;
 use App\Models\Server;
 use App\Scripts\AbstractServerScript;
+use App\Support\Arr;
 use phpseclib3\Net\SFTP;
+use RuntimeException;
 
 class RunDeploymentScriptScript extends AbstractServerScript
 {
+    private const PREFIX = 'MICHMAN_';
+
     public function execute(Server $server, Deployment $deployment, SFTP $ssh = null): void
     {
         $project = $deployment->project;
@@ -16,6 +20,23 @@ class RunDeploymentScriptScript extends AbstractServerScript
         $this->init($server, $ssh, $project->serverUsername);
 
         // TODO: CRITICAL! CONTINUE.
+
+        // TODO: CRITICAL! Add some more variables here for the user's convenience and list them in docs.
+        // Environment variables that will be available for the deploy script.
+        $michmanVars = [
+            'PROJECT_DIR' => $project->projectDir,
+            'PROJECT_BRANCH' => $project->branch,
+            //
+        ];
+
+        $this->exec(
+            implode(' ', Arr::mapWithKeys($michmanVars,
+                fn(string $name, string $value) => "{$name}=\"{$value}\""
+            )) . " && source {$project->envFilePath} && cd {$project->projectDir} && {$project->deployScriptFilePath}"
+        );
+
+        if ($this->failed())
+            throw new RuntimeException('Deployment script execution failed.');
 
         //
     }
