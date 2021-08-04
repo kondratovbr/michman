c39692aa518f0beb8c4de0e05864184d579d6b32
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-04 17:27:04 +0300

    Create michman-placeholder.blade.php

---
+++ resources/views/michman-placeholder.blade.php
@@ -0,0 +1,29 @@
+{{--TODO: CRITICAL! Make this placeholder page nicer and better for SEO. Maybe add some content and some links to out main page and blog.--}}
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="referrer" content="always">
+
+{{--        TODO: CRITICAL! Don't forget to update fonts. Maybe just use OS ones, as usual.--}}
+        {{-- Fonts --}}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        {{-- Styles --}}
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        {{-- Page-specific --}}
+        <title>Michman Placeholder Page</title>
+        <link rel="canonical" href="{{ $pageUrl }}">
+        <meta name="description" content="This is a project managed by Michman - no hassle Python and Django deployment tool.">
+{{--        TODO: CRITICAL! Add SEO and misc metas. Don't forget to fill out these "canonical" and "description"! Also, favicons! --}}
+    </head>
+    <body class="font-sans antialiased w-screen overflow-x-hidden text-gray-100 bg-navy-100">
+
+        Oy! Michman reporting!
+
+    </body>
+</html>
