2ea000ab3a1eb311c83e5ce0c9482377a4bf6997
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-05 17:29:11 +0300

    TODO

--- app/Actions/Projects/InstallProjectRepoAction.php
+++ app/Actions/Projects/InstallProjectRepoAction.php
@@ -73,7 +73,7 @@ class InstallProjectRepoAction
                 $jobs[] = new AddServerSshKeyToVcsJob($server, $vcsProvider);
             }
 
-            // TODO: CRITICAL! CONTINUE. Implement a notification system similar to the Forge's one to notify users about various mishaps with their servers and projects. Forge calls it "Server Alerts".
+            // TODO: CRITICAL! Implement a notification system similar to the Forge's one to notify users about various mishaps with their servers and projects. Forge calls it "Server Alerts".
 
             $jobs[] = new InstallProjectToServerJob($project, $server, $installDependencies);
 
