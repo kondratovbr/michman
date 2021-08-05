813eab8c95544be15729d409bb4473849cad1613
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-05 15:31:03 +0300

    Create UninstallProjectRepoAction.php

---
+++ app/Actions/Projects/UninstallProjectRepoAction.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Projects;
+
+use App\Models\Project;
+
+class UninstallProjectRepoAction
+{
+    public function execute(Project $project): void
+    {
+        //
+    }
+}
