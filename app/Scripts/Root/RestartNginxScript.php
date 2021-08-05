c58d5d6a20d76f485ccffefa03471caf5777e640
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-05 15:31:22 +0300

    Create RestartNginxScript.php

---
+++ app/Scripts/Root/RestartNginxScript.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use App\Support\Str;
+use phpseclib3\Net\SFTP;
+use RuntimeException;
+
+class RestartNginxScript extends AbstractServerScript
+{
+    public function execute(Server $server, SFTP $ssh = null): void
+    {
+        $this->init($server, $ssh);
+
+        $this->exec("systemctl restart nginx");
+        if ($this->failed())
+            throw new RuntimeException('systemctl command to restart Nginx has failed.');
+
+        // Wait a bit for Nginx to be started by systemd.
+        $this->setTimeout(60);
+        $this->exec('sleep 30');
+
+        // Verify that Nginx has started.
+        $output = $this->exec('systemctl status nginx');
+        if (
+            ! Str::contains(Str::lower($output), 'active (running)')
+            || $this->failed()
+        ) {
+            throw new RuntimeException('Nginx failed to start.');
+        }
+    }
+}
