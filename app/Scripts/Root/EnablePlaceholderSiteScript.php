07f43228b219cf449349bee8ed5aeddae8ed4d63
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-05 15:31:16 +0300

    Create EnablePlaceholderSiteScript.php

---
+++ app/Scripts/Root/EnablePlaceholderSiteScript.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Facades\ConfigView;
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+use RuntimeException;
+
+class EnablePlaceholderSiteScript extends AbstractServerScript
+{
+    public function execute(Server $server, Project $project, SFTP $ssh = null): void
+    {
+        $this->init($server, $ssh);
+
+        $available = "/etc/nginx/sites-available";
+        $enabled = "/etc/nginx/sites-enabled";
+        $file = "{$project->projectName}_placeholder.conf";
+
+        if (! $this->sendString(
+            "{$available}/{$file}",
+            ConfigView::render('nginx.server_placeholder', [
+                'project' => $project,
+                'server' => $server,
+            ]),
+        )) {
+            throw new RuntimeException('Command to upload Nginx placeholder config has failed.');
+        }
+
+        $this->exec("ln -sf {$available}/{$file} {$enabled}/{$file}");
+        if ($this->failed())
+            throw new RuntimeException('Failed to create a symlink to Nginx placeholder config.');
+    }
+}
