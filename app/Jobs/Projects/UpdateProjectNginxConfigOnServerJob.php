951e81b93bd2ae501f2abf934cf157174040f976
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-10 17:40:04 +0300

    Create UpdateProjectNginxConfigOnServerJob.php

---
+++ app/Jobs/Projects/UpdateProjectNginxConfigOnServerJob.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Projects;
+
+use App\Jobs\AbstractRemoteServerJob;
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\User\UpdateProjectNginxConfigOnServerScript;
+use Illuminate\Bus\Batchable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class UpdateProjectNginxConfigOnServerJob extends AbstractRemoteServerJob
+{
+    use Batchable;
+
+    protected Project $project;
+
+    public function __construct(Server $server, Project $project)
+    {
+        parent::__construct($server);
+
+        $this->project = $project->withoutRelations();
+    }
+
+    public function handle(UpdateProjectNginxConfigOnServerScript $script): void
+    {
+        DB::transaction(function () use ($script) {
+            $server = $this->lockServer();
+
+            if (! $server->projects->contains($this->project)) {
+                Log::warning('UpdateProjectNginxConfigOnServerJob: The project is no longer deployed on this server.');
+                return;
+            }
+
+            $script->execute($server, $this->project);
+        }, 5);
+    }
+}
