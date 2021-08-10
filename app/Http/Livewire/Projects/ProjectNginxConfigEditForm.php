7dbd8fd87461056b029858879f3139140fac3966
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-10 17:40:19 +0300

    Create project Nginx config editing form

---
+++ app/Http/Livewire/Projects/ProjectNginxConfigEditForm.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Livewire\Projects;
+
+use App\Actions\Projects\UpdateProjectNginxConfigAction;
+use App\Http\Livewire\Traits\TrimsInputBeforeValidation;
+use App\Models\Project;
+use App\Validation\Rules;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Livewire\Component as LivewireComponent;
+
+class ProjectNginxConfigEditForm extends LivewireComponent
+{
+    use AuthorizesRequests,
+        TrimsInputBeforeValidation;
+
+    public Project $project;
+
+    public string $nginxConfig = '';
+
+    protected function prepareForValidation($attributes): array
+    {
+        // This will be used as a content of a file, so would be nice to make sure it ends with a newline character.
+        // It won't have more than one since the attributes are trimmed before getting to this method.
+        $attributes['nginxConfig'] = $attributes['nginxConfig'] . "\n";
+
+        return $attributes;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nginxConfig' => Rules::string()->nullable(),
+        ];
+    }
+
+    public function mount(): void
+    {
+        $this->authorize('update', $this->project);
+
+        $this->resetState();
+    }
+
+    protected function resetState(): void
+    {
+        $this->nginxConfig = $this->project->refresh()->nginxConfig ?? '';
+    }
+
+    /**
+     * Update the project's Nginx config.
+     */
+    public function update(UpdateProjectNginxConfigAction $action): void
+    {
+        $nginxConfig = $this->validate()['nginxConfig'] ?? '';
+
+        $this->authorize('update', $this->project);
+
+        $action->execute($this->project, $nginxConfig);
+
+        $this->resetState();
+    }
+
+    public function render(): View
+    {
+        return view('projects.project-nginx-config-edit-form');
+    }
+}
---
+++ resources/views/projects/project-nginx-config-edit-form.blade.php
@@ -0,0 +1,29 @@
+{{--TODO: CRITICAL! Add some explanation and a link to docs (with more detailed explanation).--}}
+
+<x-form-section submit="store">
+
+    <x-slot name="title">{{ __('projects.deployment.nginx-config.title') }}</x-slot>
+
+    <x-slot name="form">
+        <x-field class="mt-6">
+            <x-editor
+                wire:model="nginxConfig"
+{{--                mode="sh"--}}
+            >{{ $nginxConfig }}</x-editor>
+        </x-field>
+    </x-slot>
+
+    <x-slot name="actions">
+        <div class="flex items-center space-x-3">
+            <x-buttons.secondary
+                wire:click.prevent="reload"
+                wire:loading.attr="disabled"
+            >{{ __('buttons.reload') }}</x-buttons.secondary>
+            <x-buttons.primary
+                wire:click.prevent="update"
+                wire:loading.attr="disabled"
+            >{{ __('buttons.save') }}</x-buttons.primary>
+        </div>
+    </x-slot>
+
+</x-form-section>
