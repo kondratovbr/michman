129bd8ff099625a0a59dbe0770c4639670f4208d
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-16 14:39:50 +0300

    Create deployments.php

---
+++ resources/lang/en/deployments.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Deployment;
+
+return [
+
+    'deploy-button' => 'Deploy Now',
+
+    'table' => [
+        'title' => 'Recent Deployments',
+        'empty' => 'The project hasn\'t been deployed yet.',
+        'started-at' => 'Started At',
+        'status' => 'Status',
+        'commit' => 'Commit',
+        'branch' => 'Branch',
+        'duration' => 'Duration',
+    ],
+
+    'statuses' => [
+        Deployment::STATUS_PENDING => 'Pending',
+        Deployment::STATUS_WORKING => 'In Progress',
+        Deployment::STATUS_COMPLETED => 'Completed',
+        Deployment::STATUS_FAILED => 'Failed',
+    ],
+
+];
