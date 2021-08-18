820420c42e63a7fef4e95bf73ba9c4653093baf9
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-18 16:17:22 +0300

    Refactoring

--- resources/views/components/code-block.blade.php
+++ resources/views/components/code-block.blade.php
@@ -1,12 +1,14 @@
 {{--TODO: Looks a bit meh. Any way to improve?--}}
+{{--TODO: CRITICAL! I've changed this thing a bit. Check that works in every place I use it.--}}
 
 @props(['wrap' => false])
 
-<pre {{ $attributes->class([
+<div {{ $attributes->class([
     'bg-code-bg rounded-md',
     'px-3-em py-2-em',
     'text-code-text font-mono',
-    $wrap
-        ? 'whitespace-pre-wrap break-all'
-        : 'whitespace-pre overflow-x-scroll',
-]) }}><code>{{ $slot }}</code></pre>
+]) }}>
+    <pre
+        class="{{ $wrap ? 'whitespace-pre-wrap break-all' : 'whitespace-pre overflow-x-scroll' }}"
+    ><code>{{ $slot }}</code></pre>
+</div>
