fccfc1606a3ba821f83333ec5e6be15024ea3621
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-24 15:01:45 +0300

    Update DeploymentSeeder.php

--- database/seeders/DeploymentSeeder.php
+++ database/seeders/DeploymentSeeder.php
@@ -2,15 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Deployment;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class DeploymentSeeder extends Seeder
 {
+    /** @var int Number of deployments to seed. */
+    const NUM_DEPLOYMENTS=20;
+
     /**
      * Seed the database.
      */
     public function run(): void
     {
-        //
+        Deployment::factory()
+            ->forRandomProjectFrom(Project::all())
+            ->count(self::NUM_DEPLOYMENTS)
+            ->create();
     }
 }
