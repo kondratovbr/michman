f9fe0078884be5d6c717b5924efd8d52612cc007
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-24 15:01:35 +0300

    Create ProjectSeeder.php

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /** @var int Number of fake projects to seed. */
+    private const NUM_PROJECTS = 10;
+
+    /**
+     * Seed the database.
+     */
+    public function run(): void
+    {
+        Project::factory()
+            ->forRandomUserFromCollection(User::query()->whereHas('servers')->get())
+            ->count(self::NUM_PROJECTS)
+            ->create();
+    }
+}
