142a0ae555dc0abebd29c502f89abbc9067896c1
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-28 21:09:52 +0300

    Update ProjectView.php

--- app/Http/Livewire/ProjectView.php
+++ app/Http/Livewire/ProjectView.php
@@ -14,6 +14,7 @@ class ProjectView extends AbstractSubpagesView
         'deployment' => 'projects.deployment',
         'config' => 'projects.config',
         'queue' => 'projects.queue',
+        'ssl' => 'projects.ssl',
         //
     ];
 
