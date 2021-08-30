2ba4a0c4f2e2b52fd2b97e3f0fc4d8595787baac
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-30 17:14:51 +0300

    Cleanup

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\Deployments\DeploymentFailedEvent;
-use App\Models\Deployment;
 use Illuminate\Http\Request;
 
 class TestController extends AbstractController
 {
     public function __invoke(Request $request)
     {
-        /** @var Deployment $deployment */
-        $deployment = Deployment::query()->latest()->firstOrFail();
-
-        DeploymentFailedEvent::dispatch($deployment);
+        //
 
         return view('debug.blank');
     }
