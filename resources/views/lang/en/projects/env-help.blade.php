739b510281c98fc93fedc463cd122201d2d20b2a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-31 15:15:52 +0300

    Update env-help.blade.php

--- resources/views/lang/en/projects/env-help.blade.php
+++ resources/views/lang/en/projects/env-help.blade.php
@@ -1,7 +1,7 @@
 <div>
     Here you may edit the <x-code>.env</x-code> file for your application,
     which is the default environment file that is loaded by packages like
-    <x-code>django-environ</x-code> or <x-code>python-decouple</x-code>
+    <x-code>django-environ</x-code>, <x-code>django-dotenv</x-code> or <x-code>python-decouple</x-code>
     The file will not be readable by any users apart from the one running your application,
     so you can use it to store sensitive data, like a database password.
     If the application is uninstalled, the environment file will also be removed.
