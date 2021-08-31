8bfce5142f2dc68374097fd7bc320ede3b21afea
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-08-31 11:33:02 +0300

    Update ssl.blade.php

--- resources/views/projects/ssl.blade.php
+++ resources/views/projects/ssl.blade.php
@@ -1,9 +1,9 @@
 <x-sub-page name="ssl">
 
-    SSL!
+    <livewire:certificates.create-lets-encrypt-certificate-form :project="$project" />
 
     <x-section-separator/>
 
-    //
+    <livewire:certificates.certificates-index-table :project="$project" />
 
 </x-sub-page>
