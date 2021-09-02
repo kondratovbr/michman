fae728c5764c61ca38b73ecdc7a446395426dad3
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-02 15:54:25 +0300

    Update DeleteCertificateAction.php

--- app/Actions/Certificates/DeleteCertificateAction.php
+++ app/Actions/Certificates/DeleteCertificateAction.php
@@ -2,9 +2,10 @@
 
 namespace App\Actions\Certificates;
 
-use App\Jobs\Certificates\DeleteCertificateJob;
+use App\Jobs\Certificates\DeleteLetsEncryptCertificateJob;
 use App\Models\Certificate;
 use Illuminate\Support\Facades\DB;
+use RuntimeException;
 
 class DeleteCertificateAction
 {
@@ -16,7 +17,11 @@ class DeleteCertificateAction
             $certificate->status = Certificate::STATUS_DELETING;
             $certificate->save();
 
-            DeleteCertificateJob::dispatch($certificate);
+            // TODO: CRITICAL! Don't forget to implement the rest as well, if I have any.
+            if ($certificate->type !== Certificate::TYPE_LETS_ENCRYPT)
+                throw new RuntimeException("The certificate with ID $certificate->id is not of the 'lets-encrypt' type, but deletion is implemented for that type only.");
+
+            DeleteLetsEncryptCertificateJob::dispatch($certificate);
         }, 5);
     }
 }
