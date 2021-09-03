05c5ecc05fdfbf3ac6f36dca9311fb85ae1352c5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-03 14:24:20 +0300

    Scaffold Daemon model

---
+++ app/Models/Daemon.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Daemon Eloquent model
+ *
+ * Represents a supervisord-managed program running on a server.
+ *
+ * @property int $id
+ * @property string $command
+ * @property string $user
+ * @property string|null $directory
+ * @property int $processes
+ * @property int $start_seconds
+ * @property CarbonInterface $c
+ */
+class Daemon extends AbstractModel
+{
+    use HasFactory;
+
+    /** @var string[] The attributes that are mass assignable. */
+    protected $fillable = [
+        'command',
+        'user',
+        'directory',
+        'processes',
+        'start_seconds',
+    ];
+
+    /** @var string[] The attributes that should be visible in arrays and JSON. */
+    protected $visible = [];
+
+    /** @var string[] The attributes that should be cast to native types with their respective types. */
+    protected $casts = [
+        //
+    ];
+
+    /** @var string[] The event map for the model. */
+    protected $dispatchesEvents = [
+        //
+    ];
+}
---
+++ database/migrations/2021_09_03_110328_create_daemons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDaemonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('daemons', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('server_id')->references('id')->on('servers');
+
+            $table->string('command');
+            $table->string('user');
+            $table->string('directory')->nullable();
+            $table->integer('processes');
+            $table->integer('start_seconds');
+
+            $table->timestamps();
+        });
+    }
+}
