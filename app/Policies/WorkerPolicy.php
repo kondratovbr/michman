ebbc2e5d18ec844a8026d41b1bddf857e337df60
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-03 16:48:06 +0300

    Update WorkerPolicy.php

--- app/Policies/WorkerPolicy.php
+++ app/Policies/WorkerPolicy.php
@@ -2,12 +2,27 @@
 
 namespace App\Policies;
 
+use App\Models\Project;
 use App\Models\User;
+use App\Models\Worker;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class WorkerPolicy
 {
     use HandlesAuthorization;
 
-    //
+    public function create(User $user, Project $project): bool
+    {
+        return $user->is($project->user);
+    }
+
+    public function index(User $user, Project $project): bool
+    {
+        return $user->is($project->user);
+    }
+
+    public function delete(User $user, Worker $worker): bool
+    {
+        return $user->is($worker->user);
+    }
 }
