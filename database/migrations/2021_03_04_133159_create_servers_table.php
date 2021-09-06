00d64db76bf60cfc307525cd93a002641f19a821
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-06 14:20:47 +0300

    Cleanup

--- database/migrations/2021_03_04_133159_create_servers_table.php
+++ database/migrations/2021_03_04_133159_create_servers_table.php
@@ -11,8 +11,6 @@ class CreateServersTable extends Migration
      */
     public function up()
     {
-        // TODO: IMPORTANT! Don't forget to add all new fields to tests.
-
         Schema::create('servers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('provider_id')->references('id')->on('providers');
