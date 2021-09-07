480d15d9c1a3ec768e5090ab1b93f58172dff1f7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-07 16:48:54 +0300

    Create DaemonPolicy.php

---
+++ app/Policies/DaemonPolicy.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Daemon;
+use App\Models\Server;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class DaemonPolicy
+{
+    use HandlesAuthorization;
+
+    public function create(User $user, Server $server): bool
+    {
+        return $user->is($server->user);
+    }
+
+    public function delete(User $user, Daemon $daemon): bool
+    {
+        return $user->is($daemon->user);
+    }
+}
