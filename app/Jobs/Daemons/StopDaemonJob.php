a0a9b5f64fadc20bd8450dd1ae4d1dc78d4a6a37
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-08 17:40:09 +0300

    Create StopDaemonJob.php

---
+++ app/Jobs/Daemons/StopDaemonJob.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Daemons;
+
+use App\Jobs\AbstractRemoteServerJob;
+use App\Models\Daemon;
+use App\Scripts\Root\StopDaemonScript;
+use Illuminate\Support\Facades\DB;
+
+// TODO: CRITICAL! Cover with tests!
+
+class StopDaemonJob extends AbstractRemoteServerJob
+{
+    protected Daemon $daemon;
+
+    public function __construct(Daemon $daemon)
+    {
+        parent::__construct($daemon->server);
+
+        $this->daemon = $daemon->withoutRelations();
+    }
+
+    public function handle(StopDaemonScript $script): void
+    {
+        DB::transaction(function () use ($script) {
+            $server = $this->server->freshSharedLock();
+            $daemon = $this->daemon->freshLockForUpdate();
+
+            if (! $daemon->isStatus(Daemon::STATUS_STOPPING))
+                return;
+
+            $script->execute($server, $daemon);
+
+            $daemon->status = Daemon::STATUS_STOPPED;
+            $daemon->save();
+        }, 5);
+    }
+}
