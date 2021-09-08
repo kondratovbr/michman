498a38f4a7cb2c896c7670f38446f3fa91f9468b
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-08 17:40:02 +0300

    Update DaemonsIndexTable.php

--- app/Http/Livewire/Daemons/DaemonsIndexTable.php
+++ app/Http/Livewire/Daemons/DaemonsIndexTable.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Livewire\Daemons;
 
+use App\Actions\Daemons\DeleteDaemonAction;
+use App\Actions\Daemons\RestartDaemonAction;
+use App\Actions\Daemons\StartDaemonAction;
+use App\Actions\Daemons\StopDaemonAction;
 use App\Broadcasting\ServerChannel;
 use App\Events\Daemons\DaemonCreatedEvent;
 use App\Events\Daemons\DaemonDeletedEvent;
@@ -43,15 +47,59 @@ class DaemonsIndexTable extends LivewireComponent
 
     public function mount(): void
     {
-        $this->authorize('create', [Daemon::class, $this->server]);
+        $this->authorize('index', [Daemon::class, $this->server]);
     }
 
     /**
-     * Store the newly configured daemon.
+     * Stop a running daemon.
      */
-    public function store(): void
+    public function stop(string $daemonKey, StopDaemonAction $action): void
     {
-        //
+        $daemon = Daemon::validated($daemonKey, $this->daemons);
+
+        $this->authorize('update', [Daemon::class, $daemon]);
+
+        $action->execute($daemon);
+    }
+
+    /**
+     * Start a stopped daemon.
+     */
+    public function start(string $daemonKey, StartDaemonAction $action): void
+    {
+        $daemon = Daemon::validated($daemonKey, $this->daemons);
+
+        $this->authorize('update', [Daemon::class, $daemon]);
+
+        $action->execute($daemon);
+    }
+
+    /**
+     * Restart a daemon.
+     */
+    public function restart(string $daemonKey, RestartDaemonAction $action): void
+    {
+        $daemon = Daemon::validated($daemonKey, $this->daemons);
+
+        $this->authorize('update', [Daemon::class, $daemon]);
+
+        $action->execute($daemon);
+    }
+
+    /**
+     * Stop and delete a daemon.
+     */
+    public function delete(string $daemonKey, DeleteDaemonAction $action): void
+    {
+        $daemon = Daemon::validated($daemonKey, $this->daemons);
+
+        $this->authorize('delete', [Daemon::class, $daemon]);
+
+        // TODO: CRITICAL! CONTINUE. In other places where I delete stuff I should also check that the stuff isn't already in the process of being deleted. Don't forget to add this case to the tests. Or maybe I should do those checks in the actions?
+        if ($daemon->isStatus(Daemon::STATUS_DELETING))
+            return;
+
+        $action->execute($daemon);
     }
 
     public function render(): View
