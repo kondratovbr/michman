56ac4691508525fc1af0af1591444e6c73502c8e
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-13 11:39:13 +0300

    Fix dropdown menus not fitting inside tables

--- resources/views/components/table-section.blade.php
+++ resources/views/components/table-section.blade.php
@@ -1,18 +1,14 @@
-<div {{ $attributes->merge([
-    //'class' => 'lg:grid lg:grid-cols-3 lg:gap-6 overflow-visible',
-]) }}>
+<div {{ $attributes }}>
 
-{{--    <x-section-title class="lg:order-2 lg:col-span-1">--}}
-    <x-section-title class="">
+    <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         @isset($description)
             <x-slot name="description">{{ $description }}</x-slot>
         @endisset
     </x-section-title>
 
-{{--    TODO: IMPORTANT! The "overflow-x-auto" is a temporary crotch for tables that don't quite fit. Should do something about them.--}}
-{{--    <x-box class="lg:order-1 lg:col-span-2 mt-5 lg:mt-0 overflow-x-auto">--}}
-    <x-box class="mt-5 overflow-x-auto">
+{{--    TODO: CRITICAL! The "overflow-x-auto" here was a temporary crotch for tables that don't quite fit. Check that all tables look OK on all screens.--}}
+    <x-box class="mt-5">
         @isset($empty)
             <x-box.content>
                 {{ $empty }}
