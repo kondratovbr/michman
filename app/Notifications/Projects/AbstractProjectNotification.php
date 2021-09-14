e0ba94d41bd877b946c290ffd0f3697fb6540510
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-14 11:49:40 +0300

    Update AbstractProjectNotification.php

--- app/Notifications/Projects/AbstractProjectNotification.php
+++ app/Notifications/Projects/AbstractProjectNotification.php
@@ -10,12 +10,25 @@ abstract class AbstractProjectNotification extends AbstractNotification
 {
     public function __construct(
         protected Project $project,
-    ) {}
+    ) {
+        parent::__construct();
+    }
 
     public function toArray(User $notifiable): array
     {
         return [
             'projectKey' => $this->project->getKey(),
         ];
+    }
+
+    /**
+     * Retrieve the project from the database.
+     */
+    protected static function project(array $data): Project|null
+    {
+        /** @var Project|null $project */
+        $project = Project::query()->find($data['projectKey']);
+
+        return $project;
     }
 }
