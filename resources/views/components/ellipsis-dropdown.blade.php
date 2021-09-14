89aa1fa90894f83669332eddaa46753211477687
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-14 16:57:55 +0300

    TODO

--- resources/views/components/ellipsis-dropdown.blade.php
+++ resources/views/components/ellipsis-dropdown.blade.php
@@ -1,5 +1,6 @@
 {{--TODO: CRITICAL! Does it work on touch? Is it supposed to?--}}
 {{--TODO: Maybe figure out how to highlight the activator button when the dropdown is open somehow.--}}
+{{--TODO: IMPORTANT! My dropdowns (other as well) are a bit stupid - they drop down even when they're at the bottom of the screen. Should drop up. Maybe try some JS library with dropdowns? Like maybe even Tippy? Or just google some. Aim for no-dependency and lightweight. Does "Alpine Components" projects has it?--}}
 
 @props(['disabled' => false])
 
