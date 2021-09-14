92482234435ca5dd320f89c4cb9588e58435daa7
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-14 15:57:46 +0300

    TODOs

--- resources/views/components/ellipsis-dropdown.blade.php
+++ resources/views/components/ellipsis-dropdown.blade.php
@@ -1,4 +1,4 @@
-{{--TODO: IMPORTANT! Does it work on touch? Is it supposed to?--}}
+{{--TODO: CRITICAL! Does it work on touch? Is it supposed to?--}}
 {{--TODO: Maybe figure out how to highlight the activator button when the dropdown is open somehow.--}}
 
 @props(['disabled' => false])
@@ -13,9 +13,7 @@
     x-on:close.stop="open = false"
 >
     <x-buttons.ellipsis
-{{--        TODO: CRITICAL! Check how it looks and works in Safari. It had a bug with sizing button contents that's why I had a as link ("a" tag) instead of a button.--}}
 {{--        :link="true"--}}
-        {{-- TODO: IMPORTANT! Does this work on touch? --}}
         x-on:click.prevent="open = !open"
         role="button"
         :disabled="$disabled"
