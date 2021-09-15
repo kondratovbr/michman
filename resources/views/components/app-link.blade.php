6c22b3f35489420197b490b5994ff0b688d9a1ba
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-15 12:00:48 +0300

    Add "external" option

--- resources/views/components/app-link.blade.php
+++ resources/views/components/app-link.blade.php
@@ -1,7 +1,16 @@
+@props(['external' => false, 'icon' => true])
+
 <a {{ $attributes->class([
     'cursor-pointer font-bold text-gold-800',
     'hover:text-gold-700 hover:underline',
     'active:text-gold-500',
-]) }}>
-    {{ $slot }}
+]) }}
+    @if($external)
+        target="_blank"
+    @endif
+>
+    <span>{{ $slot }}</span>
+    @if($external && $icon)
+        <x-icon><i class="fas fa-external-link-alt"></i></x-icon>
+    @endif
 </a>
