9a1e2273271d87d82c331cd7d1e597db6a2dbae8
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-15 16:25:11 +0300

    Create DisableWebhookAction.php

---
+++ app/Actions/Projects/DisableWebhookAction.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Projects;
+
+use App\Models\Project;
+
+// TODO: CRITICAL! Cover with tests!
+
+class DisableWebhookAction
+{
+    public function execute(Project $project): void
+    {
+        // TODO: CRITICAL! CONTINUE. Implement.
+
+        //
+    }
+}
