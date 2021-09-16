38282fc6490f5076ab060fada889b746db608648
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-16 17:18:45 +0300

    Cleanup

--- app/Notifications/Projects/WebhookEnablingFailedNotification.php
+++ app/Notifications/Projects/WebhookEnablingFailedNotification.php
@@ -2,8 +2,6 @@
 
 namespace App\Notifications\Projects;
 
-use App\Notifications\Projects\AbstractProjectNotification;
-
 class WebhookEnablingFailedNotification extends AbstractProjectNotification
 {
     //
