e1c4f5adb554d6c0c28d09f471a11b0b8a31043c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-16 10:10:29 +0300

    Update quick-deploy-form.blade.php

--- resources/views/projects/quick-deploy-form.blade.php
+++ resources/views/projects/quick-deploy-form.blade.php
@@ -5,13 +5,19 @@
 
     <x-slot name="content">
 
-        <x-buttons.primary
-            wire:click.prevent="enable"
-        >{{ __('projects.quick-deploy.enable') }}</x-buttons.primary>
-
-        <x-buttons.danger
-            wire:click.prevent="disable"
-        >{{ __('projects.quick-deploy.disable') }}</x-buttons.danger>
+        @if(is_null($hook) || $hook->enabling())
+            <x-buttons.primary
+                wire:click.prevent="enable"
+                wire:loading.attr="disabled"
+                :loading="$hook?->enabling()"
+            >{{ __('projects.quick-deploy.enable') }}</x-buttons.primary>
+        @else
+            <x-buttons.danger
+                wire:click.prevent="disable"
+                wire:loading.attr="disabled"
+                :loading="$hook->deleting()"
+            >{{ __('projects.quick-deploy.disable') }}</x-buttons.danger>
+        @endif
 
     </x-slot>
 </x-action-section>
