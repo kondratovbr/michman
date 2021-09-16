1e5d747abb343f6a46c92ac1c10d59670d0bf520
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-16 10:10:45 +0300

    Add self-check

--- app/States/AbstractModelState.php
+++ app/States/AbstractModelState.php
@@ -4,12 +4,18 @@ namespace App\States;
 
 use App\Support\Arr;
 use Spatie\ModelStates\State;
+use RuntimeException;
 
 abstract class AbstractModelState extends State
 {
     public function is(string|array $states): bool
     {
+        $abstract = get_parent_class($this);
+
         foreach (Arr::wrap($states) as $state) {
+            if (! is_subclass_of($state, $abstract))
+                throw new RuntimeException('A state provided extends a different model state. Provided: ' . $state . ', $this: ' . $this::class);
+
             if ($this instanceof $state)
                 return true;
         }
