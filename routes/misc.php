3ed7658a587d723194dc0689fc27f3a484c08a69
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-17 16:08:37 +0300

    Create misc routes file

---
+++ routes/misc.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Miscellaneous Routes
+|--------------------------------------------------------------------------
+|
+| These are the routes that are neither API, nor web
+| and don't belong to any defined group.
+| Any necessary middleware and parameters should be defined per-route.
+| These routes are loaded by the RouteServiceProvider
+| with no common configuration.
+|
+*/
+
