085651b9497a314c4d8e23659e2f0407b5f4dcf5
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-21 14:16:19 +0300

    Layout fix

--- resources/views/projects/quick-deploy-form.blade.php
+++ resources/views/projects/quick-deploy-form.blade.php
@@ -5,24 +5,26 @@
 
     <x-slot name="content">
 
-        @if(is_null($this->hook) || $this->hook->isEnabling())
-            <x-buttons.primary
-                wire:click.prevent="enable"
-                wire:loading.attr="disabled"
-                :loading="$this->hook?->isEnabling()"
-            >{{ __('projects.quick-deploy.enable') }}</x-buttons.primary>
-        @else
-            <x-buttons.danger
-                wire:click.prevent="disable"
-                wire:loading.attr="disabled"
-                :loading="$this->hook->isDeleting()"
-            >{{ __('projects.quick-deploy.disable') }}</x-buttons.danger>
-        @endif
+        <x-buttons>
+            @if(is_null($this->hook) || $this->hook->isEnabling())
+                <x-buttons.primary
+                    wire:click.prevent="enable"
+                    wire:loading.attr="disabled"
+                    :loading="$this->hook?->isEnabling()"
+                >{{ __('projects.quick-deploy.enable') }}</x-buttons.primary>
+            @else
+                <x-buttons.danger
+                    wire:click.prevent="disable"
+                    wire:loading.attr="disabled"
+                    :loading="$this->hook->isDeleting()"
+                >{{ __('projects.quick-deploy.disable') }}</x-buttons.danger>
+            @endif
 
-        <x-buttons.secondary
-            wire:click.prevent="$refresh"
-            wire:loading.attr="disabled"
-        >Refresh</x-buttons.secondary>
+            <x-buttons.secondary
+                wire:click.prevent="$refresh"
+                wire:loading.attr="disabled"
+            >Refresh</x-buttons.secondary>
+        </x-buttons>
 
     </x-slot>
 </x-action-section>
