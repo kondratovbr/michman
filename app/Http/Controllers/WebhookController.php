fb696cdee8ca22f2fa1e740ed7cfb7118317760a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-22 16:37:44 +0300

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,50 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\DataTransferObjects\DeploymentDto;
-use App\Exceptions\NotImplementedException;
 use App\Http\Exceptions\InvalidWebhookSignatureException;
 use App\Http\Exceptions\WebhookEventNotSupportedException;
 use App\Models\Webhook;
+use App\Models\WebhookCall;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class WebhookController extends AbstractController
 {
-    // TODO: CRITICAL! Don't forget that services (see GitHub) want me to actually send some valid response to them with some actual info. See their docs.
+    // TODO: CRITICAL! CONTINUE. Unfinished.
 
     /** Handle a received webhook. */
-    public function __invoke(string $webhookProvider, Webhook $webhook, Request $request): mixed
+    public function __invoke(string $webhookProvider, Webhook $webhook, Request $request): Response
     {
-        ray('WebhookController::push()');
+        $this->validateWebhook($webhookProvider, $request, $webhook);
 
+        DB::transaction(function () use ($request, $webhook) {
+            /** @var WebhookCall $call */
+            $call = $webhook->calls()->create([
+                'type' => $webhook->service()->getEventName($request),
+                'url' => $request->fullUrl(),
+                'headers' => $request->headers->all(),
+                'payload' => $request->input(),
+            ]);
+
+            config("webhooks.jobs.{$call->type}")::dispatch($call);
+        }, 5);
+
+        return response(status: Response::HTTP_OK);
+
+        // TODO: CRITICAL! Don't forget that I must update the webhook when user changes the deployment branch. And when they change the repo, of course.
+    }
+
+    protected function validateWebhook(string $webhookProvider, Request $request, Webhook $webhook): void
+    {
         $this->validateWebhookProvider($webhookProvider, $webhook);
 
         $this->validateEvent($request, $webhook);
 
         $this->validateSignature($request, $webhook);
-
-        ray('WebhookController - A OK!');
-
-        return 'OK';
-
-        // TODO: CRITICAL! Don't forget that I must update the webhook when user changes the deployment branch. And when they change the repo, of course.
-
-        // TODO: CRITICAL! CONTINUE. Unfinished.
-
-        throw new NotImplementedException;
-
-        // Validation!
-        //
-
-        // /** @var Project $project */
-        // $project = Project::query()->where('webhook_url_key', $hookUrlKey)->firstOrFail();
-
-        $action->execute(new DeploymentDto(
-            //
-        ), $project);
-
-        //
     }
 
     protected function validateWebhookProvider(string $provider, Webhook $webhook): void
