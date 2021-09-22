f0470e6845a371aeb8e2b32f9a11570201950b62
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-22 13:39:07 +0300

    Add PATCH requests and refactor requests

--- app/Services/AbstractProvider.php
+++ app/Services/AbstractProvider.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Log;
+use RuntimeException;
 
 abstract class AbstractProvider
 {
@@ -20,24 +21,16 @@ abstract class AbstractProvider
         $this->identifier = $identifier;
     }
 
-    /**
-     * Get an internal config name for this server provider.
-     */
+    /** Get an internal config name for this server provider. */
     abstract protected function getConfigPrefix(): string;
 
-    /**
-     * Create a pending request with authentication configured.
-     */
+    /** Create a pending request with authentication configured. */
     abstract protected function request(): PendingRequest;
 
-    /**
-     * Get a cache prefix for this specific server provider API credentials.
-     */
+    /** Get a cache prefix for this specific server provider API credentials. */
     abstract protected function getCachePrefix(): string;
 
-    /**
-     * Determine if we can even use cache to store some data for this API credentials.
-     */
+    /** Determine if we can even use cache to store some data for this API credentials. */
     protected function canUseCache(): bool
     {
         if (! isset($this->identifier)) {
@@ -48,76 +41,68 @@ abstract class AbstractProvider
         return true;
     }
 
-    /**
-     * Get a config value for this provider using standard dot-notation.
-     */
+    /** Get a config value for this provider using standard dot-notation. */
     protected function config(string $key, mixed $default = null): mixed
     {
         return config($this->configPrefix . '.' . $key, $default);
     }
 
-    /**
-     * Get a properly prefixed cache key for some parameter related to this specific server provider API credentials.
-     */
+    /** Get a properly prefixed cache key for some parameter related to this specific server provider API credentials. */
     protected function cacheKey(string $key): string
     {
         return $this->getCachePrefix() . '.' . $key;
     }
 
-    /**
-     * Decode JSON response throwing en exceptions on failure.
-     */
+    /** Decode JSON response throwing en exceptions on failure. */
     protected function decodeJson(string $json): array|object
     {
         return json_decode($json, false, 512, JSON_THROW_ON_ERROR);
     }
 
-    /**
-     * Send a GET request to a relative path with provided parameters.
-     */
+    /** Send a request to a relative path with provided parameters. */
+    private function send(
+        string $method,
+        string $path,
+        array $parameters = [],
+        PendingRequest $pendingRequest = null,
+    ): Response {
+        $pendingRequest ??= $this->request();
+
+        if (! in_array($pendingRequest, ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']))
+            throw new RuntimeException("Unsupported HTTP method \"{$method}\" provided.");
+
+        return $pendingRequest
+            ->send($method, $this->basePath . $path, $parameters)
+            ->throw();
+    }
+
+    /** Send a GET request to a relative path with provided parameters. */
     protected function get(string $path, array $parameters = [], PendingRequest $pendingRequest = null): Response
     {
-        $pendingRequest ??= $this->request();
-
-        return $pendingRequest
-            ->get($this->basePath . $path, $parameters)
-            ->throw();
+        return $this->send('GET', $path, $parameters, $pendingRequest);
     }
 
-    /**
-     * Send a POST request to a relative path with provided parameters.
-     */
+    /** Send a POST request to a relative path with provided parameters. */
     protected function post(string $path, array $parameters = [], PendingRequest $pendingRequest = null): Response
     {
-        $pendingRequest ??= $this->request();
-
-        return $pendingRequest
-            ->post($this->basePath . $path, $parameters)
-            ->throw();
+        return $this->send('POST', $path, $parameters, $pendingRequest);
     }
 
-    /**
-     * Send a PUT request to a relative path with provided parameters.
-     */
+    /** Send a PUT request to a relative path with provided parameters. */
     protected function put(string $path, array $parameters = [], PendingRequest $pendingRequest = null): Response
     {
-        $pendingRequest ??= $this->request();
-
-        return $pendingRequest
-            ->put($this->basePath . $path, $parameters)
-            ->throw();
+        return $this->send('PUT', $path, $parameters, $pendingRequest);
     }
 
+    /** Send a PATCH request to a relative path with provided parameters. */
+    protected function patch(string $path, array $parameters = [], PendingRequest $pendingRequest = null): Response
+    {
+        return $this->send('PATCH', $path, $parameters, $pendingRequest);
+    }
 
-    /**
-     * Send a DELETE request to a relative path with provided parameters.
-     */
+    /** Send a DELETE request to a relative path with provided parameters. */
     protected function delete(string $path, array $parameters = [], PendingRequest $pendingRequest = null): Response
     {
-        $pendingRequest ??= $this->request();
-
-        return $pendingRequest
-            ->delete($this->basePath . $path, $parameters)
-            ->throw();
+        return $this->send('DELETE', $path, $parameters, $pendingRequest);
     }
 }
