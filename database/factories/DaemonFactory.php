874361927b2b5196e4aef45e2efa5e819291a842
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-23 16:37:37 +0300

    Update DaemonFactory.php

--- database/factories/DaemonFactory.php
+++ database/factories/DaemonFactory.php
@@ -3,20 +3,44 @@
 namespace Database\Factories;
 
 use App\Models\Daemon;
+use App\Models\Server;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DaemonFactory extends Factory
 {
-    /** @var string The name of the factory's corresponding model. */
     protected $model = Daemon::class;
 
-    /**
-     * Define the model's default state.
-     */
     public function definition(): array
     {
         return [
-            //
+            'command' => '/usr/bin/python3 --help',
+            'username' => 'admin',
+            'directory' => '/home/admin',
+            'processes' => 2,
+            'start_seconds' => 10,
+            'state' => 'starting',
         ];
+    }
+
+    /**
+     * Also create a server for this daemon.
+     *
+     * @return $this
+     */
+    public function withServer(): static
+    {
+        return $this->for(Server::factory()->withProvider());
+    }
+
+    /**
+     * Set the created Daemon model to this state.
+     *
+     * @return $this
+     */
+    public function inState(string $state): static
+    {
+        return $this->state([
+            'state' => $state,
+        ]);
     }
 }
