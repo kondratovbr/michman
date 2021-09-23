bd52c3f4c8d6457927ad9d58c677faf860c2327a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-23 13:10:53 +0300

    Update VerifyWebhookEnabledJob.php

--- app/Jobs/Webhooks/VerifyWebhookEnabledJob.php
+++ app/Jobs/Webhooks/VerifyWebhookEnabledJob.php
@@ -4,11 +4,17 @@ namespace App\Jobs\Webhooks;
 
 use App\Jobs\AbstractJob;
 use App\Models\Webhook;
+use App\Notifications\Projects\WebhookEnablingFailedNotification;
 use App\States\Webhooks\Enabling;
 use Illuminate\Support\Facades\DB;
 
+// TODO: CRITICAL! Cover with tests!
+
 class VerifyWebhookEnabledJob extends AbstractJob
 {
+    /** Delete the job if its models no longer exist. */
+    public bool $deleteWhenMissingModels = true;
+
     protected Webhook $hook;
 
     public function __construct(Webhook $hook)
@@ -21,14 +27,20 @@ class VerifyWebhookEnabledJob extends AbstractJob
     public function handle(): void
     {
         DB::transaction(function () {
-            $hook = $this->hook->freshSharedLock();
+            $hook = $this->hook->freshLockForUpdate('calls');
 
             if (! $hook->state->is(Enabling::class))
                 return;
 
-            // TODO: CRITICAL! CONTINUE. Somehow delete the hook if it failed and notify the user.
+            /*
+             * If the webhook is still "enabling" it means we likely didn't receive
+             * a "ping" event for some reason, i.e. something went wrong.
+             */
 
-            //
+            $hook->user->notify(new WebhookEnablingFailedNotification($hook->project));
+
+            $hook->calls()->delete();
+            $hook->delete();
         }, 5);
     }
 }
