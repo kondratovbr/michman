6b4e74c5900aaa9ef9ed270bed5f12de72be7e54
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-25 21:30:02 +0300

    Cover UpdateDaemonsStatusesAction with tests

---
+++ tests/Feature/Daemons/UpdateDaemonsStatusesActionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Daemons;
+
+use App\Actions\Daemons\UpdateDaemonsStatusesAction;
+use App\Events\Daemons\DaemonUpdatedEvent;
+use App\Jobs\Daemons\UpdateDaemonStateJob;
+use App\Models\Daemon;
+use App\Models\Server;
+use Illuminate\Bus\PendingBatch;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
+use Tests\AbstractFeatureTest;
+
+class UpdateDaemonsStatusesActionTest extends AbstractFeatureTest
+{
+    public function test_example()
+    {
+        /** @var Server $server */
+        $server = Server::factory()->withProvider()->create();
+        /** @var Collection $daemons */
+        $daemons = Daemon::factory()
+            ->for($server)
+            ->count(3)
+            ->create();
+
+        /** @var UpdateDaemonsStatusesAction $action */
+        $action = $this->app->make(UpdateDaemonsStatusesAction::class);
+
+        Bus::fake();
+        Event::fake();
+
+        $action->execute($server);
+
+        Bus::assertBatched(function (PendingBatch $batch) {
+            foreach ($batch->jobs as $job) {
+                if (! $job instanceof UpdateDaemonStateJob)
+                    return false;
+            }
+
+            return $batch->jobs->count() === 3
+                && $batch->allowsFailures()
+                && $batch->queue() === 'servers';
+        });
+        Event::assertNotDispatched(DaemonUpdatedEvent::class);
+    }
+}
