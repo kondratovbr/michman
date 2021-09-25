663462a25b6b2b04f19ad873e91b18a5aba634e6
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-25 23:28:10 +0300

    Code style

--- app/Notifications/Deployments/AbstractDeploymentNotification.php
+++ app/Notifications/Deployments/AbstractDeploymentNotification.php
@@ -21,9 +21,7 @@ abstract class AbstractDeploymentNotification extends AbstractNotification
         ];
     }
 
-    /**
-     * Retrieve the deployment model from the database.
-     */
+    /** Retrieve the deployment model from the database. */
     protected static function deployment(array $data): Deployment|null
     {
         /** @var Deployment|null $deployment */
