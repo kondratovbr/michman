d06c20796ecf85d0b4e5800a0f0b5b2030e5eba4
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-27 17:14:35 +0300

    Cover UpdateVcsProviderAction with tests

--- app/Actions/VcsProviders/UpdateVcsProviderAction.php
+++ app/Actions/VcsProviders/UpdateVcsProviderAction.php
@@ -6,8 +6,6 @@ use App\DataTransferObjects\VcsProviderDto;
 use App\Models\VcsProvider;
 use Illuminate\Support\Facades\DB;
 
-// TODO: CRITICAL! Cover with tests!
-
 class UpdateVcsProviderAction
 {
     public function execute(VcsProvider $vcsProvider, VcsProviderDto $data): VcsProvider
---
+++ tests/Feature/VcsProviders/UpdateVcsProviderActionTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\VcsProviders;
+
+use App\Actions\VcsProviders\UpdateVcsProviderAction;
+use App\DataTransferObjects\VcsProviderDto;
+use App\Events\VcsProviders\VcsProviderUpdatedEvent;
+use App\Models\VcsProvider;
+use Illuminate\Support\Facades\Event;
+use Tests\AbstractFeatureTest;
+
+class UpdateVcsProviderActionTest extends AbstractFeatureTest
+{
+    public function test_provider_gets_updated()
+    {
+        /** @var VcsProvider $vcs */
+        $vcs = VcsProvider::factory()->withUser()->create();
+
+        /** @var UpdateVcsProviderAction $action */
+        $action = $this->app->make(UpdateVcsProviderAction::class);
+
+        $data = new VcsProviderDto(
+            'github_v3', '123', 'TheOtherName', '98765'
+        );
+
+        Event::fake();
+
+        $action->execute($vcs, $data);
+
+        $this->assertDatabaseHas('vcs_providers', [
+            'id' => $vcs->id,
+            'user_id' => $vcs->user->id,
+            'provider' => 'github_v3',
+            'external_id' => '123',
+            'nickname' => 'TheOtherName',
+        ]);
+
+        $vcs->refresh();
+
+        $this->assertEquals('98765', $vcs->token);
+
+        Event::assertDispatched(VcsProviderUpdatedEvent::class);
+    }
+}
