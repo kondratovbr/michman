984fee3e8eab00640423a7b04ce36fa33b8e8621
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-27 15:45:20 +0300

    Update WorkerFactory.php

--- database/factories/WorkerFactory.php
+++ database/factories/WorkerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Server;
 use App\Models\Worker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -12,7 +13,28 @@ class WorkerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'type' => 'celery',
+            'app' => 'django_app',
+            'processes' => 1,
+            'queues' => ['one', 'two'],
+            'stop_seconds' => 10,
+            'max_tasks_per_child' => 100,
+            'max_memory_per_child' => 256,
+            'state' => 'starting',
         ];
+    }
+
+    /** @return $this */
+    public function withServer(): static
+    {
+        return $this->for(Server::factory()->withProvider());
+    }
+
+    /** @return $this */
+    public function inState(string $state): static
+    {
+        return $this->state([
+            'state' => $state,
+        ]);
     }
 }
