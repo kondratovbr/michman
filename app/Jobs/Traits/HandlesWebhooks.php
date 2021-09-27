3fabf82d3081cc89fb928b8f6afc37c5321c105f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-27 14:34:51 +0300

    Update HandlesWebhooks.php

--- app/Jobs/Traits/HandlesWebhooks.php
+++ app/Jobs/Traits/HandlesWebhooks.php
@@ -3,6 +3,8 @@
 namespace App\Jobs\Traits;
 
 use App\Jobs\AbstractJob;
+use App\Jobs\Exceptions\WrongWebhookCallTypeException;
+use App\Models\WebhookCall;
 
 /**
  * @mixin AbstractJob
@@ -12,5 +14,10 @@ trait HandlesWebhooks
     /** Delete the job if its models no longer exist. */
     public bool $deleteWhenMissingModels = true;
 
-    //
+    /** Verify that we're working with a webhook that has the type we expect. */
+    protected function verifyHookCallType(WebhookCall $call, string $exceptedType): void
+    {
+        if ($call->type !== $exceptedType)
+            throw new WrongWebhookCallTypeException($exceptedType, $call->type);
+    }
 }
