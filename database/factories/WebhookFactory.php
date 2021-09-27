6f5db5dbb5ab5d101993c39f4f4b2880149b7661
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-27 12:36:45 +0300

    Update WebhookFactory.php

--- database/factories/WebhookFactory.php
+++ database/factories/WebhookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Project;
 use App\Models\Webhook;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -12,7 +13,34 @@ class WebhookFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'provider' => 'github',
+            'type' => 'push',
+            'url' => 'http://localhost/',
+            'secret' => '1234567890',
+            'state' => 'enabling',
         ];
+    }
+
+    /** @return $this */
+    public function withProject(): static
+    {
+        return $this->for(Project::factory()->withUserAndServers());
+    }
+
+    /** @return $this */
+    public function enabled(): static
+    {
+        return $this->state([
+            'state' => 'enabled',
+            'external_id' => 123,
+        ]);
+    }
+
+    /** @return $this */
+    public function inState(string $state): static
+    {
+        return $this->state([
+            'state' => $state,
+        ]);
     }
 }
