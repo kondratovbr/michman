356b8b86e36d5f51b61c6c50669f738dc8c051d1
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-09-29 11:38:19 +0300

    Fix table cells rounding on hover

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -3,7 +3,7 @@
 @props(['show' => null])
 
 <td {{ $attributes->class([
-    'px-table-cell py-2 sm:py-3 lg:py-3',
+    'px-table-cell py-2 sm:py-3 lg:py-3 first:rounded-l-lg last:rounded-r-lg',
     match ($show) {
         'sm' => 'hidden sm:table-cell',
         'md' => 'hidden md:table-cell',
