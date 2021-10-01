90f5f7fcf7df186c3303d7f3bd8b8f5af10e0a6f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-10-01 14:30:09 +0300

    Cleanup

--- app/Events/Projects/ProjectDeletedEvent.php
+++ app/Events/Projects/ProjectDeletedEvent.php
@@ -4,10 +4,6 @@ namespace App\Events\Projects;
 
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-/*
- * TODO: CRITICAL! This event gets broadcasted only on ProjectChannel due to the fact that project has to be detached from servers before deletion - we can't get the servers it was attached to. Is this OK?
- */
-
 class ProjectDeletedEvent extends AbstractProjectEvent implements ShouldBroadcast
 {
     //
