e12211037d8f6870db16c7de95c9aa976b9ee76a
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-10-04 13:19:42 +0300

    Rename parameter for clarity

--- app/Jobs/Exceptions/WrongWebhookCallTypeException.php
+++ app/Jobs/Exceptions/WrongWebhookCallTypeException.php
@@ -7,9 +7,9 @@ use Throwable;
 
 class WrongWebhookCallTypeException extends Exception
 {
-    public function __construct(string $intendedType, string $receivedType, $code = 0, Throwable $previous = null)
+    public function __construct(string $expectedType, string $receivedType, $code = 0, Throwable $previous = null)
     {
-        $message = "Trying to handle a webhook call with a job of a non-matching type. Excepted type: {$intendedType}, call type: {$receivedType}";
+        $message = "Trying to handle a webhook call with a job of a non-matching type. Excepted type: {$expectedType}, call type: {$receivedType}";
 
         parent::__construct($message, $code, $previous);
     }
