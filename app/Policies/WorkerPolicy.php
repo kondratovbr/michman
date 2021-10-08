9753ead9f015432d9a8adead976b58ae88ae392c
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-10-08 15:11:57 +0300

    Update WorkerPolicy.php

--- app/Policies/WorkerPolicy.php
+++ app/Policies/WorkerPolicy.php
@@ -11,14 +11,19 @@ class WorkerPolicy
 {
     use HandlesAuthorization;
 
+    public function index(User $user, Project $project): bool
+    {
+        return $user->is($project->user);
+    }
+
     public function create(User $user, Project $project): bool
     {
         return $user->is($project->user);
     }
 
-    public function index(User $user, Project $project): bool
+    public function view(User $user, Worker $worker): bool
     {
-        return $user->is($project->user);
+        return $user->is($worker->user);
     }
 
     public function restart(User $user, Worker $worker): bool
