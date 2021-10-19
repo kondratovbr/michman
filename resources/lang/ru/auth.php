828a13a011173e440783802a98e718d49217d0a8
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-10-19 17:19:26 +0300

    Update auth.php

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Неверный логин или пароль.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
