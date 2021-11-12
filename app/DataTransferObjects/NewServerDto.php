80fe5e89c9455e0ec5fe9db202be150cf64b2fdf
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-11-12 13:46:25 +0300

    Code style

--- app/DataTransferObjects/NewServerDto.php
+++ app/DataTransferObjects/NewServerDto.php
@@ -5,12 +5,12 @@ namespace App\DataTransferObjects;
 class NewServerDto extends AbstractDto
 {
     public function __construct(
-        public string      $name,
-        public string      $region,
-        public string      $size,
-        public string      $type,
+        public string $name,
+        public string $region,
+        public string $size,
+        public string $type,
         public string|null $python_version = null,
-        public string      $database = 'none',
-        public string      $cache = 'none',
+        public string $database = 'none',
+        public string $cache = 'none',
     ) {}
 }
--- app/Services/ServerProviderInterface.php
+++ app/Services/ServerProviderInterface.php
@@ -25,19 +25,13 @@ use App\DataTransferObjects\SshKeyDto;
 
 interface ServerProviderInterface extends ProviderInterface
 {
-    /**
-     * Check if provided credentials are valid by trying some auth-protected GET request.
-     */
+    /** Check if provided credentials are valid by trying some auth-protected GET request. */
     public function credentialsAreValid(): bool;
 
-    /**
-     * Get server data by its ID.
-     */
+    /** Get server data by its ID. */
     public function getServer(string $serverId): ServerDto;
 
-    /**
-     * Get a collection of all servers created on this account.
-     */
+    /** Get a collection of all servers created on this account. */
     public function getAllServers(): ServerDataCollection;
 
     /**
@@ -47,29 +41,19 @@ interface ServerProviderInterface extends ProviderInterface
      */
     public function getServerPublicIp4(string $serverId): string|null;
 
-    /**
-     * Provision a new server with this server provider.
-     */
+    /** Provision a new server with this server provider. */
     public function createServer(NewServerDto $data, string $sshKeyIdentifier): ServerDto;
 
-    /**
-     * Get a collection of all regions supported by the API.
-     */
+    /** Get a collection of all regions supported by the API. */
     public function getAllRegions(): RegionDataCollection;
 
-    /**
-     * Get a collection of all sizes supported by the API.
-     */
+    /** Get a collection of all sizes supported by the API. */
     public function getAllSizes(): SizeDataCollection;
 
-    /**
-     * Get a list of regions where this account can create a server.
-     */
+    /** Get a list of regions where this account can create a server. */
     public function getAvailableRegions(): RegionDataCollection;
 
-    /**
-     * Get a list of server sizes available for this account.
-     */
+    /** Get a list of server sizes available for this account. */
     public function getAvailableSizes(): SizeDataCollection;
 
     /**
@@ -79,19 +63,13 @@ interface ServerProviderInterface extends ProviderInterface
      */
     public function getSizesAvailableInRegion(RegionDto|string $region): SizeDataCollection;
 
-    /**
-     * Add a new SSH key to the provider.
-     */
+    /** Add a new SSH key to the provider. */
     public function addSshKey(string $name, string $publicKey): SshKeyDto;
 
-    /**
-     * Get a collection of SSH keys added to this account.
-     */
+    /** Get a collection of SSH keys added to this account. */
     public function getAllSshKeys(): SshKeyDataCollection;
 
-    /**
-     * Add a new SSH key to the provider, checking if it was added before.
-     */
+    /** Add a new SSH key to the provider, checking if it was added before. */
     public function addSshKeySafely(string $name, string $publicKey): SshKeyDto;
 
     /**
