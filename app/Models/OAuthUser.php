28f801f70070ec36071da7d0cc1180903f156c37
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-11-12 16:16:39 +0300

    Scaffold OAuthUser model

---
+++ app/Models/OAuthUser.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+use Carbon\CarbonInterface;
+use Database\Factories\OAuthUserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+// TODO: CRITICAL! CONTINUE.
+
+/**
+ * OAuthUser Eloquent model
+ *
+ * Represents an OAuth account linked to some User model.
+ *
+ * @property int $id
+ * @property string $provider
+ * @property CarbonInterface $createdAt
+ * @property CarbonInterface $updatedAt
+ *
+ * @property-read User $user
+ *
+ * @method static OAuthUserFactory factory(...$parameters)
+ */
+class OAuthUser extends AbstractModel
+{
+    use HasFactory;
+
+    /** @var string[] The attributes that are mass assignable. */
+    protected $fillable = [
+        //
+    ];
+
+    /** @var string[] The attributes that should be visible in arrays and JSON. */
+    protected $visible = [
+        //
+    ];
+
+    /** @var string[] The attributes that should be cast to native types with their respective types. */
+    protected $casts = [
+        //
+    ];
+
+    /** @var string[] The accessors to append to the model's array form. */
+    protected $appends = [
+        //
+    ];
+
+    /** @var string[] The event map for the model. */
+    protected $dispatchesEvents = [
+        //
+    ];
+
+    /** Get a user this OAuth account if linked to. */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/OAuthUserFactory.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\OAuthUser;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OAuthUserFactory extends Factory
+{
+    protected $model = OAuthUser::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
