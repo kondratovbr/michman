91e953707f056913ebd6f6e06cbb67a9e026da8f
Bogdan Kondratov <5469212+KondorB@users.noreply.github.com>
2021-11-24 17:33:22 +0300

    Update flash.php

--- resources/lang/en/flash.php
+++ resources/lang/en/flash.php
@@ -4,7 +4,7 @@ return [
 
     'vcs-provider-linked' => 'Your :vcs account is successfully linked.',
     'vcs-provider-updated' => 'Link to your :vcs account is updated.',
-    'vcs-provider-link-failed-different-account' => 'Your Michman account is currently linked to a different :vcs account. You have to unlink it first.',
+    'vcs-provider-link-failed-different-account' => 'Your Michman account is currently linked to a different :vcs account. Unlink it first if you wish to change it.',
     'oauth-failed-email-taken' => 'The email you\'re using on :oauth_provider is already used by an existing account. You can link the accounts together in the account settings.',
     'oauth-linked' => 'Your :oauth account is successfully linked.',
     'set-up-password-to-disable-oauth' => 'Set up a password to completely disable OAuth.',
