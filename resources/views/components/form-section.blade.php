7968c43ad8562394cda88cacda27db5e9ccf1365
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2021-12-27 11:30:04 +0300

    Cleanup

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,18 +1,16 @@
 @props(['submit'])
 
 <div {{ $attributes->merge([
-    //'class' => 'lg:grid lg:grid-cols-3 lg:gap-6',
+    //
 ]) }}>
 
-{{--    <x-section-title class="lg:order-2 lg:col-span-1">--}}
-    <x-section-title class="">
+    <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         @isset($description)
             <x-slot name="description">{{ $description }}</x-slot>
         @endisset
     </x-section-title>
 
-{{--    <x-box class="lg:order-1 lg:col-span-2 mt-5 lg:mt-0">--}}
     <x-box class="mt-5">
         <x-box.form wire:submit.prevent="{{ $submit }}">
 
