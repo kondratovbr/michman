58be8c3c6f21078732c1cb906d3d71edae746088
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-04 17:26:50 +0300

    TODOs

--- resources/views/notifications/deployment-completed.blade.php
+++ resources/views/notifications/deployment-completed.blade.php
@@ -1,5 +1,5 @@
 {{--TODO: IMPORTANT! This view need to accommodate logs from multiple servers.--}}
-{{--TODO: CRITICAL! CONTINUE. Check this out in the UI and figure out if something more needs to be shown. It probably is. --}}
+{{--TODO: IMPORTANT! I can certainly add more useful info to this view.--}}
 
 <div class="min-h-0 flex flex-col space-y-2">
 
--- resources/views/notifications/deployment-failed.blade.php
+++ resources/views/notifications/deployment-failed.blade.php
@@ -1,5 +1,5 @@
 {{--TODO: IMPORTANT! This view need to accommodate logs from multiple servers.--}}
-{{--TODO: CRITICAL! CONTINUE. This is just a placeholder. This view obviously needs more information than just logs here. See what Forge shows for a failed deployment alert.--}}
+{{--TODO: IMPORTANT! I can certainly add more useful info to this view.--}}
 
 <div class="min-h-0 flex flex-col space-y-2">
 
