2e20bd570fb3e9408da436364cb3baf66c7b3a1b
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-05 23:17:45 +0300

    Temporarily disable status icons in navbar dropdowns

--- resources/views/partials/_projects-dropdown.blade.php
+++ resources/views/partials/_projects-dropdown.blade.php
@@ -15,9 +15,10 @@
                 href="{{ route('projects.show', [$project, 'deployment']) }}"
                 :capitalize="false"
             >
+{{--                TODO: IMPORTANT! Implement and enable this icon.--}}
 {{--                TODO: This icon looks rather dull. Any better options? Maybe some effects or slight animations? Should google it.--}}
-{{--                TODO: CRITICAL! This icon should reflect project's status. Like, blink amber if something is wrong, for example. Also, maybe replace with a spinner when the project is being configured and make the link disabled. Maybe also make a spinner for when the project is being deployed.--}}
-                <x-slot name="icon"><i class="fas fa-circle text-green-500 text-xs"></i></x-slot>
+{{--                TODO: This icon should reflect project's status. Like, blink amber if something is wrong, for example. Also, maybe replace with a spinner when the project is being configured and make the link disabled. Maybe also make a spinner for when the project is being deployed.--}}
+{{--                <x-slot name="icon"><i class="fas fa-circle text-green-500 text-xs"></i></x-slot>--}}
                 {{ $project->domain }}
             </x-dropdown.link>
         @endforeach
--- resources/views/partials/_servers-dropdown.blade.php
+++ resources/views/partials/_servers-dropdown.blade.php
@@ -15,9 +15,10 @@
                 href="{{ route('servers.show', $server) }}"
                 :capitalize="false"
             >
+{{--                TODO: IMPORTANT! Implement and enable this icon.--}}
 {{--                TODO: This icon looks rather dull. Any better options? Maybe some effects or slight animations? Should google it.--}}
-{{--                TODO: CRITICAL! This icon should reflect server's status. Like, blink amber if something is wrong, for example. Also, maybe replace with a spinner when the server is in the process of creation and make the link disabled.--}}
-                <x-slot name="icon"><i class="fas fa-circle text-green-500 text-xs"></i></x-slot>
+{{--                TODO: This icon should reflect server's status. Like, blink amber if something is wrong, for example. Also, maybe replace with a spinner when the server is in the process of creation and make the link disabled.--}}
+{{--                <x-slot name="icon"><i class="fas fa-circle text-green-500 text-xs"></i></x-slot>--}}
                 {{ $server->name }}
             </x-dropdown.link>
         @endforeach
