c2c074effe4a13f4aa698ed038be6e62655c64e1
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-19 18:34:26 +0300

    Create DeleteNginxProjectConfigScript.php

---
+++ app/Scripts/Root/DeleteNginxProjectConfigScript.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use phpseclib3\Net\SFTP;
+
+class DeleteNginxProjectConfigScript extends AbstractServerScript
+{
+    public function execute(Server $server, Project $project, SFTP $rootSsh = null): void
+    {
+        $this->init($server, $rootSsh);
+
+        $available = "/etc/nginx/sites-available";
+        $enabled = "/etc/nginx/sites-enabled";
+        $file = "{$project->projectName}.conf";
+        $placeholder = "{$project->projectName}_placeholder.conf";
+
+        $this->exec("rm -rf {$enabled}/{$file}");
+        $this->exec("rm -rf {$available}/{$file}");
+
+        $this->exec("ln -sf {$available}/{$placeholder} {$enabled}/{$placeholder}");
+    }
+}
