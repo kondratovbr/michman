8c4b8082cc422d63c554885b6e35d35ef1c07b0c
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-19 18:34:29 +0300

    Create DeleteGunicornConfigScript.php

---
+++ app/Scripts/Root/DeleteGunicornConfigScript.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\Scripts\Root;
+
+use App\Models\Project;
+use App\Models\Server;
+use App\Scripts\AbstractServerScript;
+use App\Scripts\Traits\InteractsWithSystemd;
+use phpseclib3\Net\SFTP;
+
+class DeleteGunicornConfigScript extends AbstractServerScript
+{
+    use InteractsWithSystemd;
+
+    public function execute(Server $server, Project $project, SFTP $rootSsh = null): void
+    {
+        $this->init($server, $rootSsh, 'root');
+
+        $projectName = $project->projectName;
+
+        $this->systemdStopService("{$projectName}.service");
+        $this->systemdDisableService("{$projectName}.service");
+
+        $this->systemdStopService("{$projectName}.socket");
+        $this->systemdDisableService("{$projectName}.socket");
+
+        $this->exec("rm -rf {$project->gunicornConfigFilePath}");
+        $this->exec("rm -rf /etc/systemd/system/{$projectName}.service");
+        $this->exec("rm -rf /etc/systemd/system/{$projectName}.socket");
+
+        $this->systemdReload();
+    }
+}
