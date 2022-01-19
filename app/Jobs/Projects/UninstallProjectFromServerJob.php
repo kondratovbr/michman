777f68a19067a39321e5caf22878d97b519d34ee
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-19 18:34:44 +0300

    Update UninstallProjectFromServerJob.php

--- app/Jobs/Projects/UninstallProjectFromServerJob.php
+++ app/Jobs/Projects/UninstallProjectFromServerJob.php
@@ -5,8 +5,13 @@ namespace App\Jobs\Projects;
 use App\Jobs\AbstractRemoteServerJob;
 use App\Models\Project;
 use App\Models\Server;
+use App\Scripts\Root\DeleteGunicornConfigScript;
+use App\Scripts\Root\DeleteNginxProjectConfigScript;
+use App\Scripts\Root\RestartNginxScript;
+use App\Scripts\User\DeleteProjectFilesScript;
+use Illuminate\Support\Facades\DB;
 
-// TODO: CRITICAL! DELETING. Implement.
+// TODO: CRITICAL! CONTINUE. Test.
 
 class UninstallProjectFromServerJob extends AbstractRemoteServerJob
 {
@@ -19,8 +24,28 @@ class UninstallProjectFromServerJob extends AbstractRemoteServerJob
         $this->project = $project->withoutRelations();
     }
 
-    public function handle(): void
-    {
-        //
+    public function handle(
+        DeleteNginxProjectConfigScript $deleteNginxConfig,
+        RestartNginxScript             $restartNginx,
+        DeleteGunicornConfigScript     $deleteGunicornConfig,
+        DeleteProjectFilesScript       $deleteProjectFiles,
+    ): void {
+        DB::transaction(function () use (
+            $deleteNginxConfig, $restartNginx, $deleteGunicornConfig, $deleteProjectFiles,
+        ) {
+            $server = $this->server->freshLockForUpdate();
+            $project = $this->project->freshLockForUpdate();
+
+            $userSsh = $server->sftp($project->serverUsername);
+            $rootSsh = $server->sftp('root');
+
+            $deleteNginxConfig->execute($server, $project, $rootSsh);
+
+            $restartNginx->execute($server, $rootSsh);
+
+            $deleteGunicornConfig->execute($server, $project, $rootSsh);
+
+            $deleteProjectFiles->execute($server, $project, $userSsh);
+        }, 5);
     }
 }
