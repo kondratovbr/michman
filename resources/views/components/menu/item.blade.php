4672c1d5ee587b2e9ea3a63e1090761d3e01b013
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-22 20:11:12 +0300

    Update item.blade.php

--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -1,6 +1,6 @@
 {{--TODO: VERY IMPORTANT! This menu item can't handle the browser's "Back" button - the new item doesn't get highlighted after the "Back" button is pressed - try it. Should fix it.--}}
 
-@props(['show'])
+@props(['show', 'disabled' => false])
 
 <li
     {{-- This compensates for the padding on the first and last elements. --}}
@@ -12,13 +12,13 @@
             x-on:click="current = '{{ $show }}'"
             wire:click.prefetch="show('{{ $show }}')"
         @endisset
-        @if($this->disabled)
+        @if($this->disabled || $disabled)
             disabled
         @endif
     >
         <div
             class="capitalize flex items-center py-3 px-4 rounded-lg border border-gray-300 border-opacity-0 bg-navy-300 bg-opacity-0 group-focus:border-opacity-100 transition-border-background ease-in-out duration-quick"
-            @unless($this->disabled)
+            @unless($this->disabled || $disabled)
                 x-bind:class="{
                     {{-- Default state --}}
                     'bg-opacity-0 group-hover:border-opacity-100 group-active:bg-opacity-100': current !== '{{ $show }}',
@@ -29,7 +29,7 @@
         >
             <div
                 class="md:text-sm lg:text-base transform transition-transform ease-in-out duration-quick"
-                @unless($this->disabled)
+                @unless($this->disabled || $disabled)
                     x-bind:class="{
                         {{-- Default state --}}
                         'group-hover:text-gray-100 group-hover:scale-105': current !== '{{ $show }}',
