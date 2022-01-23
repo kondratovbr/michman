757be7883b1d0578bb3f78ede0875fcd55f26402
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-23 18:07:26 +0300

    Create DeleteAllWorkersAction.php

---
+++ app/Actions/Workers/DeleteAllWorkersAction.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Workers;
+
+use App\Jobs\Workers\DeleteWorkerJob;
+use App\Models\Project;
+use App\Models\Worker;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Bus;
+
+class DeleteAllWorkersAction
+{
+    public function __construct(
+        private DeleteWorkerAction $deleteWorker
+    ) {}
+
+    /** @return Collection<int, DeleteWorkerJob>|null */
+    public function execute(Project $project, bool $returnJobs = false): Collection|null
+    {
+        $jobs = $project->workers->map(fn(Worker $worker) => $this->deleteWorker->execute($worker, true));
+
+        if ($returnJobs)
+            return $jobs;
+
+        Bus::chain($jobs)->dispatch();
+
+        return null;
+    }
+}
