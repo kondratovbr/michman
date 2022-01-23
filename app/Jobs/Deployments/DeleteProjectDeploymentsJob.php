496aed27999a9b9714970cb3dadb1a0a5ff76040
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-23 19:29:00 +0300

    Create DeleteProjectDeploymentsJob.php

---
+++ app/Jobs/Deployments/DeleteProjectDeploymentsJob.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Deployments;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\IsInternal;
+use App\Models\Deployment;
+use App\Models\Project;
+use Illuminate\Support\Facades\DB;
+
+class DeleteProjectDeploymentsJob extends AbstractJob
+{
+    use IsInternal;
+
+    protected Project $project;
+
+    public function __construct(Project $project)
+    {
+        $this->setQueue('default');
+
+        $this->project = $project->withoutRelations();
+    }
+
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            $project = $this->project->freshLockForUpdate('deployments');
+
+            /** @var Deployment $deployment */
+            foreach ($project->deployments as $deployment) {
+                $deployment->delete();
+            }
+        }, 5);
+    }
+}
