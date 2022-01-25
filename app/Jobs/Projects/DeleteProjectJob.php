7002d676fc6b726b8619803a4d5623b29b207e38
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-25 15:05:23 +0300

    Create DeleteProjectJob.php

---
+++ app/Jobs/Projects/DeleteProjectJob.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Projects;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\IsInternal;
+use App\Models\Project;
+use Illuminate\Support\Facades\DB;
+
+// TODO: Cover with tests.
+
+class DeleteProjectJob extends AbstractJob
+{
+    use IsInternal;
+
+    protected Project $project;
+
+    public function __construct(Project $project)
+    {
+        parent::__construct();
+
+        $this->project = $project;
+    }
+
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            $project = $this->project->freshLockForUpdate();
+
+            $project->servers()->sync([]);
+
+            $project->deploySshKey()->delete();
+
+            $project->delete();
+        }, 5);
+    }
+}
