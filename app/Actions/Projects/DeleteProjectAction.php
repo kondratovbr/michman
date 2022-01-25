14f3993e19a9d76845efeb5a45ab90eca83b1919
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-25 15:06:34 +0300

    Update DeleteProjectAction.php

--- app/Actions/Projects/DeleteProjectAction.php
+++ app/Actions/Projects/DeleteProjectAction.php
@@ -2,18 +2,41 @@
 
 namespace App\Actions\Projects;
 
+use App\Jobs\Projects\DeleteProjectJob;
+use App\Jobs\Servers\DeleteUserFromServerJob;
 use App\Models\Project;
+use App\Models\Server;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
 
 class DeleteProjectAction
 {
     public function __construct(
-        UninstallProjectRepoAction $uninstallRepo,
+        protected UninstallProjectRepoAction $uninstallRepo,
     ) {}
 
-    public function execute(Project $project): void
+    public function execute(Project $project, bool $returnJobs = false): Collection|null
     {
         // TODO: CRITICAL! CONTINUE. Implement.
 
-        //
+        return DB::transaction(function () use ($project, $returnJobs): Collection|null {
+            $project = $project->freshLockForUpdate();
+
+            $jobs = $this->uninstallRepo->execute($project, true);
+
+            $jobs->push($project->servers->map(
+                fn(Server $server) => new DeleteUserFromServerJob($server, $project->serverUsername)
+            ));
+
+            $jobs->push(new DeleteProjectJob($project));
+
+            if ($returnJobs)
+                return $jobs;
+
+            Bus::chain($jobs->toArray())->dispatch();
+
+            return null;
+        }, 5);
     }
 }
