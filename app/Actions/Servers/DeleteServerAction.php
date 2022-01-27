e318a3eb0f780eb494c46cf8408a59036dbebc6e
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-27 13:41:19 +0300

    Update DeleteServerAction.php

--- app/Actions/Servers/DeleteServerAction.php
+++ app/Actions/Servers/DeleteServerAction.php
@@ -2,18 +2,44 @@
 
 namespace App\Actions\Servers;
 
-// TODO: Cover with tests.
-
+use App\Actions\Projects\DeleteProjectAction;
+use App\Jobs\Servers\DeleteServerJob;
+use App\Models\Project;
 use App\Models\Server;
+use App\States\Servers\Deleting;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
+
+// TODO: Cover with tests.
 
 class DeleteServerAction
 {
+    public function __construct(
+        private DeleteProjectAction $deleteProject,
+    ) {}
+
     public function execute(Server $server): void
     {
-        // TODO: CRITICAL! CONTINUE. Implement.
+        DB::transaction(function () use ($server) {
+            $server = $server->freshLockForUpdate();
 
-        ray('Deleting server ' . $server->name);
+            if (! $server->state->canTransitionTo(Deleting::class))
+                return;
 
-        //
+            $server->state->transitionTo(Deleting::class);
+
+            $jobs = new Collection;
+
+            /** @var Project $project */
+            foreach ($server->projects as $project) {
+                if ($project->servers->count() === 1)
+                    $jobs = $jobs->concat($this->deleteProject->execute($project, true));
+            }
+
+            $jobs->push(new DeleteServerJob($server));
+
+            Bus::chain($jobs->toArray())->dispatch();
+        }, 5);
     }
 }
