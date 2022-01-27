82ab7618fb65d5e16fb140ff90f0b24d7a557730
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-27 13:41:13 +0300

    Update placeholder.blade.php

--- resources/views/servers/placeholder.blade.php
+++ resources/views/servers/placeholder.blade.php
@@ -1,6 +1,11 @@
 {{--TODO: Improve this placeholder. Show progress, show steps, add a button to stop the process and delete the server. Don't forget the server information header.--}}
 
 <div class="flex flex-col items-center">
-    <span class="text-2xl">{{ __('servers.placeholder.preparing') }}</span>
+    <span class="text-2xl">
+        {{ match ($server->state::$name) {
+            'deleting' => __('servers.placeholder.deleting'),
+            default => __('servers.placeholder.preparing'),
+        } }}
+    </span>
     <x-spinner class="mt-4 text-2xl"/>
 </div>
