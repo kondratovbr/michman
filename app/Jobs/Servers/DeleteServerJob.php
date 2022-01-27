ce28947c9865e2ed5fb3a4eb1e94f5545ba511ca
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-01-27 13:41:07 +0300

    Create DeleteServerJob.php

---
+++ app/Jobs/Servers/DeleteServerJob.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace App\Jobs\Servers;
+
+use App\Jobs\AbstractJob;
+use App\Jobs\Traits\IsInternal;
+use App\Models\Server;
+use App\States\Servers\Deleting;
+use Illuminate\Support\Facades\DB;
+
+/*
+ * TODO: Numerous failures may happen here. Need to handle somehow.
+ *       Deletion from provider may fail, for example.
+ */
+
+// TODO: Cover with tests.
+
+class DeleteServerJob extends AbstractJob
+{
+    use IsInternal;
+
+    protected Server $server;
+
+    public function __construct(Server $server)
+    {
+        parent::__construct();
+
+        $this->server = $server->withoutRelations();
+    }
+
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            $server = $this->server->freshLockForUpdate([
+                'databases',
+                'database_users',
+                'worker_ssh_key',
+                'pythons',
+                'server_ssh_key',
+                'firewall_rules',
+                'certificates',
+                'workers',
+                'daemons',
+            ]);
+
+            if (! $server->state->is(Deleting::class))
+                return;
+
+            if (
+                $server->projects()->count() > 0
+                || $server->deployments()->count() > 0
+                || $server->workers()->count() > 0
+            )
+                return;
+
+            $server->userSshKeys()->sync([]);
+
+            $server->databases()->delete();
+            $server->databaseUsers()->delete();
+            $server->workerSshKey()->delete();
+            $server->pythons()->delete();
+            $server->serverSshKey()->delete();
+            $server->firewallRules()->delete();
+            $server->certificates()->delete();
+            $server->daemons()->delete();
+
+            $server->provider->api()->deleteServer($server->externalId);
+
+            $server->delete();
+        }, 5);
+    }
+}
