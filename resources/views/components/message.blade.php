fe4b73aac65a2656c83ee3effea3c3a1e7f5b98f
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-02-01 16:31:51 +0300

    Update message.blade.php

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -5,7 +5,7 @@
     match ($colors ?? null) {
         'info'      => 'bg-navy-500 text-blue-100',
         'success'   => 'bg-green-800 text-green-100',
-        'warning'   => 'bg-yellow-400 text-yellow-900',
+        'warning'   => 'bg-yellow-300 text-yellow-900',
         'danger'    => 'bg-red-700 text-red-100',
         'none'      => '',
          default    => 'bg-gray-700 text-gray-100',
