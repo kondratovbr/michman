2f010a810cb0cd691c1e8b4d7bc84ef6bf04049a
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-02-04 16:50:53 +0300

    Safeguard with an exception for now

--- app/Actions/Jetstream/DeleteTeam.php
+++ app/Actions/Jetstream/DeleteTeam.php
@@ -2,15 +2,17 @@
 
 namespace App\Actions\Jetstream;
 
+use App\Exceptions\NotImplementedException;
+use App\Models\Team;
 use Laravel\Jetstream\Contracts\DeletesTeams;
 
 class DeleteTeam implements DeletesTeams
 {
-    /**
-     * Delete the given team.
-     */
+    /** @param Team $team */
     public function delete($team): void
     {
+        throw new NotImplementedException('Team deletion is not implemented yet.');
+
         $team->purge();
     }
 }
