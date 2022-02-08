aa34c7eba5f46146c8aebc38855fceaa01e4a321
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-02-08 17:39:19 +0300

    Update DeleteUserAction.php

--- app/Actions/Users/DeleteUserAction.php
+++ app/Actions/Users/DeleteUserAction.php
@@ -2,24 +2,43 @@
 
 namespace App\Actions\Users;
 
+use App\Actions\Webhooks\DeleteProjectWebhookAction;
 use App\Jobs\Users\DeleteUserJob;
 use App\Models\User;
+use App\Models\Webhook;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\DB;
 
 // TODO: Cover with tests.
 
 class DeleteUserAction
 {
+    public function __construct(
+        private DeleteProjectWebhookAction $deleteWebhook,
+    ) {}
+
     public function execute(User $user): void
     {
         DB::transaction(function () use ($user) {
             $user->freshLockForUpdate();
 
-            DeleteUserJob::dispatch($user);
+            $jobs = new Collection;
+
+            $jobs = $jobs->concat($this->deleteWebhooksJobs($user));
+
+            $jobs->push(new DeleteUserJob($user));
+
+            Bus::chain($jobs->toArray())->dispatch();
 
             $user->delete();
         }, 5);
     }
 
-
+    protected function deleteWebhooksJobs(User $user): Collection
+    {
+        return $user->webhooks->map(fn(Webhook $hook) =>
+            $this->deleteWebhook->execute($hook, true)
+        );
+    }
 }
