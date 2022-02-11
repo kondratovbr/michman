8d746912e41a7ad72d06a93a4819a0a6c80e5501
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-02-11 16:58:13 +0300

    Code style

--- resources/views/components/bottombar/link.blade.php
+++ resources/views/components/bottombar/link.blade.php
@@ -7,8 +7,7 @@
         href="{{ route($routeName) }}"
     @endisset
 >
-    <div
-        class="flex-grow px-4 rounded-md border border-gray-200 border-opacity-0 group-focus:border-opacity-100 transition-border-background ease-in-out duration-quick flex justify-center items-center     group-hover:border-opacity-100 group-hover:text-gray-100 bg-navy-400 bg-opacity-0 group-active:bg-opacity-100 text-gray-200 capitalize"
+    <div class="flex-grow px-4 rounded-md border border-gray-200 border-opacity-0 group-focus:border-opacity-100 transition-border-background ease-in-out duration-quick flex justify-center items-center group-hover:border-opacity-100 group-hover:text-gray-100 bg-navy-400 bg-opacity-0 group-active:bg-opacity-100 text-gray-200 capitalize"
     >
         @isset($icon)
             <x-icon class="mr-2 text-2xl" size="8">{{ $icon }}</x-icon>
