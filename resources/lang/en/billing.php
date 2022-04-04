1657e4e4750d4d1a8fbdfa081692ce5291a2bf76
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-04-04 16:41:03 +0300

    Update wording

--- resources/lang/en/billing.php
+++ resources/lang/en/billing.php
@@ -2,8 +2,7 @@
 
 return [
 
-    // TODO: Add some emoji here, like ploi.io does. Example: 🚀. Maybe find something marine-like.
-    'trial-bar' => 'You are currently on a trial with only one day left, if Michman is useful for you make sure to upgrade!|You are currently on a trial with :count days left, if Michman is useful for you make sure to upgrade!',
+    'trial-bar' => 'You are currently on a trial with only one day left, if Michman is useful for you - make sure to upgrade! ⛵|You are currently on a trial with :count days left, if Michman is useful for you - make sure to upgrade! ⛵',
 
     'not-subscribed-bar' => 'Your have no active subscription. Please subscribe to keep using Michman.',
 
