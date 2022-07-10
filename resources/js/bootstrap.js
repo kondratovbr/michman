b9a9ab639ce448c67c2abf798ea2ba0dee27bc63
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-07-10 14:41:03 +0200

    Fix indentation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,22 +16,22 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
- import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
- window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
- window.Echo = new Echo({
-     broadcaster: 'pusher',
-     key: process.env.MIX_PUSHER_APP_KEY,
-     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-     // TODO: CRITICAL! DEPLOYMENT. Don't forget to configure TLS for production!
-     //forceTLS: true,
-     forceTLS: false,
-     // These two parameters are needed to make beyondcode/laravel-websockets work.
-     // Port should be the same as configured in broadcasting.php.
-     // TODO: CRITICAL! DEPLOYMENT. Make sure it actually works in production with my Nginx setup and that the browser doesn't make any requests to pusher.com. The front-end library is a bit wonky on it.
-     wsHost: window.location.hostname,
-     wsPort: 6001,
-     // This disables Pusher API statistics. beyondcode/laravel-websockets has its own statistic gathering engine.
-     disableStats: true,
- });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // TODO: CRITICAL! DEPLOYMENT. Don't forget to configure TLS for production!
+    //forceTLS: true,
+    forceTLS: false,
+    // These two parameters are needed to make beyondcode/laravel-websockets work.
+    // Port should be the same as configured in broadcasting.php.
+    // TODO: CRITICAL! DEPLOYMENT. Make sure it actually works in production with my Nginx setup and that the browser doesn't make any requests to pusher.com. The front-end library is a bit wonky on it.
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    // This disables Pusher API statistics. beyondcode/laravel-websockets has its own statistic gathering engine.
+    disableStats: true,
+});
