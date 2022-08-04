7622f6ae009a50be03d36b4c3b2d862fbaa81dfe
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-08-04 09:09:23 +0200

    fix: Fix select components

--- resources/views/components/search-select.blade.php
+++ resources/views/components/search-select.blade.php
@@ -30,7 +30,6 @@
     @endif
     x-on:click.outside="closeListbox()"
     x-on:keydown.escape="closeListbox(true)"
-    {{ $attributes->wire('key') }}
 >
     {{-- Hidden select for keeping the state and interacting with Livewire --}}
     <select
