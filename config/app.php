a8581a31f8319ae45768b380afff8670105a8961
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-08-12 16:34:36 +0200

    Bump version

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.9.3',
+    'version' => '0.9.4',
 
     /*
     |--------------------------------------------------------------------------
