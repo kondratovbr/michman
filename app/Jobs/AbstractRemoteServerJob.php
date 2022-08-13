53c3ee49b05314e8ed99cd6b105e82c9cc9fd04e
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-08-13 19:08:23 +0200

    Cleanup

--- app/Jobs/AbstractRemoteServerJob.php
+++ app/Jobs/AbstractRemoteServerJob.php
@@ -22,7 +22,6 @@ abstract class AbstractRemoteServerJob extends AbstractJob
         return now()->addMinutes(60);
     }
 
-    /** @return $this */
     public function __construct(Server $server, bool $sync = false)
     {
         parent::__construct();
@@ -30,8 +29,6 @@ abstract class AbstractRemoteServerJob extends AbstractJob
         $this->sync($sync);
 
         $this->server = $server->withoutRelations();
-
-        return $this;
     }
 
     protected function getQueue(): string
