073ac0652ead03470a523eafb3ff5067ed1efefe
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2022-08-18 20:10:07 +0200

    Update PrepareRemoteServerJob.php

--- app/Jobs/Servers/PrepareRemoteServerJob.php
+++ app/Jobs/Servers/PrepareRemoteServerJob.php
@@ -18,6 +18,8 @@ use App\Scripts\Root\UpdateSnapScript;
 use App\Scripts\Root\UpgradePackagesScript;
 use App\States\Servers\Failed;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 // TODO: IMPORTANT! Cover with tests!
 
@@ -36,46 +38,52 @@ class PrepareRemoteServerJob extends AbstractRemoteServerJob
         ConfigureSshServerScript $configureSshServer,
         RebootServerScript $rebootServer,
     ): void {
-        DB::transaction(function () use (
-            $upgradePackages, $updateSnap, $installBasePackages, $installSnapApps, $configureUnattendedUpgrades,
-            $initializeFirewall, $enableFirewall, $createSudoUser, $configureSshServer, $rebootServer,
-            $storeFirewallRule,
-        ) {
-            $server = $this->server->freshLockForUpdate();
+        try {
+            DB::transaction(function () use (
+                $upgradePackages, $updateSnap, $installBasePackages, $installSnapApps, $configureUnattendedUpgrades,
+                $initializeFirewall, $enableFirewall, $createSudoUser, $configureSshServer, $rebootServer,
+                $storeFirewallRule,
+            ) {
+                $server = $this->server->freshLockForUpdate();
 
-            $ssh = $server->sftp('root');
+                $ssh = $server->sftp('root');
 
-            $upgradePackages->execute($server, $ssh);
+                $upgradePackages->execute($server, $ssh);
 
-            $updateSnap->execute($server, $ssh);
+                $updateSnap->execute($server, $ssh);
 
-            $installBasePackages->execute($server, $ssh);
+                $installBasePackages->execute($server, $ssh);
 
-            $installSnapApps->execute($server, $ssh);
+                $installSnapApps->execute($server, $ssh);
 
-            $configureUnattendedUpgrades->execute($server, $ssh);
+                $configureUnattendedUpgrades->execute($server, $ssh);
 
-            $initializeFirewall->execute($server, $ssh);
+                $initializeFirewall->execute($server, $ssh);
 
-            $storeFirewallRule->execute(new FirewallRuleDto(
-                name: 'SSH',
-                port: '22',
-                can_delete: false,
-            ), $server, true);
+                $storeFirewallRule->execute(new FirewallRuleDto(
+                    name: 'SSH',
+                    port: '22',
+                    can_delete: false,
+                ), $server, true);
 
-            $enableFirewall->execute($server, $ssh);
+                $enableFirewall->execute($server, $ssh);
 
-            $createSudoUser->execute(
-                $server,
-                (string) config('servers.worker_user'),
-                $server->sudoPassword,
-                $ssh,
-            );
+                $createSudoUser->execute(
+                    $server,
+                    (string) config('servers.worker_user'),
+                    $server->sudoPassword,
+                    $ssh,
+                );
 
-            $configureSshServer->execute($server, $ssh);
+                $configureSshServer->execute($server, $ssh);
 
-            $rebootServer->execute($server, $ssh);
-        });
+                $rebootServer->execute($server, $ssh);
+            });
+        } catch (Throwable $e) {
+            Log::info($e);
+
+            throw $e;
+        }
     }
 
     public function failed(): void
