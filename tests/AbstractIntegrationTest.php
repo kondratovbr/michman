ee430fb2252f867aa87db3b7be6ee8c6c84ecbf0
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2023-02-22 19:29:01 +0100

    Fix integration tests

--- tests/AbstractIntegrationTest.php
+++ tests/AbstractIntegrationTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\Traits\CreatesApplication;
 
 abstract class AbstractIntegrationTest extends TestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase, WithFaker;
 }
