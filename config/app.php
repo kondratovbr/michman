1a329ea3b68f4e8cd0a0f1753e5001614327f25f
Bogdan Kondratov <5469212+kondratovbr@users.noreply.github.com>
2023-02-22 19:28:50 +0100

    Bump version on staging

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '1.0.0',
+    'version' => '1.1.0',
 
     /*
     |--------------------------------------------------------------------------
